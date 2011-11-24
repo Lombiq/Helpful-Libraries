9319688e649d6edee972d6d85d9699c89b05b10c
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-11-24 22:04:10 +0100

    Better documentation

--- Libs/Tasks/ITaskFactory.cs
+++ Libs/Tasks/ITaskFactory.cs
@@ -5,11 +5,45 @@ using System.Threading.Tasks;
 
 namespace Piedone.HelpfulLibraries.Tasks
 {
+    /// <summary>
+    /// Creates Task objects that work well with the Orchard ecosystem
+    /// </summary>
     public interface ITaskFactory : ISingletonDependency
     {
+        /// <summary>
+        /// Creates a new Task instance with the specified parameters
+        /// 
+        /// All parameters are optional. For documentation please refer to the documentation of the Task class.
+        /// </summary>
+        /// <see cref="System.Threading.Tasks.Task"/>
         Task Factory(Action action, CancellationToken cancellationToken = new CancellationToken(), TaskCreationOptions creationOptions = TaskCreationOptions.None, bool catchExceptions = true);
+
+        /// <summary>
+        /// Creates a new Task instance with the specified parameters
+        /// 
+        /// All parameters are optional. For documentation please refer to the documentation of the Task class.
+        /// </summary>
+        /// <see cref="System.Threading.Tasks.Task"/>
         Task Factory(Action<object> action, object state, CancellationToken cancellationToken = new CancellationToken(), TaskCreationOptions creationOptions = TaskCreationOptions.None, bool catchExceptions = true);
+        
+        /// <summary>
+        /// Encapsulates the specified action so that it can safely run Orchard code
+        /// 
+        /// Use this if you want to instantiate Task yourself.
+        /// </summary>
+        /// <param name="action">The action to execute</param>
+        /// <param name="catchExceptions">If true, exceptions thrown from the action will be caught and logged (defaults to true)</param>
+        /// <returns>The encapsulated action</returns>
         Action BuildTaskAction(Action action, bool catchExceptions = true);
+
+        /// <summary>
+        /// Encapsulates the specified action so that it can safely run Orchard code
+        /// 
+        /// Use this if you want to instantiate Task yourself.
+        /// </summary>
+        /// <param name="action">The action to execute</param>
+        /// <param name="catchExceptions">If true, exceptions thrown from the action will be caught and logged (defaults to true)</param>
+        /// <returns>The encapsulated action</returns>
         Action<object> BuildTaskAction(Action<object> action, bool catchExceptions = true);
     }
 }
