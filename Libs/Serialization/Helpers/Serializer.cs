2af1174c0f45cb4f3d62a0856bec0c83e6c006f0
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-05 19:16:04 +0100

    Adding serialization libs

---
+++ Libs/Serialization/Helpers/Serializer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard.Environment.Extensions;
+using System.IO;
+using System.Xml;
+using System.Runtime.Serialization;
+
+namespace Piedone.HelpfulLibraries.Serialization.Helpers
+{
+    /// <summary>
+    /// Simplified serialization to/from strings. Uses DataContractSerializer under the hood, therefore classes 
+    /// (and their members)of objects used to serialize should be decorated with the appropriate attributes, like
+    /// [DataContract] (for classes) and [DataMember] (for properties).
+    /// </summary>
+    [OrchardFeature("Piedone.HelpfulLibraries.Serialization")]
+    public class Serializer
+    {
+        public static string Serialize<T>(T obj)
+        {
+            string serialization;
+
+            using (var sw = new StringWriter())
+            {
+                using (var writer = new XmlTextWriter(sw))
+                {
+                    var serializer = new DataContractSerializer(obj.GetType());
+                    serializer.WriteObject(writer, obj);
+                    writer.Flush();
+                    serialization = sw.ToString();
+                }
+            }
+
+            return serialization;
+        }
+
+        public static T Deserialize<T>(string serialization)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            var doc = new XmlDocument();
+            doc.LoadXml(serialization);
+            var reader = new XmlNodeReader(doc.DocumentElement);
+            return (T)serializer.ReadObject(reader);
+        }
+    }
+}
