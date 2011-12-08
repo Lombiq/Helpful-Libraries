07611ce19db7866d8ac3e77a2b6c7dee5e5fef3e
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-08 20:41:17 +0100

    Serializer an IDependency, not static anymore

---
+++ Libs/Serialization/ISimpleSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Orchard;
+
+namespace Piedone.HelpfulLibraries.Serialization
+{
+    public interface ISimpleSerializer : ISingletonDependency
+    {
+        /// <summary>
+        /// Serializes an object to string. Note that since the method uses DataContractSerializer under the hood classes
+        /// and their members should be decorated with the appropriate attributes, like [DataContract] (for classes) and 
+        /// [DataMember] (for properties).
+        /// </summary>
+        /// <typeparam name="T">The type of the object to serialize</typeparam>
+        /// <param name="obj">The object to serialize</param>
+        /// <returns>The string serialization of the object</returns>
+        string Serialize<T>(T obj);
+
+        /// <summary>
+        /// Deserializes an object previously serialized with Serialize()
+        /// </summary>
+        /// <typeparam name="T">The type of the object that was serialized</typeparam>
+        /// <param name="serialization">String serialization of the object</param>
+        /// <returns>The deserialized object</returns>
+        T Deserialize<T>(string serialization);
+    }
+}
--- Libs/Serialization/SimpleSerializer.cs
+++ Libs/Serialization/SimpleSerializer.cs
@@ -7,7 +7,7 @@ using System.IO;
 using System.Xml;
 using System.Runtime.Serialization;
 
-namespace Piedone.HelpfulLibraries.Serialization.Helpers
+namespace Piedone.HelpfulLibraries.Serialization
 {
     /// <summary>
     /// Simplified serialization to/from strings. Uses DataContractSerializer under the hood, therefore classes 
@@ -15,17 +15,9 @@ namespace Piedone.HelpfulLibraries.Serialization.Helpers
     /// [DataContract] (for classes) and [DataMember] (for properties).
     /// </summary>
     [OrchardFeature("Piedone.HelpfulLibraries.Serialization")]
-    public class Serializer
+    public class SimpleSerializer : ISimpleSerializer
     {
-        /// <summary>
-        /// Serializes an object to string. Note that since the method uses DataContractSerializer under the hood classes
-        /// and their members should be decorated with the appropriate attributes, like [DataContract] (for classes) and 
-        /// [DataMember] (for properties).
-        /// </summary>
-        /// <typeparam name="T">The type of the object to serialize</typeparam>
-        /// <param name="obj">The object to serialize</param>
-        /// <returns>The string serialization of the object</returns>
-        public static string Serialize<T>(T obj)
+        public string Serialize<T>(T obj)
         {
             string serialization;
 
@@ -43,13 +35,7 @@ namespace Piedone.HelpfulLibraries.Serialization.Helpers
             return serialization;
         }
 
-        /// <summary>
-        /// Deserializes an object previously serialized with Serialize()
-        /// </summary>
-        /// <typeparam name="T">The type of the object that was serialized</typeparam>
-        /// <param name="serialization">String serialization of the object</param>
-        /// <returns>The deserialized object</returns>
-        public static T Deserialize<T>(string serialization)
+        public T Deserialize<T>(string serialization)
         {
             var serializer = new DataContractSerializer(typeof(T));
             var doc = new XmlDocument();
