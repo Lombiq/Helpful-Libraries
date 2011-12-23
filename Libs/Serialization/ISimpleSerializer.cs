a0c08e08fc4a9d4ba40d54d7f249031031a1be93
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-23 21:48:52 +0100

    Inline documentation

--- Libs/Serialization/ISimpleSerializer.cs
+++ Libs/Serialization/ISimpleSerializer.cs
@@ -3,6 +3,9 @@ using Orchard;
 
 namespace Piedone.HelpfulLibraries.Serialization
 {
+    /// <summary>
+    /// Easy object serialization and deserialization
+    /// </summary>
     public interface ISimpleSerializer : ISingletonDependency
     {
         /// <summary>
@@ -32,14 +35,14 @@ namespace Piedone.HelpfulLibraries.Serialization
         /// </summary>
         /// <typeparam name="T">The type of the object to serialize</typeparam>
         /// <param name="obj">The object to serialize</param>
-        /// <returns>The string serialization of the object</returns>
+        /// <returns>The XML string serialization of the object</returns>
         string XmlSerialize<T>(T obj);
 
         /// <summary>
         /// Deserializes an object previously serialized with XmlSerialize()
         /// </summary>
         /// <typeparam name="T">The type of the object that was serialized</typeparam>
-        /// <param name="serialization">String serialization of the object</param>
+        /// <param name="serialization">XML string serialization of the object</param>
         /// <returns>The deserialized object</returns>
         T XmlDeserialize<T>(string serialization);
     }
