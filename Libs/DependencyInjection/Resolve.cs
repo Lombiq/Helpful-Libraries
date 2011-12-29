20cef27bb1205993fe6d5a93bddae9e4494a3dbc
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-29 12:45:10 +0100

    Dependency Injection Libraries (IResolve<>)

---
+++ Libs/DependencyInjection/Resolve.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard;
+using Orchard.Environment.Extensions;
+
+namespace Piedone.HelpfulLibraries.DependencyInjection
+{
+    [OrchardFeature("Piedone.HelpfulLibraries.DependencyInjection")]
+    public class Resolve<T> : IResolve<T>
+    {
+        private readonly IWorkContextAccessor _workContextAccessor;
+
+        public Resolve(IWorkContextAccessor workContextAccessor)
+        {
+            _workContextAccessor = workContextAccessor;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return _workContextAccessor.GetContext().Resolve<T>();
+            }
+        }
+    }
+}
