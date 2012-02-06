f9e716763fdf081c3ccc24c4bc885576cc41594c
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-02-06 19:39:03 +0100

    Contents Libaries API comments

--- Libraries/Contents/ContentExtensions.cs
+++ Libraries/Contents/ContentExtensions.cs
@@ -10,18 +10,37 @@ namespace Piedone.HelpfulLibraries.Contents
     [OrchardFeature("Piedone.HelpfulLibraries.Contents")]
     public static class ContentExtensions
     {
+        /// <summary>
+        /// Retrieves the first field with the specified type from the part
+        /// </summary>
+        /// <typeparam name="T">Type of the field</typeparam>
+        /// <param name="partName">Name of the part that contains the field</param>
+        /// <returns>The field object</returns>
         public static T AsField<T>(this IContent content, string partName)
             where T : ContentField
         {
             return AsField<T>(content, partName, LookupField<T>(content, partName).Name);
         }
 
+        /// <summary>
+        /// Checks if the specified part contains the field with the specified type
+        /// </summary>
+        /// <typeparam name="T">Type of the field</typeparam>
+        /// <param name="partName">Name of the part that contains the field</param>
+        /// <returns>True if the part contains the field, false otherwise</returns>
         public static bool HasField<T>(this IContent content, string partName)
             where T : ContentField
         {
             return HasField(content, partName, LookupField<T>(content, partName).Name);
         }
 
+        /// <summary>
+        /// Retrieves the field with the specified name from the part
+        /// </summary>
+        /// <typeparam name="T">Type of the field</typeparam>
+        /// <param name="partName">Name of the part that contains the field</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>The field object</returns>
         public static T AsField<T>(this IContent content, string partName, string fieldName)
             where T : ContentField
         {
@@ -35,6 +54,12 @@ namespace Piedone.HelpfulLibraries.Contents
                 .SingleOrDefault();
         }
 
+        /// <summary>
+        /// Checks if the specified part contains the specified field
+        /// </summary>
+        /// <param name="partName">Name of the part that contains the field</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>True if the part contains the field, false otherwise</returns>
         public static bool HasField(this IContent content, string partName, string fieldName)
         {
             // This is so the behaviour is consistent with Orchard.ContentManagement.ContentExtensions.Has<>()
