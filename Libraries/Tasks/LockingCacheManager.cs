85abc8a696c579b3d5a6c003c67b0fa1d7034884
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-03-04 18:51:37 +0100

    Typo

--- Libraries/Tasks/LockingCacheManager.cs
+++ Libraries/Tasks/LockingCacheManager.cs
@@ -20,7 +20,7 @@ namespace Piedone.HelpfulLibraries.Tasks
 
         public TResult Get<TResult>(string key, Func<AcquireContext<string>, TResult> acquire, Func<TResult> fallback, int millisecondsTimeout = 4000)
         {
-            // When using with arbitrary types, key.ToString() could leave to errors if the key is not string or the ToString() is not properly implemented.
+            // When using with arbitrary types, key.ToString() could lead to errors if the key is not string or the ToString() is not properly implemented.
             // That's why we only allow string keys here.
             using (var lockFile = _lockFileManager.TryAcquireLock(key, millisecondsTimeout))
             {
