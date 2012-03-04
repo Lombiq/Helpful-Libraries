420bedb9f6cdb50ab32ac6927196d37e0b82e415
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-03-04 18:51:47 +0100

    Utilities with IFreezable

---
+++ Libraries/Utilities/IFreezable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Piedone.HelpfulLibraries.Utilities
+{
+    /// <summary>
+    /// Describes a class that initially can be modified but after frozen not anymore
+    /// </summary>
+    public interface IFreezable
+    {
+        /// <summary>
+        /// Checks if the object can be modified or not
+        /// </summary>
+        bool IsFrozen { get; }
+
+        /// <summary>
+        /// Prevents further modification of the object
+        /// </summary>
+        void Freeze();
+    }
+}
