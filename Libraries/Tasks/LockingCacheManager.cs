256f1f5a67e2739eb2ad3e77ef74e57a3bd63f4e
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-03-11 20:15:25 +0100

    This could prevent file locking if the cache entry is present. Could cause that the Monitor() call in the inner acquire doesn't get registered when the fallback is run.
    --HG--
    branch : LockingCacheManagerAlternatives

--- Libraries/Tasks/LockingCacheManager.cs
+++ Libraries/Tasks/LockingCacheManager.cs
@@ -22,15 +22,19 @@ namespace Piedone.HelpfulLibraries.Tasks
         {
             // When using with arbitrary types, key.ToString() could lead to errors if the key is not string or the ToString() is not properly implemented.
             // That's why we only allow string keys here.
-            using (var lockFile = _lockFileManager.TryAcquireLock(key, millisecondsTimeout))
-            {
-                if (lockFile != null)
+
+            return _cacheManager.Get(key, ctx =>
                 {
-                    // If we waited for the lock to be released, here the result computed by the locking code should be returned.
-                    return _cacheManager.Get(key, acquire);
-                }
-                else return fallback();
-            }
+                    using (var lockFile = _lockFileManager.TryAcquireLock(key, millisecondsTimeout))
+                    {
+                        if (lockFile != null)
+                        {
+                            // If we waited for the lock to be released, here the result computed by the locking code should be returned.
+                            return _cacheManager.Get(key, acquire);
+                        }
+                        else return fallback();
+                    }
+                });
         }
     }
 }
