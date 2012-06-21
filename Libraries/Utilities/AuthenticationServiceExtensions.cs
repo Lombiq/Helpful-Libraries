81fa85a0cb9a50c56501f22243e005f130f23bc4
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-06-21 17:29:20 +0200

    AuthenticationServiceExtensions

---
+++ Libraries/Utilities/AuthenticationServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard.Environment.Extensions;
+using Orchard.Security;
+
+namespace Piedone.HelpfulLibraries.Utilities
+{
+    [OrchardFeature("Piedone.HelpfulLibraries.Utilities")]
+    public static class AuthenticationServiceExtensions
+    {
+        public static bool IsAuthenticated(this IAuthenticationService service)
+        {
+            return service.GetAuthenticatedUser() != null;
+        }
+    }
+}
