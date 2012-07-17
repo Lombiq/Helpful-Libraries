f16cfeeb5bb12dbcb0dbea71d9effe0ea865332a
Piedone <piedone@pyrocenter.hu>
2012-07-17 14:12:34 +0200

    IdSerializer and Empty shape template

---
+++ Libraries/Serialization/IdSerializer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace Piedone.HelpfulLibraries.Serialization
+{
+    public class IdSerializer
+    {
+        private IEnumerable<int> _ids;
+        public IEnumerable<int> Ids
+        {
+            get
+            {
+                if (_ids == null)
+                {
+                    _ids = DeserializeIds(_idsDefinition);
+                }
+
+                return _ids;
+            }
+            set
+            {
+                _ids = value;
+                _idsDefinition = SerializeIds(_ids);
+            }
+        }
+
+
+        private string _idsDefinition;
+
+        /// <summary>
+        /// JSON array of the ids
+        /// </summary>
+        public string IdsDefinition
+        {
+            get { return _idsDefinition; }
+            set
+            {
+                _idsDefinition = value;
+                _ids = null;
+            }
+        }
+
+
+        public IdSerializer(string idsDefinition = "")
+        {
+            IdsDefinition = idsDefinition;
+        }
+
+
+        public static string SerializeIds(IEnumerable<int> ids)
+        {
+            if (ids == null) ids = Enumerable.Empty<int>();
+            return new JavaScriptSerializer().Serialize(ids);
+        }
+
+        public static IEnumerable<int> DeserializeIds(string idsDefinition)
+        {
+            if (String.IsNullOrEmpty(idsDefinition))
+            {
+                return Enumerable.Empty<int>();
+            }
+
+            return new JavaScriptSerializer().Deserialize<IEnumerable<int>>(idsDefinition);
+        }
+    }
+}
