b8a0f659f624e70dbb59f20897e9f85e8dd8ade2
Piedone <piedone@pyrocenter.hu>
2012-11-06 23:48:34 +0100

    IJobManager documentation

--- Libraries/Tasks/Jobs/IJobManager.cs
+++ Libraries/Tasks/Jobs/IJobManager.cs
@@ -7,12 +7,42 @@ using Orchard.Environment.Extensions;
 
 namespace Piedone.HelpfulLibraries.Tasks.Jobs
 {
+    /// <summary>
+    /// Deals with managing tasks that should be completed once
+    /// </summary>
     public interface IJobManager : IDependency
     {
+        /// <summary>
+        /// Creates a new job in the specified industy
+        /// </summary>
+        /// <param name="industry">"Industry", i.e. type, group of the job</param>
+        /// <param name="context">An arbitrary serializable context object</param>
         void CreateJob(string industry, object context);
+
+        /// <summary>
+        /// Takes the oldest job of the industry. Till this job is not done or given back calls to this method won't receive a job.
+        /// </summary>
+        /// <param name="industry">"Industry", i.e. type, group of the job</param>
+        /// <returns>The oldest job or null if there are no jobs or the only one is already worked on.</returns>
         IJob TakeOnlyJob(string industry);
+
+        /// <summary>
+        /// Takes the oldest available job of the industry. Subsequent calls to this method will get the next jobs, even if the calls were simultaneous.
+        /// </summary>
+        /// <param name="industry">"Industry", i.e. type, group of the job</param>
+        /// <returns>The oldest available job of the industy or null if there are no available jobs left.</returns>
         IJob TakeJob(string industry);
+
+        /// <summary>
+        /// Marks the job as done, removing it from the set of available jobs
+        /// </summary>
+        /// <param name="job">The job object</param>
         void Done(IJob job);
+
+        /// <summary>
+        /// Frees up the job so it can be taken again.
+        /// </summary>
+        /// <param name="job">The job object</param>
         void GiveBack(IJob job);
     }
 }
