d6e01caf6bfbdb04efc25aa241c758a10f4a4b3e
Piedone <piedone@pyrocenter.hu>
2012-11-06 23:54:21 +0100

    Atomic job queue documentation

--- Libraries/Tasks/Jobs/IAtomicJobExecutor.cs
+++ Libraries/Tasks/Jobs/IAtomicJobExecutor.cs
@@ -6,8 +6,15 @@ using Orchard;
 
 namespace Piedone.HelpfulLibraries.Tasks.Jobs
 {
+    /// <summary>
+    /// Interface for types capable of executing atomic jobs
+    /// </summary>
     public interface IAtomicJobExecutor : IDependency
     {
+        /// <summary>
+        /// Works on the job
+        /// </summary>
+        /// <param name="job">The job object</param>
         void Run(IJob job);
     }
 }
--- Libraries/Tasks/Jobs/IAtomicJobQueue.cs
+++ Libraries/Tasks/Jobs/IAtomicJobQueue.cs
@@ -6,8 +6,16 @@ using Orchard;
 
 namespace Piedone.HelpfulLibraries.Tasks.Jobs
 {
+    /// <summary>
+    /// Queues job executors to be run in an independent work context and transaction
+    /// </summary>
     public interface IAtomicJobQueue : IDependency
     {
+        /// <summary>
+        /// Queues an executor to work on a job from the specified industry in an independent work context and transaction
+        /// </summary>
+        /// <typeparam name="TAtomicJobExecutor">The type executing the job</typeparam>
+        /// <param name="industry">"Industry", i.e. type, group of the job</param>
         void Queue<TAtomicJobExecutor>(string industry) where TAtomicJobExecutor : IAtomicJobExecutor;
     }
 }
