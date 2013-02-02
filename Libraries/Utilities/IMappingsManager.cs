b5f711e9559f90b7cdede7182f60140eef769d0c
Piedone <piedone@pyrocenter.hu>
2013-02-02 12:33:12 +0100

    Adding MappingsManager

---
+++ Libraries/Utilities/IMappingsManager.cs
@@ -0,0 +1,19 @@
+﻿using Orchard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Piedone.HelpfulLibraries.Libraries.Utilities
+{
+    /// <summary>
+    /// Service for managing ORM mappings
+    /// </summary>
+    public interface IMappingsManager : IDependency
+    {
+        /// <summary>
+        /// Clears cached mappings and thus forces the regeneration of the cache
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ Libraries/Utilities/MappingsManager.cs
@@ -0,0 +1,30 @@
+﻿using Orchard.Environment.Configuration;
+using Orchard.Environment.Extensions;
+using Orchard.FileSystems.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Piedone.HelpfulLibraries.Libraries.Utilities
+{
+    [OrchardFeature("Piedone.HelpfulLibraries.Utilities")]
+    public class MappingsManager : IMappingsManager
+    {
+        private readonly ShellSettings _shellSettings;
+        private readonly IAppDataFolder _appDataFolder;
+
+
+        public MappingsManager(ShellSettings shellSettings, IAppDataFolder appDataFolder)
+        {
+            _shellSettings = shellSettings;
+            _appDataFolder = appDataFolder;
+        }
+
+
+        public void Clear()
+        {
+            _appDataFolder.DeleteFile(_appDataFolder.Combine("Sites", _shellSettings.Name, "mappings.bin"));
+        }
+    }
+}
