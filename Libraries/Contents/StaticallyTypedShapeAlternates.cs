aa603ed0dba47c76cf57bb17b5a65934f736f2b4
Piedone <piedone@pyrocenter.hu>
2013-02-22 18:37:44 +0100

    Adding alternates for statically typed shapes (although alternates are added, they don't work yet...)

---
+++ Libraries/Contents/StaticallyTypedShapeAlternates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.DisplayManagement.Implementation;
+using Orchard.Environment.Extensions;
+
+namespace Piedone.HelpfulLibraries.Contents
+{
+    [OrchardFeature("Piedone.HelpfulLibraries.Contents")]
+    public class StaticallyTypedShapeAlternates : IShapeDisplayEvents
+    {
+        public void Displaying(ShapeDisplayingContext context)
+        {
+            var shape = context.Shape;
+            if (shape.TemplateName != null)
+            {
+                var alternates = shape.Metadata.Alternates;
+                var fileName = Path.GetFileName((string)shape.TemplateName);
+
+                // E.g. for EditorTemplates/Parts.Title.TitlePart this will create an alternate with the same path.
+                var fullPath = shape.Metadata.Type + "s/" + shape.TemplateName;
+                alternates.Add(fullPath);
+                alternates.Add("DisplayTemplate-" + fileName);
+
+                if (shape.ContentItem != null)
+                {
+                    var contentType = ((ContentItem)shape.ContentItem).ContentType;
+                    alternates.Add(fullPath + "-" + contentType);
+                    alternates.Add("DisplayTemplate-" + fileName + "-" + contentType);
+                }
+            }
+        }
+
+        public void Displayed(ShapeDisplayedContext context)
+        {
+        }
+    }
+}
