49619fef57686a2bbc2ed187004a932934f302fc
Piedone <piedone@pyrocenter.hu>
2013-04-15 20:12:09 +0200

    Adapting LockingCacheManager to TimeSpan timeout

--- Libraries/Tasks/ILockingCacheManager.cs
+++ Libraries/Tasks/ILockingCacheManager.cs
@@ -18,8 +18,25 @@ namespace Piedone.HelpfulLibraries.Tasks
         /// <param name="key">Key for the cache entry</param>
         /// <param name="acquire">Delegate for acquiring the cache entry</param>
         /// <param name="fallback">If the lock couldn't be acquired and the process timed out, this delegate will be run</param>
-        /// <param name="millisecondsTimeout">Milliseconds to wait for the lock before timing out</param>
+        /// <param name="timeout">Time span to wait for the lock before timing out</param>
         /// <returns>Result of either the acquire or the fallback delegate</returns>
-        TResult Get<TResult>(string key, Func<AcquireContext<string>, TResult> acquire, Func<TResult> fallback, int millisecondsTimeout = 4000);
+        TResult Get<TResult>(string key, Func<AcquireContext<string>, TResult> acquire, Func<TResult> fallback, TimeSpan timeout);
+    }
+
+    public static class LockingCacheManageExtensions
+    {
+        /// <summary>
+        /// Tries to get the cache entry, if none is present, tries to acquire a lock on the entry so any other process simultaneously requesting the entry
+        /// has to wait.
+        /// </summary>
+        /// <typeparam name="TResult">Type of the computation's result</typeparam>
+        /// <param name="key">Key for the cache entry</param>
+        /// <param name="acquire">Delegate for acquiring the cache entry</param>
+        /// <param name="fallback">If the lock couldn't be acquired and the process timed out, this delegate will be run</param>
+        /// <returns>Result of either the acquire or the fallback delegate</returns>
+        public static TResult Get<TResult>(this ILockingCacheManager lockingCacheManager, string key, Func<AcquireContext<string>, TResult> acquire, Func<TResult> fallback)
+        {
+            return lockingCacheManager.Get(key, acquire, fallback, new TimeSpan(0, 0, 4));
+        }
     }
 }
--- Libraries/Tasks/LockingCacheManager.cs
+++ Libraries/Tasks/LockingCacheManager.cs
@@ -18,7 +18,7 @@ namespace Piedone.HelpfulLibraries.Tasks
             _lockFileManager = lockFileManager;
         }
 
-        public TResult Get<TResult>(string key, Func<AcquireContext<string>, TResult> acquire, Func<TResult> fallback, int millisecondsTimeout = 4000)
+        public TResult Get<TResult>(string key, Func<AcquireContext<string>, TResult> acquire, Func<TResult> fallback, TimeSpan timeout)
         {
             // When using with arbitrary types, key.ToString() could lead to errors if the key is not string or the ToString() is not properly implemented.
             // That's why we only allow string keys here.
@@ -27,7 +27,7 @@ namespace Piedone.HelpfulLibraries.Tasks
             {
                 return _cacheManager.Get(key, ctx =>
                     {
-                        using (var lockFile = _lockFileManager.AcquireLock(key, millisecondsTimeout))
+                        using (var lockFile = _lockFileManager.AcquireLock(key, timeout))
                         {
                             // If we waited for the lock to be released, here the result computed by the locking code should be returned.
                             return _cacheManager.Get(key, acquire);
