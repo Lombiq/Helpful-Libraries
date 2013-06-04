02e0e8059397b5d957a8b6847498b2f18fb44c0b
Piedone <piedone@pyrocenter.hu>
2013-06-05 01:06:53 +0200

    Adding documentation to KVSTore
    --HG--
    branch : Orchard 1.7

--- Libraries/KeyValueStore/IKeyValueStore.cs
+++ Libraries/KeyValueStore/IKeyValueStore.cs
@@ -6,11 +6,30 @@ using Orchard;
 
 namespace Piedone.HelpfulLibraries.KeyValueStore
 {
+    /// <summary>
+    /// A simple persistent key-value store
+    /// </summary>
     public interface IKeyValueStore : IDependency
     {
+        /// <summary>
+        /// Checks if an entry with the specified key exists
+        /// </summary>
         bool Exists(string key);
+
+        /// <summary>
+        /// Sets the value of an entry with the specified key. If the entry doesn't exists it creates it, if it exists, overwrites it.
+        /// </summary>
         void Set(string key, object value);
+
+        /// <summary>
+        /// Gets the value of the entry with the specified key
+        /// </summary>
+        /// <typeparam name="T">Type of the value that was saved</typeparam>
         T Get<T>(string key);
+
+        /// <summary>
+        /// Removes the entry with the specified key
+        /// </summary>
         void Remove(string key);
     }
 }
