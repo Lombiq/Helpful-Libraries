795c5b68435816a99c6d008d2a93590005bd629c
Piedone <piedone@pyrocenter.hu>
2013-07-18 12:26:51 +0200

    Lock files are now stored in a hidden media folder

--- Libraries/Tasks/WellKnownConstants.cs
+++ Libraries/Tasks/WellKnownConstants.cs
@@ -3,6 +3,6 @@ namespace Piedone.HelpfulLibraries.Tasks
 {
     public static class WellKnownConstants
     {
-        public const string LockFileFolder = "Modules/Piedone/HelpfulLibraries/Tasks/LockFiles/";
+        public const string LockFileFolder = "_PiedoneModules/HelpfulLibraries/Tasks/LockFiles/";
     }
 }
