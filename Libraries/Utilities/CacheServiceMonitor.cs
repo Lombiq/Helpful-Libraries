c8e4857740b96e3db34afd75357b0a1045c0838d
Piedone <piedone@pyrocenter.hu>
2013-11-27 20:47:42 +0100

    Utilities: new CacheFileServiceMonitor Contribution of Lombiq Technologies (http://lombiq.com/)
    --HG--
    branch : Orchard 1.8

---
+++ Libraries/Utilities/CacheServiceMonitor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard.Caching.Services;
+using Orchard.Environment.Extensions;
+
+namespace Piedone.HelpfulLibraries.Utilities
+{
+    /// <summary>
+    /// Lets you subscribe ICacheService cache keys to events. If the event is triggered all the entries for the subscribed keys will
+    /// be removed.
+    /// </summary>
+    [OrchardFeature("Piedone.HelpfulLibraries.Utilities")]
+    public static class CacheServiceMonitor
+    {
+        private const string KeyChainCacheKey = "Piedone.HelpfulLibraries.Utilities.CacheServiceMonitor.KeyChain.";
+
+
+        /// <summary>
+        /// Subscribe a cache key to an event. If the event is triggered the entry for the cache key will be removed.
+        /// </summary>
+        /// <param name="eventKey">The key of the event that can be triggered.</param>
+        /// <param name="cacheKey">The key of the cache entry.</param>
+        public static void Monitor(this ICacheService cacheService, string eventKey, string cacheKey)
+        {
+            cacheService.GetKeys(eventKey).GetOrAdd(cacheKey, 0);
+        }
+
+        /// <summary>
+        /// Removes all cache entries that were subscribed to the specified event.
+        /// </summary>
+        /// <param name="eventKey">The key of the event that will be triggered.</param>
+        public static void Trigger(this ICacheService cacheService, string eventKey)
+        {
+            foreach (var cacheKey in cacheService.GetKeys(eventKey).Keys.ToList())
+            {
+                cacheService.Remove(cacheKey);
+            }
+        }
+
+
+        private static ConcurrentDictionary<string, byte> GetKeys(this ICacheService cacheService, string eventKey)
+        {
+            return cacheService.Get(KeyChainCacheKey + eventKey, () =>
+            {
+                return new ConcurrentDictionary<string, byte>();
+            });
+        }
+    }
+}
