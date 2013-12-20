c798ca94c04cad95fd852ec26fc7026bf30a9b65
Piedone <piedone@pyrocenter.hu>
2013-12-20 19:01:00 +0100

    ConfigurationAccessor service for accessing ConfigurationManager settings in an injectable way
    --HG--
    branch : Orchard 1.8

---
+++ Libraries/Utilities/ConfigurationAccessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard;
+using Orchard.Environment.Extensions;
+
+namespace Piedone.HelpfulLibraries.Utilities
+{
+    /// <summary>
+    /// Can be used to access the <see cref="System.Configuration.ConfigurationManager">ConfigurationManager</see> collections.
+    /// </summary>
+    /// <remarks>
+    /// As an injectable dependency this can be easily mocked or stubbed, on contrary to the default ConfigurationManager static class.
+    /// </remarks>
+    public interface IConfigurationAccessor : ISingletonDependency
+    {
+        NameValueCollection AppSettings { get; }
+        ConnectionStringSettingsCollection ConnectionStrings { get; }
+    }
+
+
+    [OrchardFeature("Piedone.HelpfulLibraries.Utilities")]
+    public class ConfigurationAccessor : IConfigurationAccessor
+    {
+        public NameValueCollection AppSettings
+        {
+            get { return ConfigurationManager.AppSettings; }
+        }
+
+        public ConnectionStringSettingsCollection ConnectionStrings
+        {
+            get { return ConfigurationManager.ConnectionStrings; }
+        }
+    }
+}
