b6dd474f6eceaa239d63ecb04afeb190ba164bab
Piedone <piedone@pyrocenter.hu>
2014-02-20 15:25:10 +0100

    Better Uri utilities Contribution of Lombiq Technologies (http://lombiq.com/)
    --HG--
    branch : Orchard 1.8

--- Libraries/Utilities/UriExtensions.cs
+++ Libraries/Utilities/UriExtensions.cs
@@ -12,7 +12,10 @@ namespace Piedone.HelpfulLibraries.Utilities
         public static string ToStringWithoutScheme(this Uri uri)
         {
             if (!uri.IsAbsoluteUri) return uri.ToString();
-            return "//" + uri.Host + uri.PathAndQuery;
+
+            // Include the port if it's a non-default one.
+            var portString = uri.Scheme == "http" && uri.Port == 80 || uri.Scheme == "https" && uri.Port == 443 ? string.Empty : ":" + uri.Port.ToString();
+            return "//" + uri.Host + portString + uri.PathAndQuery;
         }
     }
 }
