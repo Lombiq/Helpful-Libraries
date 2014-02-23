84308a93b1ba592a28b739975572a1b3e45e3333
Piedone <piedone@pyrocenter.hu>
2014-02-24 00:35:32 +0100

    Adding injectable dependency for Localizer Contribution of Lombiq Technologies (http://lombiq.com/)
    --HG--
    branch : Orchard 1.8

---
+++ Libraries/Utilities/LocalizerDependency.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard;
+using Orchard.Localization;
+
+namespace Piedone.HelpfulLibraries.Libraries.Utilities
+{
+    /// <summary>
+    /// A simple container for the Localizer delegate. Resolve this interface from the DI container if you want to resolve a Localizer
+    /// in-place instead of through property injection.
+    /// </summary>
+    public interface ILocalizerDependency : IDependency
+    {
+        Localizer T { get; }
+    }
+
+
+    public class LocalizerDependency : ILocalizerDependency
+    {
+        public Localizer T { get; set; }
+
+
+        public LocalizerDependency()
+        {
+            T = NullLocalizer.Instance;
+        }
+    }
+}
