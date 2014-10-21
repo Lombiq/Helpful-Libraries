0520d3479fc58a00c5db580f5710ea2281e18e18
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-10-21 14:40:53 +0200

    Implementing transient context for content objects

---
+++ Libraries/Contents/TransientContextPart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard.ContentManagement;
+
+namespace Piedone.HelpfulLibraries.Contents
+{
+    public class TransientContextPart : ContentPart
+    {
+        internal Dictionary<string, object> Context { get; set; }
+
+
+        public TransientContextPart()
+        {
+            Context = new Dictionary<string, object>();
+        }
+    }
+}
