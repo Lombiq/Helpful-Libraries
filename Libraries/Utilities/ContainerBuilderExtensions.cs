9dfd41ea7913d89ff1411a3bcce4775b2ccb50e9
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-05-12 23:27:20 +0200

    Code styling
    --HG--
    branch : dev

--- Libraries/Utilities/ContainerBuilderExtensions.cs
+++ Libraries/Utilities/ContainerBuilderExtensions.cs
@@ -6,12 +6,12 @@ using Piedone.HelpfulLibraries.Libraries.Utilities;
 
 namespace Autofac
 {
-    /* Custom ILifetimeScope builder to register the placeholder IHttpContextAccessor implementation. This is necessary because
-     * IHttpContextAccessor is an application-wide dependency, not even a shell singleton. This would cause all types of failures
-     * on other shells when calling IHttpContextAccessor.Set() (e.g. to work around work context being lost because of async
-     * or building a new WorkContextScope), since that would set the HttpContext everywhere.
-     * This could be much better if this is fixed: https://github.com/OrchardCMS/Orchard/issues/4338 as now we need a stub HttpContext
-     * to carry the work context.
+    /* Custom ILifetimeScope builder to register the placeholder IHttpContextAccessor implementation. This is necessary 
+     * because IHttpContextAccessor is an application-wide dependency, not even a shell singleton. This would cause all 
+     * types of failures on other shells when calling IHttpContextAccessor.Set() (e.g. to work around work context being 
+     * lost because of async or building a new WorkContextScope), since that would set the HttpContext everywhere.
+     * This could be much better if this is fixed: https://github.com/OrchardCMS/Orchard/issues/4338 as now we need a 
+     * stub HttpContext to carry the work context.
      * See also: http://stackoverflow.com/questions/8658946/autofac-lifetimes-and-the-default-provider-within-a-matching-lifetime-scope */
     [OrchardFeature("Piedone.HelpfulLibraries.Utilities")]
     public static class ContainerBuilderExtensions
