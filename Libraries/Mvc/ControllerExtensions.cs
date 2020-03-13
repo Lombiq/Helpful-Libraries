8662099253de90fc97167e0b5aacd8b5442ecc13
Márk Bartha <mark.bartha@lombiq.com>
2020-03-13 11:21:38 +0100

    Adding Controller extensions
    --HG--
    branch : issue/OSOCE-27

---
+++ Libraries/Mvc/ControllerExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.AspNetCore.Mvc
+{
+    public static class ControllerExtensions
+    {
+        public static ActionResult RedirectLocal(this Controller controller, string redirectUrl) =>
+            new RedirectResult(controller.Url.IsLocalUrl(redirectUrl) ? redirectUrl : "~/");
+    }
+}
