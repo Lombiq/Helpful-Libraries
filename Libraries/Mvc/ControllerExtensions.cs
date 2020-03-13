33de71888271a2c11b5b537417635d9e7ff6b017
Márk Bartha <mark.bartha@lombiq.com>
2020-03-13 12:40:45 +0100

    Renaming and documenting ControllerExtensions
    --HG--
    branch : issue/OSOCE-27

--- Libraries/Mvc/ControllerExtensions.cs
+++ Libraries/Mvc/ControllerExtensions.cs
@@ -2,7 +2,21 @@
 {
     public static class ControllerExtensions
     {
-        public static ActionResult RedirectLocal(this Controller controller, string redirectUrl) =>
-            new RedirectResult(controller.Url.IsLocalUrl(redirectUrl) ? redirectUrl : "~/");
+        /// <summary>
+        /// Will redirect to the given URL if that is local. Otherwise it will redirect to "~/".
+        /// </summary>
+        /// <param name="redirectUrl">Local URL to redirect to.</param>
+        /// <returns>Redirect action result.</returns>
+        public static ActionResult RedirectToLocal(this Controller controller, string redirectUrl)
+        {
+            if (controller.Url.IsLocalUrl(redirectUrl))
+            {
+                return controller.Redirect(redirectUrl);
+            }
+            else
+            {
+                return controller.Redirect("~/");
+            }
+        }
     }
 }
