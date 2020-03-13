a576508f8dd877d2269a2a015a26ca7cf600d085
Márk Bartha <mark.bartha@lombiq.com>
2020-03-13 12:39:33 +0100

    Adding ContentDefinitionManagerExtensions
    --HG--
    branch : issue/OSOCE-27

---
+++ Libraries/Contents/ContentDefinitionManagerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace OrchardCore.ContentManagement.Metadata
+{
+    public static class ContentDefinitionManagerExtensions
+    {
+        public static T GetContentPartSettings<T>(
+            this IContentDefinitionManager contentDefinitionManager,
+            string contentType,
+            string contentPart) where T : new()
+        {
+            var contentTypeDefinition = contentDefinitionManager.GetTypeDefinition(contentType);
+            var contentTypePartDefinition = contentTypeDefinition.Parts
+                .FirstOrDefault(x => string.Equals(x.PartDefinition.Name, contentPart, StringComparison.Ordinal));
+
+            return contentTypePartDefinition.GetSettings<T>();
+        }
+    }
+}
