1d375bec6bb1487001e9b25772fd9aab8fcf0cb2
Márk Bartha <mark.bartha@lombiq.com>
2020-05-06 17:19:01 +0200

    Adding ContentListExtensions

---
+++ Libraries/Contents/ContentListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OrchardCore.ContentManagement
+{
+    public static class ContentListExtensions
+    {
+        /// <summary>
+        /// Gets a content part list by its type.
+        /// </summary>
+        /// <returns>The content part list or empty list if it doesn't exist.</returns>
+        public static IEnumerable<TPart> As<TPart>(this IEnumerable<IContent> contents) where TPart : ContentPart
+        {
+            var list = contents?.Select(content => content.As<TPart>()) ?? Enumerable.Empty<TPart>();
+
+            return list.Any(content => content == null) ? Enumerable.Empty<TPart>() : list;
+        }
+    }
+}
