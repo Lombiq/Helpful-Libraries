eb3614d8e043d9782171233c4b488d7db9810b76
Márk Bartha <mark.bartha@lombiq.com>
2020-08-27 15:26:57 +0200

    Adding NumberExtensions to utilities

---
+++ Libraries/Utilities/NumberExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    public static class NumberExtensions
+    {
+        public static string ToTechnicalString(this int number) => number.ToString(CultureInfo.InvariantCulture);
+    }
+}
