d6bbc37c2ecb411848a02b2e2811f837299cc6c7
Márk Bartha <mark.bartha@lombiq.com>
2020-08-31 12:57:55 +0200

    Adding GetOrchardUserAsync extension method

---
+++ Libraries/Utilities/UserServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using OrchardCore.Users.Models;
+
+namespace OrchardCore.Users.Services
+{
+    public static class UserServiceExtensions
+    {
+        public static async Task<User> GetOrchardUserAsync(this IUserService userService, string userName)
+        {
+            var user = await userService.GetUserAsync(userName);
+            if (user == null) return null;
+
+            var orchardUser = user as User;
+            if (orchardUser == null)
+                throw new ArgumentException("The given username identifies a non-Orchard user.", nameof(userName));
+
+            return orchardUser;
+        }
+    }
+}
