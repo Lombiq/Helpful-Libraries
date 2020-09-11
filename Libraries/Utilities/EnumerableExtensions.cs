bbcfbba4051d5bf4cbd9c6493b781176224bf3b6
Dávid El-Saig <david.el-saig@lombiq.com>
2020-09-11 13:32:28 +0200

    Added AwaitEach extension (select + sequential await).

---
+++ Libraries/Utilities/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace System.Collections.Generic
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Awaits the tasks sequentially. An alternative to
+        /// <see cref="Task.WhenAll(System.Collections.Generic.IEnumerable{System.Threading.Tasks.Task})"/> and
+        /// <code>Nito.AsyncEx.TaskExtensions.WhenAll</code> when true multi-threaded asynchronicity is not desirable.
+        /// </summary>
+        /// <param name="source">A collection of items.</param>
+        /// <param name="asyncOperation">An <code>async</code> function to call on each item.</param>
+        /// <typeparam name="TItem">The type of the input collection's items.</typeparam>
+        /// <typeparam name="TResult">The type of the output collection's items.</typeparam>
+        /// <returns>When awaited the task contains the results which were added one-by-one.</returns>
+        public static async Task<IList<TResult>> AwaitEach<TItem, TResult>(
+            this IEnumerable<TItem> source,
+            Func<TItem, Task<TResult>> asyncOperation)
+        {
+            var results = new List<TResult>();
+            foreach (var item in source) results.Add(await asyncOperation(item));
+            return results;
+        }
+    }
+}
