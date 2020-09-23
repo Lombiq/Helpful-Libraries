1c0a3f6eeefb8e09c091edfbd952c92b97240bd0
Dávid El-Saig <david.el-saig@lombiq.com>
2020-09-23 13:20:36 +0200

    Documentation formatting.

--- Libraries/Utilities/EnumerableExtensions.cs
+++ Libraries/Utilities/EnumerableExtensions.cs
@@ -8,10 +8,10 @@ namespace System.Collections.Generic
         /// <summary>
         /// Awaits the tasks sequentially. An alternative to
         /// <see cref="Task.WhenAll(System.Collections.Generic.IEnumerable{System.Threading.Tasks.Task})"/> and
-        /// Nito.AsyncEx.TaskExtensions.WhenAll when true multi-threaded asynchronicity is not desirable.
+        /// <c>Nito.AsyncEx.TaskExtensions.WhenAll</c> when true multi-threaded asynchronicity is not desirable.
         /// </summary>
         /// <param name="source">A collection of items.</param>
-        /// <param name="asyncOperation">An async function to call on each item.</param>
+        /// <param name="asyncOperation">An <see langword="async"/> function to call on each item.</param>
         /// <typeparam name="TItem">The type of the input collection's items.</typeparam>
         /// <typeparam name="TResult">The type of the output collection's items.</typeparam>
         /// <returns>When awaited the task contains the results which were added one-by-one.</returns>
