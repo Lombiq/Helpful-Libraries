fa46d99f18b37362be9c1e83bcaf627fe88ce734
Dávid El-Saig <david.el-saig@lombiq.com>
2020-09-23 19:26:30 +0200

    Added documentation.

--- Libraries/Utilities/EnumerableExtensions.cs
+++ Libraries/Utilities/EnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace System.Collections.Generic
@@ -23,6 +24,13 @@ namespace System.Collections.Generic
             return results;
         }
 
+        /// <summary>
+        /// Attempts to cast <paramref name="collection"/> into <see cref="List{T}"/>. If that's not possible then
+        /// converts it into one. Not to be confused with <see cref="Enumerable.ToList{TSource}"/> that always creates a
+        /// separate <see cref="List{T}"/> regardless of source type. This extension is more suitable when the
+        /// <paramref name="collection"/> is expected to be <see cref="List{T}"/> but has to be stored as
+        /// <see cref="IEnumerable{T}"/>.
+        /// </summary>
         public static IList<T> AsList<T>(this IEnumerable<T> collection) =>
             collection is IList<T> list ? list : new List<T>(collection);
     }
