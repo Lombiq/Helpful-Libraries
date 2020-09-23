51a802d5d194ca1e0907c2214a9d1131058cd5fb
Dávid El-Saig <david.el-saig@lombiq.com>
2020-09-23 22:56:23 +0200

    Code cleanup.

---
+++ Libraries/Utilities/EnumExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace System
+{
+    public static class EnumExtensions
+    {
+        public static InvalidOperationException UnknownEnumException<T>(this T other)
+            where T : Enum =>
+            new InvalidOperationException($"Unknown {other.GetType().Name}: '{other}'");
+    }
+}
