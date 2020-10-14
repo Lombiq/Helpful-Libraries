bffec03af885769b3ce66743dceca603d4ea4116
Dávid El-Saig <david.el-saig@lombiq.com>
2020-10-14 02:23:24 +0200

    Move MockOrchardServices to HL.

---
+++ Libraries/DependencyInjection/OrchardServicesExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Lombiq.HelpfulLibraries.Libraries.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using OrchardCore.ContentManagement;
+using OrchardCore.Modules;
+using OrchardCore.Settings;
+using System;
+
+namespace Moq.AutoMock
+{
+    public static class OrchardServicesExtensions
+    {
+        public static void MockOrchardServices<T>(this AutoMocker mocker) =>
+            mocker.Use<IOrchardServices<T>>(
+                new OrchardServices<T>(
+                    new Lazy<IClock>(mocker.Get<IClock>()),
+                    new Lazy<IContentAliasManager>(mocker.Get<IContentAliasManager>()),
+                    new Lazy<IContentManager>(mocker.Get<IContentManager>()),
+                    new Lazy<IHttpContextAccessor>(mocker.Get<IHttpContextAccessor>()),
+                    new Lazy<ILogger<T>>(mocker.Get<ILogger<T>>()),
+                    new Lazy<YesSql.ISession>(mocker.Get<YesSql.ISession>()),
+                    new Lazy<ISiteService>(mocker.Get<ISiteService>())));
+    }
+}
