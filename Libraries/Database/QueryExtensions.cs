9ec802d66ec1e0a1d85478996d44ecde96c3aa32
Bence Puschmann <bence.puschmann@lombiq.com>
2020-12-31 09:46:08 +0100

    Format fixing

--- Libraries/Database/QueryExtensions.cs
+++ Libraries/Database/QueryExtensions.cs
@@ -41,18 +41,18 @@ namespace YesSql
             PaginateAsync(query, pageIndex, count).ContinueWith(t => t.Result.As<TPart>().Where(part => part != null), TaskScheduler.Default);
 
         /// <summary>
-        /// Breaks the query up into pages and lists the page using the given zero-based index.If pageIndex is 0 and
+        /// Breaks the query up into pages and lists the page. If skip is 0 and
         /// count is <see cref="int.MaxValue"/> then the whole query is listed.
         /// </summary>
         /// <param name="query">The query to paginate.</param>
         /// <param name="skip">Number of items to skip. Can be null.</param>
         /// <param name="count">The page size. Can be null.</param>
-        /// <returns>The desired page of the resulting <see cref="ContentItem"/>s converted into the desired ContentPart. Those
-        /// that don't have it are discarded.</returns>
+        /// <returns>The desired page of the resulting <see cref="ContentItem"/>s.</returns>
         public static Task<IEnumerable<ContentItem>> SliceAsync(this IQuery<ContentItem> query, int? skip = 0, int? count = int.MaxValue)
         {
             if (skip is not null and > 0) query = query.Skip(skip.Value);
             if (count is not null and > 0) query = query.Take(count.Value);
+
             return query.ListAsync();
         }
     }
