fc5924dc4572a4c27cb10656680b9b60e227c94d
Márk Bartha <mark.bartha@lombiq.com>
2021-01-26 16:46:56 +0100

    Adding JsonHelpers.AlterDeep helper

--- Libraries/Utilities/JsonHelpers.cs
+++ Libraries/Utilities/JsonHelpers.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 
 namespace Lombiq.HelpfulLibraries.Libraries.Utilities
 {
@@ -21,6 +22,36 @@ namespace Lombiq.HelpfulLibraries.Libraries.Utilities
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Alters a <see cref="JObject"/> by iterating through all their inner JObject nodes deeply and executing the
+        /// provided alter operation on it.
+        /// </summary>
+        /// <param name="jObject"><see cref="JObject"/> to alter.</param>
+        /// <param name="alter">Operation that alters a deep <see cref="JObject"/> node.</param>
+        public static void AlterDeep(JObject jObject, Action<JObject> alter)
+        {
+            alter(jObject);
+
+            foreach (var (_, value) in jObject)
+            {
+                if (value is JObject innerObject)
+                {
+                    AlterDeep(innerObject, alter);
+                }
+
+                if (value is JArray innerArray)
+                {
+                    foreach (var innerToken in innerArray)
+                    {
+                        if (innerToken is JObject innerTokenObject)
+                        {
+                            AlterDeep(innerTokenObject, alter);
+                        }
+                    }
+                }
+            }
         }
     }
 }
