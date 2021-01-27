5aa7b25106e5caf26ab916f261b7dd493cbf08bc
Márk Bartha <mark.bartha@lombiq.com>
2021-01-28 00:23:41 +0100

    ZipFile helper accepts streams instead of byte arrays

--- Libraries/Mvc/ActionResultHelpers.cs
+++ Libraries/Mvc/ActionResultHelpers.cs
@@ -12,21 +12,20 @@ namespace Lombiq.HelpfulLibraries.Libraries.Mvc
         /// <summary>
         /// Compresses the <paramref name="files"/> into a zip archive ready to be served by a controller.
         /// </summary>
-        /// <param name="files">The file collection where key is the zip entry file name and value is the data.</param>
+        /// <param name="files">The file collection where key is the zip entry file name and value is the data stream.</param>
         /// <param name="zipFileName">The filename of the archive, can be with or without the ".zip" at the end.</param>
-        public static FileResult ZipFile(IDictionary<string, byte[]> files, string zipFileName)
+        public static FileResult ZipFile(IDictionary<string, Stream> files, string zipFileName)
         {
-            // The MemoryStream gets disposed by the FileStreamResult. see: https://stackoverflow.com/a/51655063
             using var outStream = new MemoryStream();
 
             using (var archive = new ZipArchive(outStream, ZipArchiveMode.Create, leaveOpen: true))
             {
-                foreach ((string fileName, byte[] fileBytes) in files)
+                foreach ((string fileName, var fileStream) in files)
                 {
                     var entry = archive.CreateEntry(fileName, CompressionLevel.Optimal);
                     using var entryStream = entry.Open();
-                    using var fileToCompressStream = new MemoryStream(fileBytes);
-                    fileToCompressStream.CopyTo(entryStream);
+                    fileStream.CopyTo(entryStream);
+                    fileStream.Dispose();
                 }
             }
 
