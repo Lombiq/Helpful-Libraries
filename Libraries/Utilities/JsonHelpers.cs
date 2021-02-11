4ee23d60eb29ffcc41ece4f6090d5d19c82330f4
Dávid El-Saig <david.el-saig@lombiq.com>
2021-02-11 18:57:43 +0100

    Added string.JsonHtmlContent() extension.

--- Libraries/Utilities/JsonHelpers.cs
+++ Libraries/Utilities/JsonHelpers.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Html;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -22,5 +23,12 @@ namespace Lombiq.HelpfulLibraries.Libraries.Utilities
 
             return true;
         }
+
+        /// <summary>
+        /// Returns a raw HTML string that's been JSON serialized and therefore safe to use within <c>&lt;script&gt;</c>
+        /// tags in a Razor view.
+        /// </summary>
+        public static IHtmlContent JsonHtmlContent(this string htmlString) =>
+            new HtmlString(JsonConvert.SerializeObject(htmlString));
     }
 }
