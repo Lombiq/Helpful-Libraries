3193eb75c3cfdee132fa6afd7006101e73e0263e
Dávid El-Saig <david.el-saig@lombiq.com>
2021-02-17 18:01:14 +0100

    Added ResourceHelper.

---
+++ Libraries/ResourceManagement/ResourceHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.IO;
+using System.Reflection;
+
+namespace Lombiq.HelpfulLibraries.Libraries.ResourceManagement
+{
+    public static class ResourceHelper
+    {
+        /// <summary>
+        /// Returns the contents of an embedded text file in a given assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly the file is embedded into.</param>
+        /// <param name="path">The relative path inside the assembly's project.</param>
+        public static string GetFile(Assembly assembly, string path)
+        {
+            var provider = new EmbeddedFileProvider(assembly);
+            using var stream = provider.GetFileInfo(path.Replace('/', '.')).CreateReadStream();
+            using var streamReader = new StreamReader(stream);
+            return streamReader.ReadToEnd();
+        }
+
+        /// <summary>
+        /// Returns the contents of an embedded text file inside the same assembly as the given type. The embedded path
+        /// is "Resources/{typeName}.{extension}".
+        /// </summary>
+        /// <typeparam name="T">A type defined in the same assembly where the embedded resource is.</typeparam>
+        /// <param name="extension">The extension of the target file.</param>
+        public static string GetFile<T>(string extension) =>
+            GetFile(typeof(T).Assembly, $"Resources/{typeof(T).Name}.{extension}");
+    }
+}
