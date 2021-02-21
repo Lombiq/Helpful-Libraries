3fd35040c2c2d3d5fee44e7f4e9ff859dc4514aa
Dávid El-Saig <david.el-saig@lombiq.com>
2021-02-21 21:22:47 +0100

    Added UrlHelperExtensions.EditContentItemWithTab method.

---
+++ Libraries/Mvc/UrlHelperExtensions.cs
@@ -0,0 +1,27 @@
+﻿using OrchardCore.ContentManagement;
+using OrchardCore.Mvc.Core.Utilities;
+using OrchardCore.Queries.Controllers;
+
+namespace Microsoft.AspNetCore.Mvc.Routing
+{
+    public static class UrlHelperExtensions
+    {
+        /// <summary>
+        /// Returns a relative URL for the editor action of <paramref name="content"/> with the tab of
+        /// <paramref name="tabIdPart"/> selected.
+        /// </summary>
+        public static string EditContentItemWithTab(this IUrlHelper helper, string tabIdPart, IContent content)
+        {
+            var url = helper.Action(
+                nameof(AdminController.Edit),
+                typeof(AdminController).ControllerName(),
+                new
+                {
+                    area = "OrchardCore.Contents",
+                    content.ContentItem.ContentItemId,
+                });
+
+            return $"{url}#{tabIdPart}-{content.ContentItem.ContentItemId}";
+        }
+    }
+}
