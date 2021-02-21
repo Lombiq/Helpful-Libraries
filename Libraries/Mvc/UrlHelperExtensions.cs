c26f3474ef283e35adafef20b842b1f417ce0d69
Dávid El-Saig <david.el-saig@lombiq.com>
2021-02-21 21:30:36 +0100

    Make the tabIdPart simpler.

--- Libraries/Mvc/UrlHelperExtensions.cs
+++ Libraries/Mvc/UrlHelperExtensions.cs
@@ -10,6 +10,10 @@ namespace Microsoft.AspNetCore.Mvc.Routing
         /// Returns a relative URL for the editor action of <paramref name="content"/> with the tab of
         /// <paramref name="tabIdPart"/> selected.
         /// </summary>
+        /// <param name="tabIdPart">
+        /// The part between the prefix and the content item ID. For example <c>users</c> when the hash is
+        /// <c>#tab-users-4csgbqx40dgsq5ckebyrsnptcj</c>
+        /// </param>
         public static string EditContentItemWithTab(this IUrlHelper helper, string tabIdPart, IContent content)
         {
             var url = helper.Action(
@@ -20,8 +24,9 @@ namespace Microsoft.AspNetCore.Mvc.Routing
                     area = "OrchardCore.Contents",
                     content.ContentItem.ContentItemId,
                 });
-
-            return $"{url}#{tabIdPart}-{content.ContentItem.ContentItemId}";
+#pragma warning disable CA1308 // Normalize strings to uppercase. This is not normalization, the ID is lowercase.
+            return $"{url}#tab-{tabIdPart.ToLowerInvariant()}-{content.ContentItem.ContentItemId}";
+#pragma warning enable CA1308 // Normalize strings to uppercase. This is not normalization, the ID is lowercase.
         }
     }
 }
