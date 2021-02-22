9c00a05d3404f6e5f4b2512452af1ddf3505dfbf
Dávid El-Saig <david.el-saig@lombiq.com>
2021-02-22 01:43:20 +0100

    Typos.

--- Libraries/Mvc/UrlHelperExtensions.cs
+++ Libraries/Mvc/UrlHelperExtensions.cs
@@ -11,8 +11,8 @@ namespace Microsoft.AspNetCore.Mvc.Routing
         /// <paramref name="tabIdPart"/> selected.
         /// </summary>
         /// <param name="tabIdPart">
-        /// The part between the prefix and the content item ID. For example <c>users</c> when the hash is
-        /// <c>#tab-users-4csgbqx40dgsq5ckebyrsnptcj</c>
+        /// The part between the prefix and the content item ID. For example when the hash is
+        /// <c>#tab-users-4csgbqx40dgsq5ckebyrsnptcj</c> it should be <c>users</c>.
         /// </param>
         public static string EditContentItemWithTab(this IUrlHelper helper, string tabIdPart, IContent content)
         {
@@ -26,7 +26,7 @@ namespace Microsoft.AspNetCore.Mvc.Routing
                 });
 #pragma warning disable CA1308 // Normalize strings to uppercase. This is not normalization, the ID is lowercase.
             return $"{url}#tab-{tabIdPart.ToLowerInvariant()}-{content.ContentItem.ContentItemId}";
-#pragma warning enable CA1308 // Normalize strings to uppercase. This is not normalization, the ID is lowercase.
+#pragma warning restore CA1308 // Normalize strings to uppercase. This is not normalization, the ID is lowercase.
         }
     }
 }
