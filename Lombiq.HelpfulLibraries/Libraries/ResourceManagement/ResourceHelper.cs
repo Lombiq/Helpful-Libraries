9a4ed9d1c39aed95b8939cdc7d16de523fbfdbaf
Dávid El-Saig <david.el-saig@lombiq.com>
2021-03-09 14:08:21 +0100

    Convert GetFile to GetFileAsync

--- Lombiq.HelpfulLibraries/Libraries/ResourceManagement/ResourceHelper.cs
+++ Lombiq.HelpfulLibraries/Libraries/ResourceManagement/ResourceHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.FileProviders;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Lombiq.HelpfulLibraries.Libraries.ResourceManagement
 {
@@ -11,14 +12,14 @@ namespace Lombiq.HelpfulLibraries.Libraries.ResourceManagement
         /// Returns the text content of the file located in the <paramref name="path"/> of the
         /// <paramref name="provider"/> or <see langword="null"/> if it doesn't exist.
         /// </summary>
-        public static string GetFile(IFileProvider provider, string path)
+        public static async Task<string> GetFileAsync(IFileProvider provider, string path)
         {
             var fileInfo = provider.GetDirectoryContents(string.Empty).SingleOrDefault(x => x.Name == path);
             if (fileInfo?.Exists != true) return null;
 
-            using var stream = fileInfo.CreateReadStream();
+            await using var stream = fileInfo.CreateReadStream();
             using var streamReader = new StreamReader(stream);
-            return streamReader.ReadToEnd();
+            return await streamReader.ReadToEndAsync();
         }
 
         /// <summary>
@@ -26,8 +27,8 @@ namespace Lombiq.HelpfulLibraries.Libraries.ResourceManagement
         /// <paramref name="provider"/> or <see langword="null" /> if it doesn't exist.
         /// </summary>
         /// <param name="extension">The extension of the target file.</param>
-        public static string GetFile<T>(IFileProvider provider, string extension) =>
-            GetFile(provider, GetTypeFilePath<T>(extension));
+        public static Task<string> GetFileAsync<T>(IFileProvider provider, string extension) =>
+            GetFileAsync(provider, GetTypeFilePath<T>(extension));
 
         /// <summary>
         /// Returns the contents of an embedded text file inside the same assembly as the given type. The embedded path
@@ -35,8 +36,8 @@ namespace Lombiq.HelpfulLibraries.Libraries.ResourceManagement
         /// </summary>
         /// <typeparam name="T">A type defined in the same assembly where the embedded resource is.</typeparam>
         /// <param name="extension">The extension of the target file.</param>
-        public static string GetEmbeddedFile<T>(string extension) =>
-            GetFile(new EmbeddedFileProvider(typeof(T).Assembly), GetTypeFilePath<T>(extension));
+        public static Task<string> GetEmbeddedFileAsync<T>(string extension) =>
+            GetFileAsync(new EmbeddedFileProvider(typeof(T).Assembly), GetTypeFilePath<T>(extension));
 
         /// <summary>
         /// Returns a path usable for looking up resources.
