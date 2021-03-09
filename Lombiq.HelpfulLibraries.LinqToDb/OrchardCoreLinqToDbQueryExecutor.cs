6228be742b138a9d1167bae7587aad9a36ea8c66
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-03-09 16:38:08 +0100

    Moving LinqToDB to HelpfulLibraries

---
+++ Lombiq.HelpfulLibraries.LinqToDb/OrchardCoreLinqToDbQueryExecutor.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using LinqToDB;
+using Lombiq.HelpfulLibraries.LinqToDb;
+using YesSql;
+
+namespace Lombiq.HelpfulLibraries.LinqToDb
+{
+    public static class OrchardCoreLinqToDbQueryExecutor
+    {
+        public static async Task<IEnumerable<TResult>> QueryAsync<TResult>(
+            this ISession session,
+            Func<ITableAccessor, IQueryable> query)
+        {
+            var transaction = await session.DemandAsync();
+            var convertedSql = await SetupAndConvertSqlToDialectAsync(session, transaction, query);
+
+            return await transaction.Connection.QueryAsync<TResult>(convertedSql, transaction: transaction);
+        }
+
+        public static async Task<IEnumerable<TResult>> QueryAsync<TFirst, TSecond, TResult>(
+            this ISession session,
+            Func<ITableAccessor, IQueryable> query,
+            Func<TFirst, TSecond, TResult> map,
+            string splitOn)
+        {
+            var transaction = await session.DemandAsync();
+            var convertedSql = await SetupAndConvertSqlToDialectAsync(session, transaction, query);
+
+            return await transaction.Connection.QueryAsync(convertedSql, map: map, transaction: transaction, splitOn: splitOn);
+        }
+
+        private static async Task<string> SetupAndConvertSqlToDialectAsync(
+            ISession session,
+            IDbTransaction transaction,
+            Func<ITableAccessor, IQueryable> query)
+        {
+            // Instantiating a linq2db connection object as it is required to start building the query. Note that it
+            // won't create an actual connection with the database yet.
+            await using var linqToDbConnection = new PrefixedDataConnection(
+                GetDatabaseProvider(session.Store.Dialect.Name),
+                transaction.Connection.ConnectionString,
+                session.Store.Configuration.TablePrefix);
+
+            // Generate aliases for final projection.
+            LinqToDB.Common.Configuration.Sql.GenerateFinalAliases = true;
+
+            return query(linqToDbConnection).ToString();
+        }
+
+        private static string GetDatabaseProvider(string dbName) =>
+            dbName switch
+            {
+                // Using explicit string instead of LinqToDB.ProviderName.SQLite because the "System.Data.SqlClient"
+                // provider will be used causing "Could not load type System.Data.SqlClient.SqlCommandBuilder"
+                // exception. See: https://github.com/linq2db/linq2db/issues/2191#issuecomment-618450439
+                "SqlServer" => "Microsoft.Data.SqlClient",
+                "Sqlite" => ProviderName.SQLite,
+                "MySql" => ProviderName.MySql,
+                "Postgres" => ProviderName.PostgreSQL,
+                _ => throw new NotSupportedException("The provider name is not supported."),
+            };
+    }
+}
---
+++ Lombiq.HelpfulLibraries.LinqToDb/PrefixedDataConnection.cs
@@ -0,0 +1,23 @@
+﻿using LinqToDB;
+using LinqToDB.Data;
+
+namespace Lombiq.HelpfulLibraries.LinqToDb
+{
+    public class PrefixedDataConnection : DataConnection, ITableAccessor
+    {
+        private readonly string _tablePrefix;
+
+        public PrefixedDataConnection(string databaseName, string connectionString, string tablePrefix)
+            : base(databaseName, connectionString) =>
+            _tablePrefix = tablePrefix;
+
+        public ITable<T> GetPrefixedTable<T>()
+            where T : class
+        {
+            var table = GetTable<T>();
+            return table.TableName(_tablePrefix + table.TableName);
+        }
+
+        ITable<T> ITableAccessor.GetTable<T>() => GetPrefixedTable<T>();
+    }
+}
