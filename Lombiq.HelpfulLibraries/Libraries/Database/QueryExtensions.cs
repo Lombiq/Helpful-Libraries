a3a4e5c8bcfddd7829954d27e74a171ae08d2954
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-03-11 16:12:18 +0100

    Code styling

--- Lombiq.HelpfulLibraries/Libraries/Database/QueryExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Database/QueryExtensions.cs
@@ -16,7 +16,10 @@ namespace YesSql
         /// <param name="pageIndex">Zero-based index of the desired page.</param>
         /// <param name="count">The page size.</param>
         /// <returns>The desired page of the resulting <see cref="ContentItem"/>s.</returns>
-        public static Task<IEnumerable<ContentItem>> PaginateAsync(this IQuery<ContentItem> query, int pageIndex = 0, int count = int.MaxValue)
+        public static Task<IEnumerable<ContentItem>> PaginateAsync(
+            this IQuery<ContentItem> query,
+            int pageIndex = 0,
+            int count = int.MaxValue)
         {
             if (pageIndex > 0) query = query.Skip(pageIndex * count);
             if (count < int.MaxValue) query = query.Take(count);
@@ -37,7 +40,10 @@ namespace YesSql
         /// The desired page of the resulting <see cref="ContentItem"/>s converted into the desired ContentPart. Those
         /// that don't have it are discarded.
         /// </returns>
-        public static Task<IEnumerable<TPart>> PaginateAsync<TPart>(this IQuery<ContentItem> query, int pageIndex = 0, int count = int.MaxValue)
+        public static Task<IEnumerable<TPart>> PaginateAsync<TPart>(
+            this IQuery<ContentItem> query,
+            int pageIndex = 0,
+            int count = int.MaxValue)
             where TPart : ContentPart =>
             PaginateAsync(query, pageIndex, count).ContinueWith(t => t.Result.As<TPart>().Where(part => part != null), TaskScheduler.Default);
 
@@ -45,7 +51,10 @@ namespace YesSql
         /// Breaks the query up into pages and lists the page using the given zero-based index. If pageIndex is 0 and
         /// count is <see cref="int.MaxValue"/> then the whole query is listed.
         /// </summary>
-        public static Task<IEnumerable<TIndex>> PaginateAsync<TIndex>(this IQueryIndex<TIndex> query, int pageIndex = 0, int count = int.MaxValue)
+        public static Task<IEnumerable<TIndex>> PaginateAsync<TIndex>(
+            this IQueryIndex<TIndex> query,
+            int pageIndex = 0,
+            int count = int.MaxValue)
             where TIndex : IIndex =>
             query.Skip(pageIndex * count).Take(count).ListAsync();
 
