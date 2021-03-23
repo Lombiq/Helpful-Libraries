7205e10d054b4d7a59a4af9e92d07ae49a1ae9b0
Dávid El-Saig <david.el-saig@lombiq.com>
2021-03-23 15:51:05 +0100

    Added doc.

--- Lombiq.HelpfulLibraries/Libraries/Mvc/ShapeResultExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Mvc/ShapeResultExtensions.cs
@@ -5,6 +5,10 @@
     /// </summary>
     public static class ShapeResultExtensions
     {
+        /// <summary>
+        /// Uses the <see cref="ShapeResult.Location(string)"/> to set the <paramref name="name"/> of the tab and its
+        /// <paramref name="priority"/> in the order of the tabs.
+        /// </summary>
         public static ShapeResult UseTab(this ShapeResult shapeResult, string name, int priority) =>
             shapeResult.Location($"Parts#{name}: {priority}");
     }
