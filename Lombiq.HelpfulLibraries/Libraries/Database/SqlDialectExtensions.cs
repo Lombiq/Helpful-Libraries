c10028fe53bbac4eca1d86a56a2b2c442a9b7585
Dávid El-Saig <david.el-saig@lombiq.com>
2021-03-30 16:33:23 +0200

    Use type checking.

--- Lombiq.HelpfulLibraries/Libraries/Database/SqlDialectExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Database/SqlDialectExtensions.cs
@@ -8,11 +8,11 @@ namespace YesSql
         /// <summary>
         /// Returns <see langword="true"/> if the database uses Microsoft SQL Server's dialect of SQL.
         /// </summary>
-        public static bool IsSqlServer(this ISqlDialect dialect) => dialect.GetType().Name == nameof(SqlServerDialect);
+        public static bool IsSqlServer(this ISqlDialect dialect) => dialect is SqlServerDialect;
 
         /// <summary>
         /// Returns <see langword="true"/> if the database uses SQLite's dialect of SQL.
         /// </summary>
-        public static bool IsSqlite(this ISqlDialect dialect) => dialect.GetType().Name == nameof(SqliteDialect);
+        public static bool IsSqlite(this ISqlDialect dialect) => dialect is SqliteDialect;
     }
 }
