2b7cc456869d8cd01a064b49344b472da2664dcc
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-04-11 13:35:51 +0200

    Grammar

--- Lombiq.HelpfulLibraries/Libraries/Mvc/ShapeResultExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Mvc/ShapeResultExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace OrchardCore.DisplayManagement.Views
+namespace OrchardCore.DisplayManagement.Views
 {
     /// <summary>
     /// Helper class for generating placement strings.
@@ -6,7 +6,7 @@
     public static class ShapeResultExtensions
     {
         /// <summary>
-        /// Uses the <see cref="ShapeResult.Location(string)"/> to set the <paramref name="name"/> of the tab and its
+        /// Uses <see cref="ShapeResult.Location(string)"/> to set the <paramref name="name"/> of the tab and its
         /// <paramref name="priority"/> in the order of the tabs.
         /// </summary>
         public static ShapeResult UseTab(this ShapeResult shapeResult, string name, int priority) =>
