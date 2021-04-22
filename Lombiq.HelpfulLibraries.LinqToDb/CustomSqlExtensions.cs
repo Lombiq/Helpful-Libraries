b532bc9ee6d98c10468c9e2c58814fc101914265
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-22 12:27:29 +0200

    Suppress code analysis

--- Lombiq.HelpfulLibraries.LinqToDb/CustomSqlExtensions.cs
+++ Lombiq.HelpfulLibraries.LinqToDb/CustomSqlExtensions.cs
@@ -7,6 +7,7 @@ namespace Lombiq.HelpfulLibraries.Linq2Db
     {
         [Sql.Expression("SqlServer", "JSON_VALUE({0}, {1})", ServerSideOnly = true, InlineParameters = true)]
         [Sql.Expression("Sqlite", "json_extract({0}, {1})", ServerSideOnly = true, InlineParameters = true)]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
         public static string JsonValue(object expression, string path) => throw new InvalidOperationException();
     }
 }
