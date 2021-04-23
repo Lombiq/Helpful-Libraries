cb93ab326d3e96cf04995a741f6cc4d4c94abdcf
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-23 21:43:19 +0200

    Code cleanup

--- Lombiq.HelpfulLibraries.LinqToDb/Startup.cs
+++ Lombiq.HelpfulLibraries.LinqToDb/Startup.cs
@@ -1,11 +1,8 @@
 using LinqToDB.AspNet;
 using LinqToDB.AspNet.Logging;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Modules;
-using System;
 using static Lombiq.HelpfulLibraries.LinqToDb.Constants.FeatureIds;
 
 namespace Lombiq.HelpfulLibraries.LinqToDb
@@ -25,15 +22,6 @@ namespace Lombiq.HelpfulLibraries.LinqToDb
                     .UseConnectionString("Microsoft.Data.SqlClient", connectionString)
                     .UseDefaultLogging(provider),
                 ServiceLifetime.Transient);
-
-        }
-
-        public override void Configure(
-            IApplicationBuilder app,
-            IEndpointRouteBuilder routes,
-            IServiceProvider serviceProvider)
-        {
-            // Method intentionally left empty.
         }
     }
 }
