742ef80effdc63b5f1c5b368fb866345452f1aa0
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-04-23 23:18:33 +0200

    Code refactoring

--- Lombiq.HelpfulLibraries.LinqToDb/Startup.cs
+++ Lombiq.HelpfulLibraries.LinqToDb/Startup.cs
@@ -1,8 +1,12 @@
 using LinqToDB.AspNet;
 using LinqToDB.AspNet.Logging;
+using LinqToDB.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using OrchardCore.Environment.Shell.Configuration;
+using Microsoft.Extensions.Options;
+using OrchardCore.Environment.Shell;
 using OrchardCore.Modules;
+using System;
+using System.IO;
 using static Lombiq.HelpfulLibraries.LinqToDb.Constants.FeatureIds;
 
 namespace Lombiq.HelpfulLibraries.LinqToDb
@@ -10,18 +14,47 @@ namespace Lombiq.HelpfulLibraries.LinqToDb
     [Feature(Default)]
     public class Startup : StartupBase
     {
-        private readonly IShellConfiguration _shellConfiguration;
+        private readonly IOptions<ShellOptions> _shellOptions;
+        private readonly ShellSettings _shellSettings;
 
-        public Startup(IShellConfiguration shellConfiguration) => _shellConfiguration = shellConfiguration;
+        public Startup(IOptions<ShellOptions> shellOptions, ShellSettings shellSettings)
+        {
+            _shellOptions = shellOptions;
+            _shellSettings = shellSettings;
+        }
+
         public override void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = _shellConfiguration["ConnectionString"];
-            services.AddLinqToDbContext<PrefixedDataConnection>(
-                (provider, options) =>
-                    options
-                    .UseConnectionString("Microsoft.Data.SqlClient", connectionString)
-                    .UseDefaultLogging(provider),
-                ServiceLifetime.Transient);
+            var connectionString = _shellSettings["ConnectionString"];
+
+            switch (_shellSettings["DatabaseProvider"])
+            {
+                case "SqlConnection":
+                    services.AddLinqToDbContext<PrefixedDataConnection>(
+                    (provider, options) =>
+                        options
+                        // Using explicit string instead of LinqToDB.ProviderName.SQLServer because the "System.Data.SqlClient"
+                        // provider will be used causing "Could not load type System.Data.SqlClient.SqlCommandBuilder"
+                        // exception. See: https://github.com/linq2db/linq2db/issues/2191#issuecomment-618450439
+                        .UseConnectionString("Microsoft.Data.SqlClient", connectionString)
+                        .UseDefaultLogging(provider),
+                    ServiceLifetime.Transient);
+                    break;
+                case "Sqlite":
+                    var option = _shellOptions.Value;
+                    var databaseFolder = Path.Combine(option.ShellsApplicationDataPath, option.ShellsContainerName, _shellSettings.Name);
+                    var databaseFile = Path.Combine(databaseFolder, "yessql.db");
+
+                    services.AddLinqToDbContext<PrefixedDataConnection>(
+                    (provider, options) =>
+                        options
+                        .UseSQLite($"Data Source={databaseFile};Cache=Shared")
+                        .UseDefaultLogging(provider),
+                    ServiceLifetime.Transient);
+                    break;
+                default:
+                    throw new ArgumentException("Unknown database provider: " + _shellSettings["DatabaseProvider"]);
+            }
         }
     }
 }
