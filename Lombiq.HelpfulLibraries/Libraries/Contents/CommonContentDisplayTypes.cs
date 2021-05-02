28683c4b1b6626f83a6ac59da8e4bad6b5611613
Dávid El-Saig <david.el-saig@lombiq.com>
2021-05-02 17:21:55 +0200

    Added CommonContentDisplayTypes.

---
+++ Lombiq.HelpfulLibraries/Libraries/Contents/CommonContentDisplayTypes.cs
@@ -0,0 +1,14 @@
+﻿using OrchardCore.ContentManagement.Display;
+
+namespace Lombiq.HelpfulLibraries.Libraries.Contents
+{
+    /// <summary>
+    /// Values that can be used with <see cref="IContentItemDisplayManager.BuildDisplayAsync"/> or
+    /// <see cref="OrchardRazorHelperExtensions.DisplayAsync"/>.
+    /// </summary>
+    public static class CommonContentDisplayTypes
+    {
+        public const string Summary = nameof(Summary);
+        public const string SummaryAdmin = nameof(SummaryAdmin);
+    }
+}
