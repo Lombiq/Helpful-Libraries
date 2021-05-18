4ed445e2662559749457e927d35041216689de85
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-05-18 11:49:50 +0200

    Code cleanup

--- Lombiq.HelpfulLibraries.LinqToDb/Extensions/CustomSqlExtensions.cs
+++ Lombiq.HelpfulLibraries.LinqToDb/Extensions/CustomSqlExtensions.cs
@@ -1,5 +1,6 @@
 using LinqToDB;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Lombiq.HelpfulLibraries.LinqToDb.Extensions
 {
@@ -7,17 +8,17 @@ namespace Lombiq.HelpfulLibraries.LinqToDb.Extensions
     {
         [Sql.Expression(ProviderName.SqlServer, "JSON_VALUE({0}, {1})", ServerSideOnly = true, InlineParameters = true)]
         [Sql.Expression(ProviderName.SQLite, "json_extract({0}, {1})", ServerSideOnly = true, InlineParameters = true)]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
+        [SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
         public static string JsonValue(object expression, string path) => throw new InvalidOperationException();
 
         [Sql.Expression(ProviderName.SqlServer, "JSON_MODIFY({0}, {1}, {2})", ServerSideOnly = true, InlineParameters = true)]
         [Sql.Expression(ProviderName.SQLite, "json_replace({0}, {1}, {2})", ServerSideOnly = true, InlineParameters = true)]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
+        [SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
         public static string JsonModify(string json, string path, string newValue) => throw new InvalidOperationException();
 
         [Sql.Expression(ProviderName.SqlServer, "JSON_QUERY({0})", ServerSideOnly = true, InlineParameters = true)]
         [Sql.Expression(ProviderName.SQLite, "json({0})", ServerSideOnly = true, InlineParameters = true)]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
+        [SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
         public static string JsonQuery(string value) => throw new InvalidOperationException();
     }
 }
