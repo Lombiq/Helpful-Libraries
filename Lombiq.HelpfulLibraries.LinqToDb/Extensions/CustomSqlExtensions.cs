e979dcac162be4fa8c4eb74cd35b82fed48f81f7
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-05-19 12:20:13 +0200

    Return null instead of exception thrown

--- Lombiq.HelpfulLibraries.LinqToDb/Extensions/CustomSqlExtensions.cs
+++ Lombiq.HelpfulLibraries.LinqToDb/Extensions/CustomSqlExtensions.cs
@@ -10,16 +10,16 @@ namespace Lombiq.HelpfulLibraries.LinqToDb.Extensions
         [Sql.Expression(ProviderName.SqlServer, "JSON_VALUE({0}, {1})", ServerSideOnly = true, InlineParameters = true)]
         [Sql.Expression(ProviderName.SQLite, "json_extract({0}, {1})", ServerSideOnly = true, InlineParameters = true)]
         [SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
-        public static string JsonValue(object expression, string path) => throw new InvalidOperationException();
+        public static string JsonValue(object expression, string path) => null;
 
         [Sql.Expression(ProviderName.SqlServer, "JSON_MODIFY({0}, {1}, {2})", ServerSideOnly = true, InlineParameters = true)]
         [Sql.Expression(ProviderName.SQLite, "json_replace({0}, {1}, {2})", ServerSideOnly = true, InlineParameters = true)]
         [SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
-        public static string JsonModify(string json, string path, string newValue) => throw new InvalidOperationException();
+        public static string JsonModify(string json, string path, string newValue) => null;
 
         [Sql.Expression(ProviderName.SqlServer, "JSON_QUERY({0})", ServerSideOnly = true, InlineParameters = true)]
         [Sql.Expression(ProviderName.SQLite, "json({0})", ServerSideOnly = true, InlineParameters = true)]
         [SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
-        public static string JsonQuery(string value) => throw new InvalidOperationException();
+        public static string JsonQuery(string value) => null;
     }
 }
