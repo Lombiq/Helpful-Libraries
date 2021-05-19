f37fca1f3827ced960cdfa0c9e0f22ee7ba94d8b
Dávid El-Saig <david.el-saig@lombiq.com>
2021-05-19 21:10:48 +0200

    Added IDeferredTask.

---
+++ Lombiq.HelpfulLibraries/Libraries/Middleware/DeferredTaskMiddleware.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulLibraries.Libraries.Middleware
+{
+    public class DeferredTaskMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public DeferredTaskMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task InvokeAsync(
+            HttpContext context,
+            IEnumerable<IDeferredTask> deferredTasks)
+        {
+            foreach (var deferredTask in deferredTasks)
+            {
+                deferredTask.IsStarted = true;
+                await deferredTask.PreProcessAsync(context);
+            }
+
+            await _next(context);
+
+            foreach (var deferredTask in deferredTasks) await deferredTask.PostProcessAsync(context);
+        }
+    }
+
+    public static class DeferredTaskApplicationBuilderExtensions
+    {
+        public static void UseDeferredTasks(this IApplicationBuilder app) =>
+            app.UseMiddleware<DeferredTaskMiddleware>();
+    }
+}
