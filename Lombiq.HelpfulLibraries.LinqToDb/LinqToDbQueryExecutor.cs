1af9e198c0f016a1e303ef45d966d53bf27bd614
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-05-19 19:39:42 +0200

    Grammar fix

--- Lombiq.HelpfulLibraries.LinqToDb/LinqToDbQueryExecutor.cs
+++ Lombiq.HelpfulLibraries.LinqToDb/LinqToDbQueryExecutor.cs
@@ -53,9 +53,10 @@ namespace Lombiq.HelpfulLibraries.LinqToDb
         private static string GetDatabaseProviderName(string dbName) =>
             dbName switch
             {
-                // Using explicit string instead of LinqToDB.ProviderName.SqlServer because the "System.Data.SqlClient"
-                // provider will be used causing "Could not load type System.Data.SqlClient.SqlCommandBuilder"
-                // exception. See: https://github.com/linq2db/linq2db/issues/2191#issuecomment-618450439
+                // Using explicit string instead of LinqToDB.ProviderName.SqlServer because if the
+                // "System.Data.SqlClient" provider will be used it will cause "Could not load type
+                // System.Data.SqlClient.SqlCommandBuilder" exception.
+                // See: https://github.com/linq2db/linq2db/issues/2191#issuecomment-618450439
                 "SqlServer" => "Microsoft.Data.SqlClient",
                 "Sqlite" => ProviderName.SQLite,
                 "MySql" => ProviderName.MySql,
