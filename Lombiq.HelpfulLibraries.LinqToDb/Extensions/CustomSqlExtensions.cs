06d581247d5001f0f86f99c3f9a49201efe6f6c4
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-05-19 17:58:27 +0200

    Code styling

--- Lombiq.HelpfulLibraries.LinqToDb/Extensions/CustomSqlExtensions.cs
+++ Lombiq.HelpfulLibraries.LinqToDb/Extensions/CustomSqlExtensions.cs
@@ -5,8 +5,8 @@ namespace Lombiq.HelpfulLibraries.LinqToDb.Extensions
 {
     public static class CustomSqlExtensions
     {
-        // You don't need an actual implementation in the function body for these simple expressions. The parameters will
-        // be used by the attribute.
+        // You don't need an actual implementation in the function body for these simple expressions. The parameters
+        // will be used by the attribute.
         [Sql.Expression(ProviderName.SqlServer, "JSON_VALUE({0}, {1})", ServerSideOnly = true, InlineParameters = true)]
         [Sql.Expression(ProviderName.SQLite, "json_extract({0}, {1})", ServerSideOnly = true, InlineParameters = true)]
         [SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "Used in the Sql.Expression attribute.")]
