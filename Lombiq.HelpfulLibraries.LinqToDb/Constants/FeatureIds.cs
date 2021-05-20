13e78b791c9ff99bc96ffeb02cf6ad9bb1bd93f4
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-05-20 10:41:05 +0200

    Deleting module related code

--- Lombiq.HelpfulLibraries.LinqToDb/Constants/FeatureIds.cs
+++
@@ -1,9 +0,0 @@
-namespace Lombiq.HelpfulLibraries.LinqToDb.Constants
-{
-    public static class FeatureIds
-    {
-        public const string Area = "Lombiq.HelpfulLibraries.LinqToDb";
-
-        public const string Default = Area;
-    }
-}
