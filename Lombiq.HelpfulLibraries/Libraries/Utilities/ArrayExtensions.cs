518feb344c48d0c7e6f107cfaffd629938f6af72
Márk Bartha <mark.bartha@lombiq.com>
2021-05-23 14:12:23 +0200

    Adding ToStringArray extension

---
+++ Lombiq.HelpfulLibraries/Libraries/Utilities/ArrayExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace System
+{
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Converts an <see cref="object"/> array to a <see cref="string"/> array.
+        /// </summary>
+        /// <param name="includeNulls">If <see langword="false"/>, the converted array will exclude nulls (i.e., the
+        /// that are not <see cref="string"/>s).</param>
+        /// <returns>The created <see cref="string"/> array.</returns>
+        public static string[] ToStringArray(this object[] array, bool includeNulls = false)
+        {
+            var stringArray = Array.ConvertAll(array, item => item?.ToString());
+
+            return includeNulls ? stringArray : stringArray.Where(item => item != null).ToArray();
+        }
+    }
+}
