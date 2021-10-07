0c3b996a9b1fa5e79e84008ea544b04b472e0ae7
Dávid El-Saig <david.el-saig@lombiq.com>
2021-10-07 14:59:05 +0200

    Code cleanup.

--- Lombiq.HelpfulLibraries/Libraries/Contents/ContentDefinitionCommands.cs
+++ Lombiq.HelpfulLibraries/Libraries/Contents/ContentDefinitionCommands.cs
@@ -1,13 +1,6 @@
 using Microsoft.Extensions.Localization;
 using OrchardCore.ContentManagement.Metadata;
-using OrchardCore.ContentManagement.Metadata.Settings;
-using OrchardCore.Entities;
 using OrchardCore.Environment.Commands;
-using OrchardCore.Modules;
-using OrchardCore.Users.Models;
-using OrchardCore.Users.Services;
-using System.Threading.Tasks;
-using YesSql;
 
 namespace Lombiq.HelpfulLibraries.Libraries.Contents
 {
@@ -16,9 +9,6 @@ namespace Lombiq.HelpfulLibraries.Libraries.Contents
         private const string AttachContentPartCommandName = "attachContentPart";
 
         private readonly IContentDefinitionManager _contentDefinitionManager;
-        private readonly IUserService _userService;
-        private readonly ISession _session;
-        private readonly IClock _clock;
 
         [OrchardSwitch]
         public string Type { get; set; }
@@ -28,24 +18,16 @@ namespace Lombiq.HelpfulLibraries.Libraries.Contents
 
         public ContentDefinitionCommands(
             IStringLocalizer<ContentDefinitionCommands> localizer,
-            IContentDefinitionManager contentDefinitionManager,
-            IUserService userService,
-            ISession session,
-            IClock clock)
-            : base(localizer)
-        {
+            IContentDefinitionManager contentDefinitionManager)
+            : base(localizer) =>
             _contentDefinitionManager = contentDefinitionManager;
-            _userService = userService;
-            _session = session;
-            _clock = clock;
-        }
 
         [CommandName(AttachContentPartCommandName)]
         [CommandHelp(AttachContentPartCommandName +
             " /Type:<contentTypeName>" +
             " /Part:<contentPartName>" +
             "\r\n\t" + "Attaches a content type to a content part.")]
-        [OrchardSwitches("UserName, EmailConfirmed, UserVerified, UserApproved")]
+        [OrchardSwitches(nameof(Type) + ", " + nameof(Part))]
         public void AttachContentPart() =>
             _contentDefinitionManager.AlterTypeDefinition(Type, type => type.WithPart(Part));
     }
