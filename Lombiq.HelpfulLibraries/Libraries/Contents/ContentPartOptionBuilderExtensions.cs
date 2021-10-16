c1327d33b3140603952c4218ac67988f60cbd477
Dávid El-Saig <david.el-saig@lombiq.com>
2021-10-16 10:36:29 +0200

    Add WithIndex.

--- Lombiq.HelpfulLibraries/Libraries/Contents/ContentPartOptionBuilderExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Contents/ContentPartOptionBuilderExtensions.cs
@@ -1,16 +1,40 @@
 using Microsoft.Extensions.DependencyInjection;
-using OrchardCore.ContentManagement;
 using OrchardCore.Data.Migration;
+using YesSql.Indexes;
 
-namespace Lombiq.HelpfulLibraries.Libraries.Contents
+namespace OrchardCore.ContentManagement
 {
     public static class ContentPartOptionBuilderExtensions
     {
+        /// <summary>
+        /// Adds an <see cref="IIndexProvider"/> to the service collection while maintaining the call chain for the
+        /// content part builder.
+        /// </summary>
+        public static ContentPartOptionBuilder WithIndex<TIndex>(this ContentPartOptionBuilder builder)
+            where TIndex : class, IIndexProvider
+        {
+            builder.Services.AddSingleton<IIndexProvider, TIndex>();
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds an <see cref="IDataMigration"/> to the service collection while maintaining the call chain for the
+        /// content part builder.
+        /// </summary>
         public static ContentPartOptionBuilder WithMigration<TMigration>(this ContentPartOptionBuilder builder)
             where TMigration : IDataMigration
         {
             builder.Services.AddScoped(typeof(IDataMigration), typeof(TMigration));
             return builder;
         }
+
+        /// <summary>
+        /// Adds an <see cref="IDataMigration"/> and an <see cref="IIndexProvider"/> to the service collection while
+        /// maintaining the call chain for the content part builder.
+        /// </summary>
+        public static ContentPartOptionBuilder WithMigration<TMigration, TIndex>(this ContentPartOptionBuilder builder)
+            where TMigration : IDataMigration
+            where TIndex : class, IIndexProvider =>
+            builder.WithMigration<TMigration>().WithIndex<TIndex>();
     }
 }
