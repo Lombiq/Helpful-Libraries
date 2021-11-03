c42cdfbc5ba8174307662c750b566c0b77b702e9
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-11-03 15:22:41 +0100

    Adding taxonomy helper service

---
+++ Lombiq.HelpfulLibraries/Libraries/Contents/TaxonomyHelper.cs
@@ -0,0 +1,25 @@
+using OrchardCore.ContentManagement;
+using OrchardCore.Taxonomies.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulLibraries.Libraries.Contents
+{
+    public class TaxonomyHelper : ITaxonomyHelper
+    {
+        private readonly IContentAliasManager _contentAliasManager;
+        private readonly IContentManager _contentManager;
+        public TaxonomyHelper(IContentAliasManager contentAliasManager, IContentManager contentManager)
+        {
+            _contentAliasManager = contentAliasManager;
+            _contentManager = contentManager;
+        }
+
+        public async Task<ContentItem> GetTermContentItemAsync(string alias, string termId)
+        {
+            var contentItemId = await _contentAliasManager.GetContentItemIdAsync($"alias:{alias}");
+            var terms = (await _contentManager.GetAsync(contentItemId)).As<TaxonomyPart>().Terms;
+            return terms.FirstOrDefault(term => term.ContentItemId == termId);
+        }
+    }
+}
