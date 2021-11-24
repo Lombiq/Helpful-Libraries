2b147bf7e464dc0c0b8fd31ff36b1cdddab5bbdb
Dávid El-Saig <david.el-saig@lombiq.com>
2021-11-24 07:55:11 +0100

    Add ResourceManagementOptionsConfigurationBase.

---
+++ Lombiq.HelpfulLibraries/Libraries/ResourceManagement/ResourceManagementOptionsConfigurationBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Options;
+using OrchardCore.ResourceManagement;
+
+namespace Lombiq.HelpfulLibraries.Libraries.ResourceManagement
+{
+    public class ResourceManagementOptionsConfigurationBase : IConfigureOptions<ResourceManagementOptions>
+    {
+        protected static readonly ResourceManifest _manifest = new();
+
+        public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
+    }
+}
