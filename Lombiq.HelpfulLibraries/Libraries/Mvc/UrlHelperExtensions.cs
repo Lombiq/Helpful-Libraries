a41318dd8a864e018ee5c99a813954de5559942a
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-12-03 13:33:38 +0100

    Fixing documentation

--- Lombiq.HelpfulLibraries/Libraries/Mvc/UrlHelperExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Mvc/UrlHelperExtensions.cs
@@ -10,13 +10,13 @@ namespace Microsoft.AspNetCore.Mvc.Routing
         private const string OrchardCoreContentsArea = "OrchardCore.Contents";
 
         /// <summary>
-        /// Returns a relative URL for the editor action of <paramref name="contentItemId"/>.
+        /// Returns an absolute URL for the editor action of <paramref name="contentItemId"/>.
         /// </summary>
         public static string EditContentItem(this IUrlHelper helper, string contentItemId) =>
             EditContentItemWithTab(helper, tabIdPart: null, contentItemId);
 
         /// <summary>
-        /// Returns a relative URL for the editor action of <paramref name="content"/> with the tab of
+        /// Returns an absolute URL for the editor action of <paramref name="content"/> with the tab of
         /// <paramref name="tabIdPart"/> selected.
         /// </summary>
         /// <param name="tabIdPart">The name of the tab as used in the placement info.</param>
@@ -24,7 +24,7 @@ namespace Microsoft.AspNetCore.Mvc.Routing
             EditContentItemWithTab(helper, tabIdPart, content.ContentItem.ContentItemId);
 
         /// <summary>
-        /// Returns a relative URL for the editor action of <paramref name="contentItemId"/> with the tab of
+        /// Returns an absolute URL for the editor action of <paramref name="contentItemId"/> with the tab of
         /// <paramref name="tabIdPart"/> selected.
         /// </summary>
         /// <param name="tabIdPart">The name of the tab as used in the placement info.</param>
@@ -44,7 +44,7 @@ namespace Microsoft.AspNetCore.Mvc.Routing
         }
 
         /// <summary>
-        /// Returns a relative URL for the <see cref="ContentItem"/> display page for the given <paramref name="content"/>.
+        /// Returns an absolute URL for the <see cref="ContentItem"/> display page for the given <paramref name="content"/>.
         /// </summary>
         public static string DisplayContentItem(this IUrlHelper helper, IContent content) =>
             helper.Action(
