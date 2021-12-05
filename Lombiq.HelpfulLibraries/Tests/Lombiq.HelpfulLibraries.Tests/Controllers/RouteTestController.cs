10b22d07fba25681e8675dfd6fb99f7485882868
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-05 21:39:34 +0100

    Add unit tests.

---
+++ Lombiq.HelpfulLibraries/Tests/Lombiq.HelpfulLibraries.Tests/Controllers/RouteTestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrchardCore.Admin;
+using System;
+
+namespace Lombiq.HelpfulLibraries.Tests.Controllers
+{
+    public class RouteTestController : Controller
+    {
+        public IActionResult Foo() => Content(string.Empty);
+        public IActionResult Bar() => Content(string.Empty);
+
+        [Admin]
+        public IActionResult Baz() => Content(string.Empty);
+
+        [Route("I/Am/Routed")]
+        public IActionResult Route() => Content(string.Empty);
+
+        [Route("content/{id}")]
+        public IActionResult RouteSubstitution(int id) => Content(string.Empty);
+
+        public IActionResult Arguments(int number, double fraction, DateTime dateTime, string text) =>
+            Content(string.Empty);
+    }
+}
