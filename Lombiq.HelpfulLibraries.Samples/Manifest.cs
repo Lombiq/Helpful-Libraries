546364d52ad85d64ba03aea2e4cc009bd3a108c4
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-09 01:41:41 +0100

    Create Lombiq.HelpfulLibraries.Samples.

---
+++ Lombiq.HelpfulLibraries.Samples/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Lombiq.HelpfulLibraries.Samples",
+    Author = "Lombiq Technologies",
+    Website = "https://lombiq.com",
+    Version = "1.0.0",
+    Description = "Samples for Lombiq Helpful Libraries.",
+    Category = "Development"
+)]
