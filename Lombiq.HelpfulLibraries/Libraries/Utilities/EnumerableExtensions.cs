419628a4314bc79ac471189ea010efe7c43bfe8d
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-19 03:57:40 +0100

    Docs bug fix.

--- Lombiq.HelpfulLibraries/Libraries/Utilities/EnumerableExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Utilities/EnumerableExtensions.cs
@@ -258,7 +258,7 @@ namespace System.Collections.Generic
 
         /// <summary>
         /// Returns the Cartesian product (also known as cross product) of <paramref name="collection1"/> with <paramref
-        /// cref="collection2"/>, or if that's <see langword="null"/> then with itself. This means that each possible
+        /// name="collection2"/>, or if that's <see langword="null"/> then with itself. This means that each possible
         /// pairing of the collections are returned. This can replace two nested for loops with a single foreach loop.
         /// </summary>
         public static IEnumerable<(T Left, T Right)> CartesianProduct<T>(
