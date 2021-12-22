09b580aae136bf6a63e62d517c2c1edc3d935fc3
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-22 02:32:35 +0100

    Add docs to WithTimeoutAsync.

--- Lombiq.HelpfulLibraries/Libraries/Utilities/TaskExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Utilities/TaskExtensions.cs
@@ -7,6 +7,18 @@ namespace Lombiq.HelpfulLibraries.Libraries.Utilities
 {
     public static class TaskExtensions
     {
+        /// <summary>
+        /// Returns a <see cref="Task"/> that completes either if the provided <paramref name="task"/> is completed or
+        /// if the time indicated by <paramref name="timeout"/> has elapsed.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Note that if the <paramref name="timeout"/> is up, that won't cancel the <paramref name="task"/> because
+        /// this method doesn't have access to its cancellation token source, if there is any. However if one exists you
+        /// can manually trigger cancellation after checking if <see cref="Task.IsCompleted"/> is <see langword="false"/>
+        /// on the <paramref name="task"/>.
+        /// </para>
+        /// </remarks>
         [SuppressMessage(
             "Usage",
             "VSTHRD003",
@@ -15,6 +27,11 @@ namespace Lombiq.HelpfulLibraries.Libraries.Utilities
         public static Task WithTimeoutAsync(this Task task, TimeSpan timeout, CancellationToken cancellationToken = default) =>
             Task.WhenAny(task, Task.Delay(timeout, cancellationToken));
 
+        /// <summary>
+        /// Returns a <see cref="Task"/> that resolves the <see cref="Task{TResult}.Result"/> of <paramref name="task"/>
+        /// if it completed, or <see langword="default"/> of <typeparamref name="T"/> if the time indicated by
+        /// <paramref name="timeout"/> has elapsed.
+        /// </summary>
         [SuppressMessage(
             "Usage",
             "VSTHRD003",
