77ae67ffc8b16e530013be124e9215e557a7f52c
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-29 20:33:46 +0100

    Add AddRange and RemoveAll methods for all ICollection{T}.

--- Lombiq.HelpfulLibraries/Libraries/Utilities/EnumerableExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Utilities/EnumerableExtensions.cs
@@ -274,5 +274,29 @@ namespace System.Collections.Generic
             this ICollection<T> collection1,
             ICollection<T> collection2 = null) =>
             collection1.SelectMany(_ => collection2 ?? collection1, (left, right) => (left, right));
+
+        /// <summary>
+        /// Appends every item from <paramref name="source"/> to <paramref name="target"/> using <see
+        /// cref="ICollection{T}.Add(T)"/>.
+        /// </summary>
+        public static void AddRange<T>(this ICollection<T> target, IEnumerable<T> source)
+        {
+            foreach (var item in source) target.Add(item);
+        }
+
+        /// <summary>
+        /// Removes every item <paramref name="collection"/> where <paramref name="predicate"/> returns <see
+        /// langword="true"/> or if it's <see langword="null"/>.
+        /// </summary>
+        public static void RemoveAll<T>(this ICollection<T> collection, Func<T, bool> predicate = null)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate == null || predicate(item))
+                {
+                    collection.Remove(item);
+                }
+            }
+        }
     }
 }
