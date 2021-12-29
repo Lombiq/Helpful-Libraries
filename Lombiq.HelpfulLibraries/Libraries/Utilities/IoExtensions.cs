378a5732413c038b5af888741e73865195d2b746
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-30 00:26:27 +0100

    WriteLine for IConvertable.

--- Lombiq.HelpfulLibraries/Libraries/Utilities/IoExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Utilities/IoExtensions.cs
@@ -24,6 +24,20 @@ namespace System.IO
         /// Concatenates the provided <paramref name="lineFragments"/> to the <paramref name="writer"/> using invariant
         /// culture.
         /// </summary>
+        public static void WriteLineInvariant(this TextWriter writer, params IConvertible[] lineFragments)
+        {
+            foreach (var fragment in lineFragments)
+            {
+                writer.Write(fragment.ToString(CultureInfo.InvariantCulture));
+            }
+
+            writer.WriteLine();
+        }
+
+        /// <summary>
+        /// Concatenates the provided <paramref name="lineFragments"/> to the <paramref name="writer"/> using invariant
+        /// culture.
+        /// </summary>
         public static Task WriteLineInvariantAsync(this TextWriter writer, params FormattableString[] lineFragments) =>
             writer.WriteLineAsync(StringHelper.Concatenate(lineFragments));
     }
