3987723bf37d7154a33dbd7cc522fc512beda53a
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-06 02:47:56 +0100

    Add RequireAuthentication to PortalNavigationProviderBase.

--- Lombiq.HelpfulLibraries/Libraries/Navigation/PortalNavigationProviderBase.cs
+++ Lombiq.HelpfulLibraries/Libraries/Navigation/PortalNavigationProviderBase.cs
@@ -1,11 +1,8 @@
 ﻿using Lombiq.HelpfulLibraries.Libraries.Utilities;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Localization;
 using OrchardCore.Navigation;
-using OrchardCore.Security.Permissions;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Lombiq.HelpfulLibraries.Libraries.Navigation
@@ -13,26 +10,22 @@ namespace Lombiq.HelpfulLibraries.Libraries.Navigation
     public abstract class PortalNavigationProviderBase : INavigationProvider
     {
         public const string NavigationName = "portal";
-
-        private readonly IAuthorizationService _authorizationService;
         private readonly IHttpContextAccessor _hca;
 
-        public virtual IEnumerable<Permission> RequiredPermissions => Array.Empty<Permission>();
-
+        protected virtual bool RequireAuthentication => false;
         protected IStringLocalizer T { get; }
 
         protected PortalNavigationProviderBase(
-            IAuthorizationService authorizationService,
             IHttpContextAccessor hca,
             IStringLocalizer stringLocalizer)
         {
-            _authorizationService = authorizationService;
             _hca = hca;
             T = stringLocalizer;
         }
 
         public Task BuildNavigationAsync(string name, NavigationBuilder builder) =>
-            name.EqualsOrdinalIgnoreCase(NavigationName)
+            name.EqualsOrdinalIgnoreCase(NavigationName) &&
+            (!RequireAuthentication || _hca.HttpContext?.User?.Identity?.IsAuthenticated == true)
                 ? BuildAsync(builder)
                 : Task.CompletedTask;
 
