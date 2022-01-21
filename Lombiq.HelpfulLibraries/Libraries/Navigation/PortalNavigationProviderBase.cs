1d2332ec3c6a11c14215994ec54bfc25d32f2c46
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-21 15:11:08 +0100

    unusing

--- Lombiq.HelpfulLibraries/Libraries/Navigation/PortalNavigationProviderBase.cs
+++ Lombiq.HelpfulLibraries/Libraries/Navigation/PortalNavigationProviderBase.cs
@@ -1,9 +1,5 @@
-﻿using Lombiq.HelpfulLibraries.Libraries.Utilities;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Localization;
-using OrchardCore.Navigation;
-using System;
-using System.Threading.Tasks;
 
 namespace Lombiq.HelpfulLibraries.Libraries.Navigation
 {
