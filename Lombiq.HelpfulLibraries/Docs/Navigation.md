50d9078e30462b33060629ff1c6b618ab966fe16
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-21 14:45:40 +0100

    Create a base class for PortalNavigationName in case we want others in the future.

--- Lombiq.HelpfulLibraries/Docs/Navigation.md
+++ Lombiq.HelpfulLibraries/Docs/Navigation.md
@@ -1,5 +1,14 @@
 # Navigation Documentation
 
+
+
+## NavigationProviderBase
+
+An abstract base class for creating reducing boilerplate in `INavigationProvider` for checking the name parameter and if the user is authenticated. 
+
+
 ## PortalNavigationProviderBase
 
-An abstract base class for creating home page menu structure using the `portal` navigation name. If you use the _Lombiq.BaseTheme_ it automatically displays the generated menu as a widget in the _Navigation_ zone.
+An abstract base class derived from `NavigationProviderBase` for creating home page menu structure using the `portal` navigation name. If you use the _Lombiq.BaseTheme_ it automatically displays the generated menu as a widget in the _Navigation_ zone.
+
+
--- Lombiq.HelpfulLibraries/Libraries/Navigation/PortalNavigationProviderBase.cs
+++ Lombiq.HelpfulLibraries/Libraries/Navigation/PortalNavigationProviderBase.cs
@@ -7,39 +7,15 @@ using System.Threading.Tasks;
 
 namespace Lombiq.HelpfulLibraries.Libraries.Navigation
 {
-    public abstract class PortalNavigationProviderBase : INavigationProvider
+    public abstract class PortalNavigationProviderBase : NavigationProviderBase
     {
-        public const string NavigationName = "portal";
-        private readonly IHttpContextAccessor _hca;
+        public const string PortalNavigationName = "portal";
 
-        protected virtual bool RequireAuthentication => false;
-        protected IStringLocalizer T { get; }
+        protected override string NavigationName => PortalNavigationName;
 
-        protected PortalNavigationProviderBase(
-            IHttpContextAccessor hca,
-            IStringLocalizer stringLocalizer)
+        protected PortalNavigationProviderBase(IHttpContextAccessor hca, IStringLocalizer stringLocalizer)
+            : base(hca, stringLocalizer)
         {
-            _hca = hca;
-            T = stringLocalizer;
         }
-
-        public Task BuildNavigationAsync(string name, NavigationBuilder builder) =>
-            name.EqualsOrdinalIgnoreCase(NavigationName) &&
-            (!RequireAuthentication || _hca.HttpContext?.User?.Identity?.IsAuthenticated == true)
-                ? BuildAsync(builder)
-                : Task.CompletedTask;
-
-        protected virtual Task BuildAsync(NavigationBuilder builder)
-        {
-            Build(builder);
-            return Task.CompletedTask;
-        }
-
-        protected virtual void Build(NavigationBuilder builder) =>
-            throw new NotSupportedException(
-                StringHelper.Join(
-                    " ",
-                    $"Override either {nameof(Build)} or {nameof(BuildAsync)}!",
-                    $"Note that {nameof(BuildAsync)} takes precedence in execution."));
     }
 }
