635804a8a887a013b5c69f45e17dc79f5124e259
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-28 13:44:04 +0100

    Code styling and docs on tools used.

--- add-github-sourcelink.sh
+++ add-github-sourcelink.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-###############################################################################
-# add-github-sourcelink                                                       #
-###############################################################################
-# Adds the Microsoft.SourceLink.GitHub package to projects in the solution or #
-# the project file. Use it in the git repository's root directory.            #
-###############################################################################
+###################################################################################################
+# add-github-sourcelink                                                                           #
+###################################################################################################
+# Adds the Microsoft.SourceLink.GitHub package to projects in the solution or the project file.   #
+# Use it in the git repository's root directory.                                                  #
+#                                                                                                 #
+# Use shellcheck (https://github.com/koalaman/shellcheck) for linting and shell-format            #
+# (https://marketplace.visualstudio.com/items?itemName=foxundermoon.shell-format) for styling.    #
+###################################################################################################
 
-function err() 
-{
+function err() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
 }
 
-function panic() 
-{
+function panic() {
     error_code="$1"
     shift
 
     for line in "$@"; do
         err "$line"
     done
-    
+
     exit "$error_code"
 }
 
-function alter-solution()
-{
+function alter-solution() {
     solution_file="$1"
     [ -f "$solution_file" ] || panic 1 "Couldn't find the solution '$solution_file' in '$PWD'."
 
@@ -38,17 +38,16 @@ function alter-solution()
     done
 }
 
-function alter-project()
-{
+function alter-project() {
     project_file="$1"
     [ -f "$project_file" ] || panic 4 "Couldn't find the project '$project_file' in '$PWD'."
-    
+
     dotnet add "$project_file" package Microsoft.SourceLink.GitHub
 }
 
 if [ -f "$1" ]; then
     alter-solution "$1"
-elif solutions=(./*.sln); (( ${#solutions[@]} )); then
+elif solutions=(./*.sln) && ((${#solutions[@]})); then
     for solution in "${solutions[@]}"; do
         alter-solution "$solution"
     done
