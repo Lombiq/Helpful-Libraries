f6a081b8fa99bc8656b1febabf80015b828676cf
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-28 13:34:35 +0100

    Fix alter-solution to actually use the provided argument.

--- add-github-sourcelink.sh
+++ add-github-sourcelink.sh
@@ -26,19 +26,22 @@ function panic()
 
 function alter-solution()
 {
-    for project_path in $(dotnet sln list | sed '1,2 D'); do
+    solution_file="$1"
+    [ -f "$solution_file" ] || panic 1 "Couldn't find the solution '$solution_file' in '$PWD'."
+
+    for project_path in $(dotnet sln solution_file list | sed '1,2 D'); do
         directory=$(dirname "$project_path")
 
-        pushd "$directory" || panic 1 "Couldn't open the project directory '$directory'."
-        alter-project "$(basename $project_path)"
-        popd || panic 2 "Couldn't return to the original directory."
+        pushd "$directory" || panic 2 "Couldn't open the project directory '$directory'."
+        alter-project "$(basename "$project_path")"
+        popd || panic 3 "Couldn't return to the original directory."
     done
 }
 
 function alter-project()
 {
     project_file="$1"
-    [ -f "$project_file" ] || panic 3 "Couldn't find the project '$project_file' in '$PWD'."
+    [ -f "$project_file" ] || panic 4 "Couldn't find the project '$project_file' in '$PWD'."
     
     dotnet add "$project_file" package Microsoft.SourceLink.GitHub
 }
