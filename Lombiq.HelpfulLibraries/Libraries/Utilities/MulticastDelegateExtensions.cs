0462da4170229f974787562c255f732b154c4c90
Dávid El-Saig <david.el-saig@lombiq.com>
2022-02-05 01:04:06 +0100

    Update Lombiq.HelpfulLibraries/Libraries/Utilities/MulticastDelegateExtensions.cs
    Co-authored-by: 0liver <0liver@users.noreply.github.com>

--- Lombiq.HelpfulLibraries/Libraries/Utilities/MulticastDelegateExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Utilities/MulticastDelegateExtensions.cs
@@ -10,7 +10,6 @@ namespace System
         /// Invokes all async delegates in the given <see cref="MulticastDelegate"/> sequentially.
         /// </summary>
         /// <typeparam name="TDelegate">Type of the delegates in the given <see cref="MulticastDelegate"/>.</typeparam>
-
         /// <param name="delegateExecution">The function to execute each delegate with.</param>
         /// <returns>The <see cref="Task"/> that'll complete when all items have completed.</returns>
         public static Task InvokeAsync<TDelegate>(
