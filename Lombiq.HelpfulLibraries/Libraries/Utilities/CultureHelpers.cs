bc172c755302fc91d59d2ef88b9db997fc2d6ed1
Márk Bartha <barthamark@gmail.com>
2022-02-14 12:30:12 +0100

    Adding CultureHelpers to retrieve countries

---
+++ Lombiq.HelpfulLibraries/Libraries/Utilities/CultureHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Lombiq.HelpfulLibraries.Libraries.Utilities
+{
+    public static class CultureHelpers
+    {
+        public static IEnumerable<Country> GetCountries() =>
+            CultureInfo.GetCultures(CultureTypes.SpecificCultures).Select(culture =>
+                {
+                    var regionInfo = new RegionInfo(culture.LCID);
+                    return new Country
+                    {
+                        TwoLetterIsoCode = regionInfo.TwoLetterISORegionName,
+                        CountryName = regionInfo.EnglishName,
+                    };
+                })
+                .Distinct()
+                .OrderBy(country => country.CountryName);
+    }
+
+    public class Country
+    {
+        public string TwoLetterIsoCode { get; set; }
+        public string CountryName { get; set; }
+    }
+}
