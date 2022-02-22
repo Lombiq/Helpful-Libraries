ddcc239b2012f259b29cf00d3b032dc3eadff9a1
Dávid El-Saig <david.el-saig@lombiq.com>
2022-02-22 22:40:25 +0100

    TypedRoute cast to RouteValues & NavigationItemBuilder extension method.

---
+++ Lombiq.HelpfulLibraries/Libraries/Navigation/NavigationBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Lombiq.HelpfulLibraries.Libraries.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Environment.Extensions;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OrchardCore.Navigation
+{
+    public static class NavigationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a link to the menu item using <see cref="TypedRoute"/>.
+        /// </summary>
+        public static NavigationItemBuilder Action<TContext>(
+            this NavigationItemBuilder builder,
+            HttpContext httpContext,
+            Expression<Func<TContext, Task<IActionResult>>> actionExpression,
+            params (string Key, object Value)[] additionalArguments)
+            where TContext : ControllerBase =>
+            builder.Action(httpContext, actionExpression.StripResult(), additionalArguments);
+
+        /// <summary>
+        /// Adds a link to the menu item using <see cref="TypedRoute"/>.
+        /// </summary>
+        public static NavigationItemBuilder Action<TContext>(
+            this NavigationItemBuilder builder,
+            HttpContext httpContext,
+            Expression<Action<TContext>> actionExpression,
+            params (string Key, object Value)[] additionalArguments)
+            where TContext : ControllerBase
+        {
+            var provider = httpContext.RequestServices.GetService<ITypeFeatureProvider>();
+            var route = TypedRoute.CreateFromExpression(
+                actionExpression,
+                additionalArguments,
+                provider);
+
+            return builder.Action(route);
+        }
+    }
+}
