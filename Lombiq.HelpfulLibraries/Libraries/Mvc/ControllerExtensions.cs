0fd2c3e53bd283f9619e163e6602b96c44aaeac8
Dávid El-Saig <david.el-saig@lombiq.com>
2022-02-25 19:54:51 +0100

    Add SafeJsonAsync extension method.

--- Lombiq.HelpfulLibraries/Libraries/Mvc/ControllerExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Mvc/ControllerExtensions.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 using OrchardCore.ContentManagement;
+using System;
+using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Mvc
 {
@@ -25,5 +30,32 @@ namespace Microsoft.AspNetCore.Mvc
         /// </summary>
         public static RedirectResult RedirectToContentDisplay(this Controller controller, IContent content) =>
             controller.Redirect(controller.Url.DisplayContentItem(content));
+
+        /// <summary>
+        /// Similar to <c>controller.Json(data)</c>, but catches any exception in the <paramref name="dataFactory"/> and
+        /// if one happens returns a JSON with the <c>error</c> property. If run from a local dev machine the
+        /// <c>data</c> property is also filled with the exception string.
+        /// </summary>
+        public static async Task<JsonResult> SafeJsonAsync<T>(this Controller controller, Func<Task<T>> dataFactory)
+        {
+            try
+            {
+                return controller.Json(await dataFactory());
+            }
+            catch (Exception exception)
+            {
+                var context = controller.HttpContext;
+                return controller.Json(context.IsDevelopmentAndLocalhost()
+                    ? new { error = exception.Message, data = exception.ToString() }
+                    : new
+                    {
+                        error = context
+                            .RequestServices
+                            .GetService<IStringLocalizerFactory>()?
+                            .Create(controller.GetType())["An error has occurred while trying to process your request."]
+                            .Value ?? "An error has occurred while trying to process your request.",
+                    });
+            }
+        }
     }
 }
