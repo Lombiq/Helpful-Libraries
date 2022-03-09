bd2906ae03790e939f5c417eacc83c9826d8b611
Dávid El-Saig <david.el-saig@lombiq.com>
2022-03-09 10:25:32 +0100

    Needs to be async.

--- Lombiq.HelpfulLibraries/Libraries/AspNetCore/TemporaryResponseWrapper.cs
+++ Lombiq.HelpfulLibraries/Libraries/AspNetCore/TemporaryResponseWrapper.cs
@@ -1,15 +1,16 @@
 using Microsoft.AspNetCore.Http;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Lombiq.HelpfulLibraries.Libraries.AspNetCore
 {
     /// <summary>
-    /// An  <see cref="IDisposable"/> that replaces the <see cref="HttpContext"/>'s response stream at creation with a
-    /// <see cref="MemoryStream"/> and copies its content back into the real response stream during disposal. It doesn't
-    /// dispose anything in the <see cref="HttpContext"/>.
+    /// An  <see cref="IAsyncDisposable"/> that replaces the <see cref="HttpContext"/>'s response stream at creation
+    /// with a <see cref="MemoryStream"/> and copies its content back into the real response stream during disposal. It
+    /// doesn't dispose anything in the <see cref="HttpContext"/>.
     /// </summary>
-    public sealed class TemporaryResponseWrapper : IDisposable
+    public sealed class TemporaryResponseWrapper : IAsyncDisposable
     {
         private readonly HttpContext _httpContext;
         private readonly MemoryStream _temporaryStream;
@@ -24,19 +25,19 @@ namespace Lombiq.HelpfulLibraries.Libraries.AspNetCore
             httpContext.Response.Body = _temporaryStream;
         }
 
-        public void Dispose()
-        {
-            _temporaryStream.Seek(0, SeekOrigin.Begin);
-            _temporaryStream.CopyTo(_originalBody);
-            _httpContext.Response.Body = _originalBody;
-
-            _temporaryStream.Dispose();
-        }
-
         public void Reset()
         {
             _temporaryStream.Seek(0, SeekOrigin.Begin);
             _temporaryStream.SetLength(0);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            _temporaryStream.Seek(0, SeekOrigin.Begin);
+            await _temporaryStream.CopyToAsync(_originalBody, _httpContext.RequestAborted);
+            _httpContext.Response.Body = _originalBody;
+
+            await _temporaryStream.DisposeAsync();
         }
     }
 }
