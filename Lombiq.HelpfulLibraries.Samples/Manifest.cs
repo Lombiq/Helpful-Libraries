dc7267355d180001ea83f0c524eadee33c40e403
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-03-17 19:35:53 +0100

    Updating version number

--- Lombiq.HelpfulLibraries.Samples/Manifest.cs
+++ Lombiq.HelpfulLibraries.Samples/Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Lombiq Helpful Libraries - Samples",
     Author = "Lombiq Technologies",
     Website = "https://github.com/Lombiq/Helpful-Libraries",
-    Version = "1.0.0",
+    Version = "2.1.0",
     Description = "Samples for Lombiq Helpful Libraries.",
     Category = "Development"
 )]
