2a5121fa8da6e174b5ddf564f2bd23614ae0894a
Dávid El-Saig <david.el-saig@lombiq.com>
2022-03-24 23:17:30 +0100

    Improved and renamed shape.PropertiesAs to shape.As.

--- Lombiq.HelpfulLibraries/Docs/ShapesLibraries.md
+++ Lombiq.HelpfulLibraries/Docs/ShapesLibraries.md
@@ -9,4 +9,4 @@
 ## Extensions
 
 - `ServiceCollectionExtensions`: The `AddShapeRenderer` method adds the `ShapeRenderer` to the service collection.
-- `ShapeExtensions`: The `shape.PropertiesAs<T>()` method converts the shape properties into the provided type (basically duck typing) so you can restore the type you used e.g. in the driver.
+- `ShapeExtensions`: The `shape.As<T>()` method converts the into the provided type (basically duck typing) so you can restore the type you used e.g. in the driver.
--- Lombiq.HelpfulLibraries/Libraries/Shapes/ShapeExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Shapes/ShapeExtensions.cs
@@ -1,4 +1,5 @@
 using OrchardCore.DisplayManagement;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Lombiq.HelpfulLibraries.Libraries.Shapes;
@@ -9,14 +10,15 @@ public static class ShapeExtensions
     /// Creates a new instance of <typeparamref name="T"/> and sets its properties from the <paramref name="shape"/>'s
     /// <see cref="IShape.Properties"/> with the same keys.
     /// </summary>
-    public static T PropertiesAs<T>(this IShape shape)
+    public static T As<T>(this IShape shape)
         where T : new()
     {
         var result = new T();
 
+        var shapeProperties = shape.GetType().GetProperties().ToDictionary(property => property.Name);
         foreach (var property in typeof(T).GetProperties())
         {
-            if (shape.Properties.FirstOrDefault(pair => pair.Key == property.Name) is var (key, value) && key == property.Name)
+            if ((shape.Properties.GetMaybe(property.Name) ?? shapeProperties.GetMaybe(property.Name)?.GetValue(shape)) is { } value)
             {
                 property.SetValue(result, value);
             }
