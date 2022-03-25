efa65c3dc3169f9edd5c94ee7d3d5fb5f80fc7e3
Dávid El-Saig <david.el-saig@lombiq.com>
2022-03-25 15:10:48 +0100

    Code styling.

--- Lombiq.HelpfulLibraries/Libraries/Navigation/NavigationItemBuilderExtensions.cs
+++ Lombiq.HelpfulLibraries/Libraries/Navigation/NavigationItemBuilderExtensions.cs
@@ -7,38 +7,36 @@ using System;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace OrchardCore.Navigation
+namespace OrchardCore.Navigation;
+public static class NavigationItemBuilderExtensions
 {
-    public static class NavigationItemBuilderExtensions
+    /// <summary>
+    /// Adds a link to the menu item using <see cref="TypedRoute"/>.
+    /// </summary>
+    public static NavigationItemBuilder ActionTask<TContext>(
+        this NavigationItemBuilder builder,
+        HttpContext httpContext,
+        Expression<Func<TContext, Task<IActionResult>>> actionExpression,
+        params (string Key, object Value)[] additionalArguments)
+        where TContext : ControllerBase =>
+        builder.Action(httpContext, actionExpression.StripResult(), additionalArguments);
+
+    /// <summary>
+    /// Adds a link to the menu item using <see cref="TypedRoute"/>.
+    /// </summary>
+    public static NavigationItemBuilder Action<TContext>(
+        this NavigationItemBuilder builder,
+        HttpContext httpContext,
+        Expression<Action<TContext>> actionExpression,
+        params (string Key, object Value)[] additionalArguments)
+        where TContext : ControllerBase
     {
-        /// <summary>
-        /// Adds a link to the menu item using <see cref="TypedRoute"/>.
-        /// </summary>
-        public static NavigationItemBuilder ActionTask<TContext>(
-            this NavigationItemBuilder builder,
-            HttpContext httpContext,
-            Expression<Func<TContext, Task<IActionResult>>> actionExpression,
-            params (string Key, object Value)[] additionalArguments)
-            where TContext : ControllerBase =>
-            builder.Action(httpContext, actionExpression.StripResult(), additionalArguments);
+        var provider = httpContext.RequestServices.GetService<ITypeFeatureProvider>();
+        var route = TypedRoute.CreateFromExpression(
+            actionExpression,
+            additionalArguments,
+            provider);
 
-        /// <summary>
-        /// Adds a link to the menu item using <see cref="TypedRoute"/>.
-        /// </summary>
-        public static NavigationItemBuilder Action<TContext>(
-            this NavigationItemBuilder builder,
-            HttpContext httpContext,
-            Expression<Action<TContext>> actionExpression,
-            params (string Key, object Value)[] additionalArguments)
-            where TContext : ControllerBase
-        {
-            var provider = httpContext.RequestServices.GetService<ITypeFeatureProvider>();
-            var route = TypedRoute.CreateFromExpression(
-                actionExpression,
-                additionalArguments,
-                provider);
-
-            return builder.Action(route);
-        }
+        return builder.Action(route);
     }
 }
