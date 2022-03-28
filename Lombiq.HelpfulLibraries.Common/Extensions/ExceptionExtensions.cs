1e51ca36064f7d5e5de816cb9400eb1e167d7dcd
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-03-28 20:48:31 +0200

    Grammar
    Co-authored-by: Dávid El-Saig <david.el-saig@lombiq.com>

--- Lombiq.HelpfulLibraries.Common/Extensions/ExceptionExtensions.cs
+++ Lombiq.HelpfulLibraries.Common/Extensions/ExceptionExtensions.cs
@@ -6,7 +6,7 @@ namespace System;
 public static class ExceptionExtensions
 {
     /// <summary>
-    /// Checks if the exception is one that the application can't recover from.
+    /// Checks if the application can't recover from this type of exception.
     /// </summary>
     public static bool IsFatal(this Exception ex) => ex is OutOfMemoryException or SecurityException or SEHException;
 }
