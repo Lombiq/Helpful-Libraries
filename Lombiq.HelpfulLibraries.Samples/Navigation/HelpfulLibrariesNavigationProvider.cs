556eb4b97ecf27a0e14ac1366ad8689a49f3f894
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-01 12:37:29 +0200

    Fix sample menu.

--- Lombiq.HelpfulLibraries.Samples/Navigation/HelpfulLibrariesNavigationProvider.cs
+++ Lombiq.HelpfulLibraries.Samples/Navigation/HelpfulLibrariesNavigationProvider.cs
@@ -20,14 +20,14 @@ public class HelpfulLibrariesNavigationProvider : MainMenuNavigationProviderBase
         var context = _hca.HttpContext;
         builder
             .Add(T["Helpful Libraries"], builder => builder
-                .Add(T["LINQ to DB"])
+                .Add(T["LINQ to DB"], subMenu => subMenu.Url("#").AddClass("menuWidget__link_title"))
                 .Add(T["Simple Query"], subMenu => subMenu
                     .ActionTask<LinqToDbSamplesController>(context, controller => controller.SimpleQuery()))
                 .Add(T["Join Query"], subMenu => subMenu
                     .ActionTask<LinqToDbSamplesController>(context, controller => controller.JoinQuery()))
                 .Add(T["CRUD"], subMenu => subMenu
                     .ActionTask<LinqToDbSamplesController>(context, controller => controller.Crud()))
-                .Add(T["---"])
+                .Add(T["---"], subMenu => subMenu.Url("#"))
                 .Add(T["Typed Route"], itemBuilder => itemBuilder
                     .Action<TypedRouteController>(context, controller => controller.Index())));
     }
