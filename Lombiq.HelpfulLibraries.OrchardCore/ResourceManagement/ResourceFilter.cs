73a6e075c8812df470b060f4676174fd85796497
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-16 18:10:36 +0200

    Add async to ResourceFilter.

--- Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceFilter.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceFilter.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Http;
 using OrchardCore.ResourceManagement;
 using System;
+using System.Threading.Tasks;
 
 namespace Lombiq.HelpfulLibraries.OrchardCore.ResourceManagement;
 
 public class ResourceFilter
 {
     public Func<HttpContext, bool> Filter { get; set; }
+    public Func<HttpContext, Task<bool>> FilterAsync { get; set; }
     public Action<IResourceManager> Execution { get; set; }
 
     public void Execute(Action<IResourceManager> action) => Execution = action;
