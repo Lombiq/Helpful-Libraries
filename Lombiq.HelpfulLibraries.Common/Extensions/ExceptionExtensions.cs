ad67c4e4121433fbcbd97987c4e4005b90df02f5
Dávid El-Saig <david.el-saig@lombiq.com>
2022-05-07 15:07:11 +0200

    Exception.GetAllMessages

--- Lombiq.HelpfulLibraries.Common/Extensions/ExceptionExtensions.cs
+++ Lombiq.HelpfulLibraries.Common/Extensions/ExceptionExtensions.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Security;
 
@@ -9,4 +10,29 @@ public static class ExceptionExtensions
     /// Checks if the application can't recover from this type of exception.
     /// </summary>
     public static bool IsFatal(this Exception ex) => ex is OutOfMemoryException or SecurityException or SEHException;
+
+    public static IEnumerable<string> GetAllMessages(this Exception exception)
+    {
+        var messages = new List<string>();
+
+        static void Iterate(IList<string> messages, Exception current)
+        {
+            if (!string.IsNullOrWhiteSpace(current.Message)) messages.Add(current.Message);
+
+            switch (current.InnerException)
+            {
+                case AggregateException { InnerExceptions: { } innerExceptions }:
+                    foreach (var innerItem in innerExceptions) Iterate(messages, innerItem);
+                    return;
+                case { } inner:
+                    Iterate(messages, inner);
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        Iterate(messages, exception);
+        return messages;
+    }
 }
