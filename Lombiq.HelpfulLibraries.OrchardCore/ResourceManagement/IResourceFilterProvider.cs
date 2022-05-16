24d3f6ee50978b092ec5aae5e1dcee7c600da09c
Dávid El-Saig <david.el-saig@lombiq.com>
2022-05-16 23:52:02 +0200

    Improve resource management with theme condition.

--- Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/IResourceFilterProvider.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/IResourceFilterProvider.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Lombiq.HelpfulLibraries.OrchardCore.ResourceManagement;
 
 /// <summary>
@@ -9,4 +11,10 @@ public interface IResourceFilterProvider
     /// Adds static resources to the pipeline that will be loaded based on various criteria.
     /// </summary>
     void AddResourceFilter(ResourceFilterBuilder builder);
+}
+
+public static class ResourceFilterProviderExtensions
+{
+    public static IEnumerable<string> GetRequiredThemes(this IResourceFilterProvider provider) =>
+        ResourceFilterThemeRequirementAttribute.GetRequirementsByType(provider.GetType());
 }
---
+++ Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceFilterThemeRequirementAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Lombiq.HelpfulLibraries.OrchardCore.ResourceManagement;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class ResourceFilterThemeRequirementAttribute : Attribute
+{
+    public string Theme { get; }
+
+    public ResourceFilterThemeRequirementAttribute(string theme) => Theme = theme;
+
+    public static IEnumerable<string> GetRequirementsByType(Type type) =>
+        type
+            .GetCustomAttributes<ResourceFilterThemeRequirementAttribute>(inherit: false)
+            .SelectWhere(attribute => attribute.Theme, theme => !string.IsNullOrEmpty(theme));
+}
