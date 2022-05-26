dd4adc37a388fc65a75c76cc703dafd0c02e456f
Szilárd Csere <sylord0803@gmail.com>
2022-05-26 16:38:44 +0200

    Update Lombiq.HelpfulLibraries.LinqToDb/ITableAccessor.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- Lombiq.HelpfulLibraries.LinqToDb/ITableAccessor.cs
+++ Lombiq.HelpfulLibraries.LinqToDb/ITableAccessor.cs
@@ -11,7 +11,10 @@ public interface ITableAccessor
     /// Returns a table for query building purposes.
     /// </summary>
     /// <typeparam name="T">Type of the object representing the table in the database.</typeparam>
-    /// <param name="collectionName">Name of collection that includes logically related objects. It is technically a prefix to the affected tables in the database.</param>
+    /// <param name="collectionName">
+    /// Name of the YesSql collection that includes logically related objects. It is technically a prefix to the
+    /// affected tables in the database.
+    /// </param>
     /// <returns>Linq2db table for query building purposes.</returns>
     ITable<T> GetTable<T>(string collectionName = null)
         where T : class;
