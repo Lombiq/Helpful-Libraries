599ae68952c7d25117db2f288631e12583e05be9
József Horváth <info@ministro.hu>
2022-06-07 21:31:58 +0200

    updating manifest version

--- Lombiq.HelpfulLibraries.Samples/Manifest.cs
+++ Lombiq.HelpfulLibraries.Samples/Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Lombiq Helpful Libraries - Samples",
     Author = "Lombiq Technologies",
     Website = "https://github.com/Lombiq/Helpful-Libraries",
-    Version = "4.3.0",
+    Version = "4.3.1-alpha.osoe-111",
     Description = "Samples for Lombiq Helpful Libraries.",
     Category = "Development"
 )]
