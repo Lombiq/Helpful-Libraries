f404187ca74edcb2b51b7faf7147126efa0ef362
Tamas Vincze <tamas.vincze@lombiq.com>
2022-07-22 20:31:34 +0200

    Checking for Media is currently enabled

--- Lombiq.HelpfulLibraries.Common/Extensions/ConfigurationExtensions.cs
+++ Lombiq.HelpfulLibraries.Common/Extensions/ConfigurationExtensions.cs
@@ -9,4 +9,12 @@ public static class ConfigurationExtensions
     public static bool IsAzureHosting(
         this IConfiguration configuration) =>
         configuration.GetValue<bool>("OrchardCore:IsAzureHosting");
+
+    /// <summary>
+    /// Retrieves a value indicating whether the <c>OrchardCore:IsMedia</c> configuration key is set to <see
+    /// langword="true"/>.
+    /// </summary>
+    public static bool IsMedia(
+        this IConfiguration configuration) =>
+        configuration.GetValue<bool>("OrchardCore.Media");
 }
