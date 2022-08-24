1b55dd20b3d7603b01d1eebc249095fcf8df5184
Gábor Pór <gabor.por@lombiq.com>
2022-08-24 11:14:12 +0200

    Removing now redundant extension

--- Lombiq.HelpfulLibraries.Common/Extensions/ConfigurationExtensions.cs
+++ Lombiq.HelpfulLibraries.Common/Extensions/ConfigurationExtensions.cs
@@ -9,12 +9,4 @@ public static class ConfigurationExtensions
     public static bool IsAzureHosting(
         this IConfiguration configuration) =>
         configuration.GetValue<bool>("OrchardCore:IsAzureHosting");
-
-    /// <summary>
-    /// Retrieves a value indicating whether the <c>OrchardCore:IsMedia</c> configuration key is set to <see
-    /// langword="true"/>.
-    /// </summary>
-    public static bool IsMedia(
-        this IConfiguration configuration) =>
-        configuration.GetValue<bool>("OrchardCore.Media");
 }
