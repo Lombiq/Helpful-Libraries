cc97308b0f8578e6b620d8875b97ee08fafa60c6
Gábor Pór <gabor.por@lombiq.com>
2022-10-28 10:24:01 +0200

    Temporarily removing reference

--- Lombiq.HelpfulLibraries.OrchardCore/Environment/OrchardCoreBuilderExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Environment/OrchardCoreBuilderExtensions.cs
@@ -1,4 +1,4 @@
-using Lombiq.Hosting.Tenants.FeaturesGuard.Models;
+//using Lombiq.Hosting.Tenants.FeaturesGuard.Models;
 using Microsoft.Extensions.Configuration;
 using OrchardCore.Email;
 using OrchardCore.Environment.Shell.Configuration;
@@ -49,18 +49,18 @@ public static class OrchardCoreBuilderExtensions
     /// <summary>
     /// Makes <see cref="ConditionallyEnabledFeaturesOptions"/> available for use.
     /// </summary>
-    public static OrchardCoreBuilder ConfigureFeaturesGuard(this OrchardCoreBuilder builder)
-    {
-        builder.ConfigureServices((tenantServices, serviceProvider) =>
-        {
-            var shellConfiguration = serviceProvider
-                .GetRequiredService<IShellConfiguration>()
-                .GetSection(
-                    "Lombiq_Hosting_Tenants_FeaturesGuard:ConditionallyEnabledFeaturesOptions:ConditionallyEnabledFeatures");
+    //public static OrchardCoreBuilder ConfigureFeaturesGuard(this OrchardCoreBuilder builder)
+    //{
+    //    builder.ConfigureServices((tenantServices, serviceProvider) =>
+    //    {
+    //        var shellConfiguration = serviceProvider
+    //            .GetRequiredService<IShellConfiguration>()
+    //            .GetSection(
+    //                "Lombiq_Hosting_Tenants_FeaturesGuard:ConditionallyEnabledFeaturesOptions:ConditionallyEnabledFeatures");
 
-            tenantServices.PostConfigure<ConditionallyEnabledFeaturesOptions>(options => shellConfiguration.Bind(options));
-        });
+    //        tenantServices.PostConfigure<ConditionallyEnabledFeaturesOptions>(options => shellConfiguration.Bind(options));
+    //    });
 
-        return builder;
-    }
+    //    return builder;
+    //}
 }
