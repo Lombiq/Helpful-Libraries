9248e79b374e818c987143a9fe305de700788ff8
Dávid El-Saig <david.el-saig@lombiq.com>
2022-11-23 12:27:47 +0100

    Add MarkdownDisplayService.

---
+++ Lombiq.HelpfulLibraries.OrchardCore/Markdown/MarkdownDisplayService.cs
@@ -0,0 +1,46 @@
+﻿using OrchardCore.Infrastructure.Html;
+using OrchardCore.Markdown.Services;
+using OrchardCore.Shortcodes.Services;
+using Shortcodes;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulLibraries.OrchardCore.Markdown;
+
+public class MarkdownDisplayService : IMarkdownDisplayService
+{
+    private readonly IHtmlSanitizerService _htmlSanitizerService;
+    private readonly IMarkdownService _markdownService;
+    private readonly IShortcodeService _shortcodeService;
+    public MarkdownDisplayService(
+        IHtmlSanitizerService htmlSanitizerService,
+        IMarkdownService markdownService,
+        IShortcodeService shortcodeService)
+    {
+        _htmlSanitizerService = htmlSanitizerService;
+        _markdownService = markdownService;
+        _shortcodeService = shortcodeService;
+
+    }
+
+    public async Task<string> ToHtmlAsync(
+        string markdown,
+        bool sanitizeHtml = true,
+        bool processShortcodes = true,
+        Context shortcodesContext = null)
+    {
+        if (string.IsNullOrWhiteSpace(markdown)) return string.Empty;
+
+        var html = _markdownService.ToHtml(markdown);
+
+        // Process shortcodes if desired and if it could contain a shortcode.
+        if (processShortcodes && html.Contains('['))
+        {
+            html = await _shortcodeService.ProcessAsync(html, shortcodesContext ?? new Context());
+        }
+
+        return sanitizeHtml
+            ? _htmlSanitizerService.Sanitize(html ?? string.Empty)
+            : html;
+
+    }
+}
