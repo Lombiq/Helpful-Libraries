31987020005d38366f84f35c992b3c97686971af
Dávid El-Saig <david.el-saig@lombiq.com>
2022-12-12 00:03:30 +0100

    Move MakeFileSystemFriendly to HL.

--- Lombiq.HelpfulLibraries.Common/Extensions/StringExtensions.cs
+++ Lombiq.HelpfulLibraries.Common/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,6 +10,65 @@ namespace System;
 
 public static class StringExtensions
 {
+    // The first array contains every invalid character in Windows. This is the union of GetInvalidFileNameChars() and
+    // GetInvalidPathChars() evaluated on Windows (for FAT and NTFS). Linux file systems aren't as strict, they only
+    // forbid forward slash ('/') and null character ('\0'). This is a subset of the invalid characters on Windows. So
+    // to make the paths work across all systems (e.g. when we plan to back up to an NTFS store or use GitHub's
+    // "actions/upload-artifact" action) we have to carry the banned Windows characters across operating systems.
+    // GetInvalidFileNameChars() and GetInvalidPathChars() are still included in case they contain more characters on
+    // untested operating systems.
+    private static readonly Lazy<char[]> _invalidPathCharacters = new(() => new[]
+        {
+            '\0',
+            '\u0001',
+            '\u0002',
+            '\u0003',
+            '\u0004',
+            '\u0005',
+            '\u0006',
+            '\a',
+            '\b',
+            '\t',
+            '\n',
+            '\v',
+            '\f',
+            '\r',
+            '\u000e',
+            '\u000f',
+            '\u0010',
+            '\u0011',
+            '\u0012',
+            '\u0013',
+            '\u0014',
+            '\u0015',
+            '\u0016',
+            '\u0017',
+            '\u0018',
+            '\u0019',
+            '\u001a',
+            '\u001b',
+            '\u001c',
+            '\u001d',
+            '\u001e',
+            '\u001f',
+            '|',
+            ':',
+            '"',
+            '<',
+            '>',
+            '*',
+            '?',
+        }
+        .Union(Path.GetInvalidFileNameChars())
+        .Union(Path.GetInvalidPathChars())
+        .ToArray());
+
+    public static string MakeFileSystemFriendly(this string text) =>
+        string
+            .Join("_", text.Split(_invalidPathCharacters.Value))
+            .Replace('.', '_')
+            .Replace(' ', '-');
+
     /// <summary>
     /// Returns an array by splitting the input along commas and stripping empty entries.
     /// </summary>
