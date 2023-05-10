4a6377fe47cb04f79d6ad47536cca5e9434ef95a
Dávid El-Saig <david.el-saig@lombiq.com>
2023-05-10 02:31:26 +0200

    EditorFieldSetTagHelper

--- Lombiq.HelpfulLibraries.AspNetCore/Localization/LocalizedHtmlStringExtensions.cs
+++ Lombiq.HelpfulLibraries.AspNetCore/Localization/LocalizedHtmlStringExtensions.cs
@@ -11,17 +11,22 @@ public static class LocalizedHtmlStringExtensions
     /// Returns a raw HTML string that's been JSON serialized and therefore safe to use within <c>&lt;script&gt;</c>
     /// tags in a Razor view.
     /// </summary>
-    public static IHtmlContent Json(this LocalizedHtmlString htmlString)
-    {
-        if (htmlString != null)
-        {
-            using var stringWriter = new StringWriter();
-            htmlString.WriteTo(stringWriter, HtmlEncoder.Default);
-            return new HtmlString(JsonConvert.SerializeObject(
-                stringWriter.ToString(),
-                new JsonSerializerSettings { StringEscapeHandling = StringEscapeHandling.EscapeHtml }));
-        }
+    public static IHtmlContent Json(this LocalizedHtmlString htmlString) =>
+        htmlString?.Html() is { } html
+            ? new HtmlString(JsonConvert.SerializeObject(
+                html,
+                new JsonSerializerSettings { StringEscapeHandling = StringEscapeHandling.EscapeHtml }))
+            : new HtmlString("null");
 
-        return new HtmlString("null");
+    /// <summary>
+    /// Returns a raw HTML string that's not encoded.
+    /// </summary>
+    public static string Html(this IHtmlContent htmlString)
+    {
+        if (htmlString == null) return null;
+
+        using var stringWriter = new StringWriter();
+        htmlString.WriteTo(stringWriter, HtmlEncoder.Default);
+        return stringWriter.ToString();
     }
 }
---
+++ Lombiq.HelpfulLibraries.OrchardCore/TagHelpers/EditorFieldSetTagHelper.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulLibraries.OrchardCore.TagHelpers;
+
+[HtmlTargetElement("fieldset", Attributes = "asp-for")]
+public class EditorFieldSetTagHelper : TagHelper
+{
+    private readonly IHtmlGenerator _htmlGenerator;
+
+    [HtmlAttributeNotBound]
+    [ViewContext]
+    public ViewContext ViewContext { get; set; }
+
+    [HtmlAttributeName("asp-for")]
+    public ModelExpression For { get; set; }
+
+    [HtmlAttributeName("label")]
+    public LocalizedHtmlString Label { get; set; }
+
+    [HtmlAttributeName("hint")]
+    public LocalizedHtmlString Hint { get; set; }
+
+    [HtmlAttributeName("type")]
+    public string InputType { get; set; } = "text";
+
+    public EditorFieldSetTagHelper(IHtmlGenerator htmlGenerator) =>
+        _htmlGenerator = htmlGenerator;
+
+    public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        if (output.Attributes.TryGetAttribute("class", out var classAttribute))
+        {
+            var newValue = $"{classAttribute.Value} form-group";
+            output.Attributes.Remove(classAttribute);
+            output.Attributes.Add("class", newValue);
+        }
+        else
+        {
+            output.Attributes.Add("class", "form-group mb-3");
+        }
+
+        var isCheckbox = InputType.EqualsOrdinalIgnoreCase("checkbox");
+
+        if (isCheckbox) output.Content.AppendHtml("<div class=\"custom-control custom-checkbox\">");
+
+        var label = _htmlGenerator.GenerateLabel(ViewContext, For.ModelExplorer, For.Name, Label.Html(), htmlAttributes: null);
+
+        var input = _htmlGenerator.GenerateTextBox(
+            ViewContext,
+            For.ModelExplorer,
+            For.Name,
+            For.Model,
+            For.ModelExplorer.Metadata.EditFormatString,
+            new { @class = "form-control", type = InputType });
+
+        if (isCheckbox)
+        {
+            input.Attributes["class"] = "custom-control-input";
+            label.Attributes["class"] = "custom-control-label";
+
+            AppendContent(output, input);
+            output.Content.AppendHtml("&nbsp;");
+            AppendContent(output, label);
+        }
+        else
+        {
+            AppendContent(output, label);
+            AppendContent(output, input);
+        }
+
+        if (InputType.EqualsOrdinalIgnoreCase("checkbox")) output.Content.AppendHtml("</div>");
+
+        var tagBuilder = _htmlGenerator.GenerateValidationMessage(
+            ViewContext,
+            For.ModelExplorer,
+            For.Name,
+            message: null,
+            tag: "span",
+            htmlAttributes: null);
+        AppendContent(output, tagBuilder);
+
+        AppendContent(output, Hint);
+
+        return Task.CompletedTask;
+    }
+
+    private static void AppendContent(TagHelperOutput output, IHtmlContent content)
+    {
+        if (content != null) output.Content.AppendHtml(content.Html());
+    }
+}
