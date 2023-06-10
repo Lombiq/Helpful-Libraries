6c729529bb6362292a52f55afb2a9e8db4146c5d
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-10 18:50:39 +0200

    Add missing method docs.

--- Lombiq.HelpfulLibraries.Refit/Helpers/RefitHelper.cs
+++ Lombiq.HelpfulLibraries.Refit/Helpers/RefitHelper.cs
@@ -6,12 +6,29 @@ namespace Lombiq.HelpfulLibraries.Refit.Helpers;
 
 public static class RefitHelper
 {
+    /// <summary>
+    /// Generate a Refit implementation of the specified interface with <c>Newtonsoft.Json</c> as the serializer instead
+    /// of <c>System.Text.Json</c>.
+    /// </summary>
+    /// <param name="hostUrl">Base address the implementation will use.</param>
+    /// <param name="configure">Optional action for configuring other settings.</param>
+    /// <typeparam name="T">Interface to create the implementation for.</typeparam>
+    /// <returns>An instance that implements <typeparamref name="T"/>.</returns>
     public static T WithNewtonsoftJson<T>(string hostUrl, Action<RefitSettings> configure = null) =>
         WithNewtonsoftJson<T>(new Uri(hostUrl), configure);
 
+    /// <inheritdoc cref="WithNewtonsoftJson{T}(string,System.Action{RefitSettings})"/>
     public static T WithNewtonsoftJson<T>(Uri hostUrl, Action<RefitSettings> configure = null) =>
         RestService.For<T>(hostUrl.AbsoluteUri, CreateSettingsWithNewtonsoftJson(configure));
 
+    /// <summary>
+    /// Generate a Refit implementation of the specified interface with <c>Newtonsoft.Json</c> as the serializer instead
+    /// of <c>System.Text.Json</c>.
+    /// </summary>
+    /// <param name="httpClient">The <see cref="HttpClient"/> the implementation will use to send requests.</param>
+    /// <param name="configure">Optional action for configuring other settings.</param>
+    /// <typeparam name="T">Interface to create the implementation for.</typeparam>
+    /// <returns>An instance that implements <typeparamref name="T"/>.</returns>
     public static T WithNewtonsoftJson<T>(HttpClient httpClient, Action<RefitSettings> configure = null) =>
         RestService.For<T>(httpClient, CreateSettingsWithNewtonsoftJson(configure));
 
