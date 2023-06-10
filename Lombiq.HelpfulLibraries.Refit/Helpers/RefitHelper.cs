7d5bfe788efd0179c38289071a480142d0d4d5cf
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-10 07:12:44 +0200

    Add RefitHelper.WithNewtonsoft.

---
+++ Lombiq.HelpfulLibraries.Refit/Helpers/RefitHelper.cs
@@ -0,0 +1,24 @@
+﻿using Refit;
+using System;
+using System.Net.Http;
+
+namespace Lombiq.HelpfulLibraries.Refit.Helpers;
+
+public static class RefitHelper
+{
+    public static T WithNewtonsoft<T>(string hostUrl, Action<RefitSettings> configure = null) =>
+        WithNewtonsoft<T>(new Uri(hostUrl), configure);
+
+    public static T WithNewtonsoft<T>(Uri hostUrl, Action<RefitSettings> configure = null) =>
+        RestService.For<T>(hostUrl.AbsoluteUri, CreateSettingsWithNewtonsoft(configure));
+
+    public static T WithNewtonsoft<T>(HttpClient httpClient, Action<RefitSettings> configure = null) =>
+        RestService.For<T>(httpClient, CreateSettingsWithNewtonsoft(configure));
+
+    private static RefitSettings CreateSettingsWithNewtonsoft(Action<RefitSettings> configure)
+    {
+        var settings = new RefitSettings(new NewtonsoftJsonContentSerializer());
+        configure?.Invoke(settings);
+        return settings;
+    }
+}
--- Lombiq.HelpfulLibraries.Refit/Readme.md
+++ Lombiq.HelpfulLibraries.Refit/Readme.md
@@ -5,3 +5,7 @@
 Adds helpers for working with [Refit](https://github.com/reactiveui/refit).
 
 For general details about and on using the Helpful Libraries see the [root Readme](../Readme.md).
+
+## Helpers
+
+- `RefitHelper`: Adds shortcuts for creating Refit API clients from interfaces. (e.g. `RefitHelper.WithNewtonsoft<T>()`)
