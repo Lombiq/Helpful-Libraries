cbbf70eee1abd8dc11c445e4972013a35a5afbbe
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-20 10:59:51 +0200

    Move IEmailAndPasswordValidator to HL.

---
+++ Lombiq.HelpfulLibraries.OrchardCore/Validation/IEmailAndPasswordValidator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulLibraries.OrchardCore.Validation;
+
+/// <summary>
+/// A service for returning validation errors for email of password.
+/// </summary>
+public interface IEmailAndPasswordValidator
+{
+    /// <summary>
+    /// Validates the provided <paramref name="email"/> and returns any validation errors as a localized string.
+    /// </summary>
+    Task<IEnumerable<LocalizedString>> ValidateEmailAsync(string email);
+
+    /// <summary>
+    /// Validates the provided <paramref name="password"/> and returns any validation errors as a localized string.
+    /// </summary>
+    Task<IEnumerable<LocalizedString>> ValidatePasswordAsync(string password);
+}
