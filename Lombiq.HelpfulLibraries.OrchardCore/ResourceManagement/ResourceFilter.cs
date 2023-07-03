704e3380212256184035afee21e78907a73377e2
Dávid El-Saig <david.el-saig@lombiq.com>
2023-07-04 00:34:15 +0200

    Added RegisterFavoriteIcon.

--- Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceFilter.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceFilter.cs
@@ -34,4 +34,21 @@ public class ResourceFilter
     /// </summary>
     public void RegisterHeadScript(params string[] resources) =>
         Execute(resourceManager => resources.ForEach(resource => resourceManager.RegisterResource("script", resource).AtHead()));
+
+    /// <summary>
+    /// Registers the provided <c>link</c> <paramref name="resources"/> at the head of the page.
+    /// </summary>
+    public void RegisterLink(params LinkEntry[] resources) =>
+        Execute(resourceManager => resources.ForEach(resourceManager.RegisterLink));
+
+    /// <summary>
+    /// Registers an icon <c>link</c> resource with the provided address and attributes.
+    /// </summary>
+    public void RegisterFavoriteIcon(string href, string type = "image/x-icon", string rel = "shortcut icon") =>
+        Execute(resourceManager => resourceManager.RegisterLink(new LinkEntry
+        {
+            Href = href,
+            Rel = rel,
+            Type = type,
+        }));
 }
