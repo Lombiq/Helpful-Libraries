35081b0fa4d8ac3fd9601f85056064d12f389d13
Dávid El-Saig <david.el-saig@lombiq.com>
2023-08-10 12:35:04 +0200

    fix html in SimpleEventActivityDisplayDriverBase

--- Lombiq.HelpfulLibraries.OrchardCore/Workflow/SimpleEventActivityDisplayDriverBase.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Workflow/SimpleEventActivityDisplayDriverBase.cs
@@ -1,6 +1,8 @@
 ﻿using Lombiq.HelpfulLibraries.OrchardCore.Contents;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Localization;
 using OrchardCore.DisplayManagement.Handlers;
+using OrchardCore.DisplayManagement.Html;
 using OrchardCore.DisplayManagement.Views;
 using OrchardCore.Workflows.Activities;
 using OrchardCore.Workflows.Helpers;
@@ -14,23 +16,26 @@ namespace Lombiq.HelpfulLibraries.OrchardCore.Workflow;
 public abstract class SimpleEventActivityDisplayDriverBase<TActivity> : DisplayDriver<IActivity, TActivity>
     where TActivity : class, IActivity
 {
-    public abstract string IconClass { get; }
-    public abstract LocalizedHtmlString Title { get; }
+    public virtual string IconClass { get; }
+    public virtual LocalizedHtmlString Title { get; }
     public abstract LocalizedHtmlString Description { get; }
 
     private string IconHtml => string.IsNullOrEmpty(IconClass) ? string.Empty : $"<i class=\"fa {IconClass}\"></i>";
 
     public override IDisplayResult Display(TActivity model) =>
         Combine(
-            this.RawHtml(ThumbnailHtml()).Location(CommonContentDisplayTypes.Thumbnail, CommonLocationNames.Content),
+            this.RawHtml(ThumbnailHtml(model)).Location(CommonContentDisplayTypes.Thumbnail, CommonLocationNames.Content),
             this.RawHtml(DesignHtml(model)).Location(CommonContentDisplayTypes.Design, CommonLocationNames.Content));
 
-    private string ThumbnailHtml() =>
-        $"<h4 class=\"card-title\">{IconHtml}{Title}</h4><p>{Description}</p>";
+    private string ThumbnailHtml(TActivity model)
+    {
+        var title = ((IHtmlContent)Title) ?? new HtmlContentString(model.DisplayText);
+        return $"<h4 class=\"card-title\">{IconHtml}{title.Html()}</h4><p>{Description.Html()}</p>";
+    }
 
     private string DesignHtml(TActivity model)
     {
         var title = model.GetTitleOrDefault(() => Title);
-        return $"<header><h4>{IconHtml}{title}</h4></header>";
+        return $"<header><h4>{IconHtml}{title.Html()}</h4></header>";
     }
 }
