f319df0f9719e513f776afa2977aea49d4b03d3a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-08-23 00:43:00 +0200

    Changing default distributed locking implementation to a local one

---
+++ Libraries/Tasks/Locking/ObjectLock.cs
@@ -0,0 +1,28 @@
+﻿using Orchard.Environment.Extensions;
+using Piedone.HelpfulLibraries.Tasks.Locking;
+
+namespace Piedone.HelpfulLibraries.Libraries.Tasks.Locking
+{
+    [OrchardFeature("Piedone.HelpfulLibraries.Tasks.Locking")]
+    public class ObjectLock : IDistributedLock
+    {
+        private readonly IObjectLockHolder _objectLockHolder;
+
+        private string _name;
+
+
+        public ObjectLock(IObjectLockHolder objectLockHolder) => _objectLockHolder = objectLockHolder;
+
+
+        public bool TryAcquire(string name)
+        {
+            _name = name;
+            return _objectLockHolder.TryAcquire(name);
+        }
+
+        public void Dispose()
+        {
+            if (!string.IsNullOrEmpty(_name)) _objectLockHolder.Release(_name);
+        }
+    }
+}
