d087f5ba4e90b4d64d4a80121b9956722dadd6c3
Sára El-Saig <sara.el-saig@lombiq.com>
2023-11-22 00:49:00 +0100

    Move CookieCultureScopeMiddleware from SPAL to HL.

---
+++ Lombiq.HelpfulLibraries.OrchardCore/Middlewares/CookieCultureScopeMiddleware.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Localization;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulLibraries.OrchardCore.Middlewares;
+
+/// <summary>
+/// A middleware that looks for the "culture" query string argument or cookie and uses it to initialize a <see
+/// cref="CultureScope"/>.
+/// </summary>
+public class CookieCultureScopeMiddleware
+{
+    public const string CultureKeyName = "culture";
+
+    private readonly RequestDelegate _next;
+
+    public CookieCultureScopeMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!TryGetCultureInfo(context, out var culture))
+        {
+            culture = await context
+                .RequestServices
+                .GetRequiredService<ILocalizationService>()
+                .GetDefaultCultureAsync() ?? "en-US";
+        }
+
+        using var scope = CultureScope.Create(culture, culture, ignoreSystemSettings: true);
+        context.SetCookieForever(CultureKeyName, culture);
+
+        await _next(context);
+    }
+
+    private static bool TryGetCultureInfo(HttpContext context, out string culture)
+    {
+        if (context.Request.Query.TryGetValue(CultureKeyName, out var queryCulture))
+        {
+            culture = queryCulture[0];
+        }
+        else
+        {
+            context.Request.Cookies.TryGetValue(CultureKeyName, out culture);
+        }
+
+        if (string.IsNullOrWhiteSpace(culture)) return false;
+
+        try
+        {
+            culture = new CultureInfo(culture).Name;
+            return true;
+        }
+        catch
+        {
+            culture = null;
+            return false;
+        }
+    }
+}
