ae5bb5602f345f236f0501bcfdf39840f88df1ac
Sára El-Saig <david.el-saig@lombiq.com>
2023-12-17 11:18:38 +0100

    Add GetQueryWithout extension.

---
+++ Lombiq.HelpfulLibraries.AspNetCore/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace Microsoft.AspNetCore.Http;
+
+public static class HttpRequestExtensions
+{
+    /// <summary>
+    /// Returns the query string without the leading <c>?</c> and without the keys in <paramref name="keysToExclude"/>.
+    /// </summary>
+    public static string GetQueryWithout(this HttpRequest request, params string[] keysToExclude)
+    {
+        var query = HttpUtility.ParseQueryString(request.QueryString.Value ?? string.Empty);
+        return string.Join('&', query
+            .AllKeys
+            .Where(key => key != null && !keysToExclude.Exists(key.EqualsOrdinalIgnoreCase))
+            .Select(key => $"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(query[key] ?? string.Empty)}"));
+    }
+}
