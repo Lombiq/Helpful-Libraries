7a824cd80965aa0433f0d486cda42f5e43225869
Roland Márkus <roland.markus@lombiq.com>
2023-12-21 17:12:35 +0100

    Hotfix.

--- Lombiq.HelpfulLibraries.OrchardCore/Contents/ContentTypePartDefinitionBuilderExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Contents/ContentTypePartDefinitionBuilderExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using OrchardCore.ContentManagement.Metadata.Builders;
-using OrchardCore.ContentManagement.Metadata.Settings;
 using System;
 
 namespace OrchardCore.ContentManagement.Metadata.Settings;
@@ -7,7 +6,7 @@ namespace OrchardCore.ContentManagement.Metadata.Settings;
 public static class ContentTypePartDefinitionBuilderExtensions
 {
     /// <summary>
-    /// Sets the type's editor using an <see cref="Enum"/> parameter.
+    /// Sets the part's editor using an <see cref="Enum"/> parameter.
     /// </summary>
     public static ContentTypePartDefinitionBuilder WithEditor(this ContentTypePartDefinitionBuilder builder, Enum editor) =>
         builder.MergeSettings<ContentTypePartSettings>(x => x.Editor = editor.ToString());
