339741e4e024c01ab7e49a8f429e4092a3b8b70e
Sára El-Saig <david.el-saig@lombiq.com>
2023-12-22 21:02:49 +0100

    Secure anti forgery token.

---
+++ Lombiq.HelpfulLibraries.OrchardCore/Security/OrchardCoreBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class SecurityOrchardCoreBuilderExtensions
+{
+    /// <summary>
+    /// Configures the anti-forgery token to be always secure. With this configuration the token won't work in an HTTP
+    /// environment so make sure that HTTPS redirection is enabled.
+    /// </summary>
+    public static OrchardCoreBuilder ConfigureAntiForgeryAlwaysSecure(this OrchardCoreBuilder builder) =>
+        builder.ConfigureServices((services, _) =>
+            services.Configure<AntiforgeryOptions>(options => options.Cookie.SecurePolicy = CookieSecurePolicy.Always));
+}
