c761c229b2e03dcbcf2abb32eb65ecc1abdb8136
Sára El-Saig <david.el-saig@lombiq.com>
2023-12-22 14:42:27 +0100

    Add middleware extension method and extension point for building Content-Security-Policy header.

---
+++ Lombiq.HelpfulLibraries.AspNetCore/Security/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Lombiq.HelpfulLibraries.AspNetCore.Security;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using static Lombiq.HelpfulLibraries.AspNetCore.Security.ContentSecurityPolicyDirectives;
+
+namespace Microsoft.AspNetCore.Builder;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder AddContentSecurityPolicyHeader(this IApplicationBuilder app) =>
+        app.Use(async (context, next) =>
+        {
+            var securityPolicies = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                [DefaultSrc] = CommonValues.Self,
+            };
+
+            foreach (var provider in context.RequestServices.GetService<IEnumerable<IContentSecurityPolicyProvider>>())
+            {
+                await provider.UpdateAsync(securityPolicies);
+            }
+
+            var policy = string.Join("; ", securityPolicies.Select((key, value) => $"{key} {value}"));
+            context.Response.Headers.Add("Content-Security-Policy", policy);
+
+            await next();
+        });
+}
---
+++ Lombiq.HelpfulLibraries.AspNetCore/Security/IContentSecurityPolicyProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulLibraries.AspNetCore.Security;
+
+/// <summary>
+/// A service for updating the dictionary that will be turned into the <c>Content-Security-Policy</c> header value by
+/// <see cref="ApplicationBuilderExtensions.AddContentSecurityPolicyHeader"/>.
+/// </summary>
+public interface IContentSecurityPolicyProvider
+{
+    /// <summary>
+    /// Updates the <paramref name="securityPolicies"/> dictionary where the keys are the <c>Content-Security-Policy</c>
+    /// directives names and the values are the matching directive values.
+    /// </summary>
+    public ValueTask UpdateAsync(IDictionary<string, string> securityPolicies);
+}
