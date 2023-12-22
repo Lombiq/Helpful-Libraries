c3beb82dab7be68a08a848b75ed0824b8e8b3e19
Sára El-Saig <david.el-saig@lombiq.com>
2023-12-22 21:04:36 +0100

    Change default SameSite attribute of SetCookieForever.

--- Lombiq.HelpfulLibraries.AspNetCore/Extensions/CookieHttpContextExtensions .cs
+++ Lombiq.HelpfulLibraries.AspNetCore/Extensions/CookieHttpContextExtensions .cs
@@ -7,12 +7,13 @@ public static class CookieHttpContextExtensions
     /// <summary>
     /// Sets the cookie with the given name with a maximal expiration time.
     /// </summary>
-    public static void SetCookieForever(this HttpContext httpContext, string name, string value) =>
+    public static void SetCookieForever(this HttpContext httpContext, string name, string value, SameSiteMode sameSite = SameSiteMode.Lax) =>
         httpContext.Response.Cookies.Append(name, value, new CookieOptions
         {
             Expires = DateTimeOffset.MaxValue,
             Secure = true,
             HttpOnly = true,
+            SameSite = sameSite,
         });
 
     /// <summary>
