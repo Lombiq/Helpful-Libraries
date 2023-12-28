167aa7f7ddd8919f5b01aac67cb15a8f107d4580
Roland Márkus <roland.markus@lombiq.com>
2023-12-28 10:46:04 +0100

    Adding descriptions.

--- Lombiq.HelpfulLibraries.OrchardCore/Contents/ContentFieldEditorEnums.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Contents/ContentFieldEditorEnums.cs
@@ -1,7 +1,14 @@
-﻿namespace Lombiq.HelpfulLibraries.OrchardCore.Contents;
+﻿using System.ComponentModel;
 
+namespace Lombiq.HelpfulLibraries.OrchardCore.Contents;
+
+/// <summary>
+/// Collection of different ContentField editors.
+/// Feel free to extend this class by adding more Enum classes with ContentField editors.
+/// </summary>
 public static class ContentFieldEditorEnums
 {
+    [Description("Editor options for HtmlFields.")]
     public enum HtmlFieldEditors
     {
         Monaco,
@@ -10,17 +17,20 @@ public static class ContentFieldEditorEnums
         Wysiwyg,
     }
 
+    [Description("Editor options for MarkdownFields. Wysiwyg editor can be used for MarkdownBodyParts aswell.")]
     public enum MarkdownFieldEditors
     {
         TextArea,
         Wysiwyg
     }
 
+    [Description("Editor options for TaxonomyFields.")]
     public enum TaxonomyFieldEditors
     {
         Tags,
     }
 
+    [Description("Editor options for TextFields.")]
     public enum TextFieldEditors
     {
         CodeMirror,
