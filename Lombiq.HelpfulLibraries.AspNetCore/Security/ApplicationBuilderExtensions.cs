539ac6e5bf5d943ae955bf52944c8126e4fcf675
Sára El-Saig <sara.el-saig@lombiq.com>
2023-12-29 14:49:10 +0100

    Annoying SA1629 doesn't let me have a block of quote on its own

--- Lombiq.HelpfulLibraries.AspNetCore/Security/ApplicationBuilderExtensions.cs
+++ Lombiq.HelpfulLibraries.AspNetCore/Security/ApplicationBuilderExtensions.cs
@@ -56,11 +56,11 @@ public static class ApplicationBuilderExtensions
     /// Adds a middleware that sets the <c>X-Content-Type-Options</c> header to <c>nosniff</c>.
     /// </summary>
     /// <remarks><para>
-    /// As per <a href="https://www.zaproxy.org/docs/alerts/10021/">the documentation</a>: "The Anti-MIME-Sniffing
-    /// header X-Content-Type-Options was not set to ’nosniff’. This allows older versions of Internet Explorer and
-    /// Chrome to perform MIME-sniffing on the response body, potentially causing the response body to be interpreted
-    /// and displayed as a content type other than the declared content type. Current (early 2014) and legacy versions
-    /// of Firefox will use the declared content type (if one is set), rather than performing MIME-sniffing."
+    /// "The Anti-MIME-Sniffing header X-Content-Type-Options was not set to ’nosniff’. This allows older versions of
+    /// Internet Explorer and Chrome to perform MIME-sniffing on the response body, potentially causing the response
+    /// body to be interpreted  and displayed as a content type other than the declared content type. Current (early
+    /// 2014) and legacy versions  of Firefox will use the declared content type (if one is set), rather than performing
+    /// MIME-sniffing." As written in <a href="https://www.zaproxy.org/docs/alerts/10021/">the documentation</a>.
     /// </para></remarks>
     public static IApplicationBuilder UseNosniffContentTypeOptionsHeader(this IApplicationBuilder app) =>
         app.Use(async (context, next) =>
