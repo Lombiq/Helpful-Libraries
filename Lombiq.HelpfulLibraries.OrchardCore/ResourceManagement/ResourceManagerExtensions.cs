1aa3c144021015f2eb32d408ad8b4d082c441ff4
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-13 16:55:43 +0100

    Add methods for rendering ES modules.

--- Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceManagerExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceManagerExtensions.cs
@@ -1,8 +1,14 @@
 using Lombiq.HelpfulLibraries.OrchardCore.ResourceManagement;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using OrchardCore.ResourceManagement.TagHelpers;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 namespace OrchardCore.ResourceManagement;
 
@@ -60,6 +66,105 @@ public static class ResourceManagerExtensions
     /// </summary>
     public static RequireSettings RegisterScriptModule(this IResourceManager resourceManager, string name) =>
         resourceManager.RegisterResource(ResourceTypes.ScriptModule, name);
+
+    /// <summary>
+    /// Turns the required <c>script-module"</c> resources into <c>&lt;script src="..." type="module"&gt;</c> elements.
+    /// </summary>
+    public static IEnumerable<TagBuilder> GetRequiredScriptModuleTags(
+        this IResourceManager resourceManager,
+        string applicationPath = null,
+        Func<ResourceRequiredContext, bool> filter = null)
+    {
+        var contexts = resourceManager.GetRequiredResources(ResourceTypes.ScriptModule);
+        if (filter != null) contexts = contexts.Where(filter);
+
+        return contexts.Select(context => new TagBuilder("script")
+        {
+            TagRenderMode = TagRenderMode.SelfClosing,
+            Attributes =
+            {
+                ["type"] = "module",
+                ["src"] = context.Resource.GetResourceUrl(
+                    context.FileVersionProvider,
+                    context.Settings.DebugMode,
+                    context.Settings.CdnMode,
+                    applicationPath),
+            },
+        });
+    }
+
+    /// <summary>
+    /// Turns the required <c>script-module"</c> resource with the <paramref name="resourceName"/> into a
+    /// <c>&lt;script src="..." type="module"&gt;</c> element.
+    /// </summary>
+    public static TagBuilder GetRequiredScriptModuleTag(
+        this IResourceManager resourceManager,
+        string applicationPath,
+        string resourceName) =>
+        resourceManager
+            .GetRequiredScriptModuleTags(
+                applicationPath,
+                context => context.Resource.Name.EqualsOrdinalIgnoreCase(resourceName))
+            .FirstOrDefault();
+
+    /// <summary>
+    /// Returns a <c>&lt;script type="importmap"&gt;</c> element that maps all the registered module resources by
+    /// resource name to their respective URLs so you can import these resources in your module type scripts using
+    /// <c>import someModule from 'resourceName'</c> instead of using the full resource URL. This way import will work
+    /// regardless of your CDN configuration.
+    /// </summary>
+    public static IHtmlContent GetScriptModuleMap(
+        this ResourceManagementOptions resources,
+        IFileVersionProvider fileVersionProvider)
+    {
+        var imports = resources
+            .ResourceManifests
+            .SelectMany(manifest => manifest.GetResources(ResourceTypes.ScriptModule).Values)
+            .SelectMany(list => list)
+            .ToDictionary(
+                resource => resource.Name,
+                resource => resource.GetResourceUrl(
+                    fileVersionProvider,
+                    resources.DebugMode,
+                    resources.UseCdn,
+                    resources.ContentBasePath));
+
+        var tagBuilder = new TagBuilder("script")
+        {
+            TagRenderMode = TagRenderMode.Normal,
+            Attributes = { ["type"] = "importmap" },
+        };
+
+        tagBuilder.InnerHtml.AppendHtml(JsonSerializer.Serialize(new { imports }));
+        return tagBuilder;
+    }
+
+    private static string GetResourceUrl(
+        this ResourceDefinition definition,
+        IFileVersionProvider fileVersionProvider,
+        bool isDebug,
+        bool isCdn,
+        PathString basePath)
+    {
+        static string Coalesce(params string[] strings) => strings.Find(str => !string.IsNullOrEmpty(str));
+
+        var url = (isDebug, isCdn) switch
+        {
+            (true, true) => Coalesce(definition.UrlCdnDebug, definition.UrlDebug, definition.UrlCdn, definition.Url),
+            (true, false) => Coalesce(definition.UrlDebug, definition.Url, definition.UrlCdnDebug, definition.UrlCdn),
+            (false, true) => Coalesce(definition.UrlCdn, definition.Url, definition.UrlCdnDebug, definition.UrlDebug),
+            (false, false) => Coalesce(definition.Url, definition.UrlDebug, definition.UrlCdn, definition.UrlCdnDebug),
+        };
+
+        if (string.IsNullOrEmpty(url)) return url;
+
+        if (url.StartsWith("~/", StringComparison.Ordinal))
+        {
+            url = basePath + url[1..];
+        }
+
+        return fileVersionProvider.AddFileVersionToPath(basePath, url);
+    }
 
     private static RequireSettings SetVersionIfAny(RequireSettings requireSettings, string version)
     {
