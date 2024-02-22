e1f2e121c43055c0911606c8ed9583446f35eed0
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-22 02:33:46 +0100

    Restore accidentally deleted documentation.

--- Lombiq.HelpfulLibraries.AspNetCore/Security/IContentSecurityPolicyProvider.cs
+++ Lombiq.HelpfulLibraries.AspNetCore/Security/IContentSecurityPolicyProvider.cs
@@ -20,6 +20,10 @@ public interface IContentSecurityPolicyProvider
     /// </summary>
     public ValueTask UpdateAsync(IDictionary<string, string> securityPolicies, HttpContext context);
 
+    /// <summary>
+    /// Returns the first non-empty directive from the <paramref name="names"/> or <see cref="DefaultSrc"/> or an empty
+    /// string.
+    /// </summary>
     [Obsolete($"Use the method in the {nameof(ContentSecurityPolicyProvider)} static class instead.")]
     public static string GetDirective(IDictionary<string, string> securityPolicies, params string[] names) =>
         ContentSecurityPolicyProvider.GetDirective(securityPolicies, names.AsEnumerable());
