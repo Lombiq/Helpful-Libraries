25650dc642ba30570b3fedeb58596554f8b25025
Sára El-Saig <sara.el-saig@lombiq.com>
2024-03-21 15:09:34 +0100

    unusing

--- Lombiq.HelpfulLibraries.AspNetCore/Security/BrowserLinkContentSecurityPolicyProvider.cs
+++ Lombiq.HelpfulLibraries.AspNetCore/Security/BrowserLinkContentSecurityPolicyProvider.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 using static Lombiq.HelpfulLibraries.AspNetCore.Security.ContentSecurityPolicyDirectives;
