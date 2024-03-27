b7e465478759f46de09de58ec27ac903c4282348
Aydin Erdas <info@aydinerdas.com>
2024-03-27 20:08:48 +0100

    Revert "Merge pull request #245 from Lombiq/issue/OSOE-819"
    This reverts commit 929faca7d3d2728ff42112f21a55af0b09514435, reversing
    changes made to 749343ba6e7d6f74819b3b91dc39c9b5da8b7192.

--- Lombiq.HelpfulLibraries.SourceGenerators/Lombiq.HelpfulLibraries.SourceGenerators/ConstantFromJsonAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace Lombiq.HelpfulLibraries.SourceGenerators;
-
-[System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
-public sealed class ConstantFromJsonAttribute : System.Attribute
-{
-    public ConstantFromJsonAttribute(string constantName, string fileName, string propertyName)
-    {
-    }
-}
