e6da6c3fa257cabd34e6f043bee577fbd935d93f
Aydin Erdas <info@aydinerdas.com>
2024-03-28 12:40:09 +0100

    Recreated source generator

---
+++ Lombiq.HelpfulLibraries.Tests/UnitTests/SourceGenerators/ConstantFromJsonTest.cs
@@ -0,0 +1,20 @@
+﻿using DefaultNamespace;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace Lombiq.HelpfulLibraries.Tests.UnitTests.SourceGenerators;
+
+public class ConstantFromJsonTest
+{
+    [Fact]
+    public void TestGeneratedConstants()
+    {
+        ConstantFromJsonSample.GulpVersion.ShouldBe("3.9.0");
+        ConstantFromJsonSample.GulpUglifyVersion.ShouldBe("1.4.1");
+        new ConstantFromJsonSample()
+            .ReturnVersions()
+            .Split(["\n", "\r"], StringSplitOptions.RemoveEmptyEntries)
+            .ShouldBe(["Gulp version: 3.9.0", "Gulp-uglify version: 1.4.1"]);
+    }
+}
