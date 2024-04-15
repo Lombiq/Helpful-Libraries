035dcdcafcbdc26445f38ce4df49a2b11a586dde
Sára El-Saig <sara.el-saig@lombiq.com>
2024-04-15 07:15:38 +0200

    Add ContentPartFieldDefinitionBuilderExtensions.ConfigureContentPicker.

---
+++ Lombiq.HelpfulLibraries.OrchardCore/Contents/ContentPartFieldDefinitionBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using OrchardCore.ContentFields.Settings;
+
+namespace OrchardCore.ContentManagement.Metadata.Builders;
+
+public static class ContentPartFieldDefinitionBuilderExtensions
+{
+    /// <summary>
+    /// Applies <see cref="ContentPickerFieldSettings"/> to the field. Constrains the displayed types to <paramref
+    /// name="contentTypes"/> and only allows single selection.
+    /// </summary>
+    public static ContentPartFieldDefinitionBuilder ConfigureContentPicker(
+        this ContentPartFieldDefinitionBuilder builder,
+        params string[] contentTypes) =>
+        builder.ConfigureContentPicker(multiple: false, contentTypes);
+
+    /// <summary>
+    /// Applies <see cref="ContentPickerFieldSettings"/> to the field. Constrains the displayed types to <paramref
+    /// name="contentTypes"/> and defines if <paramref name="multiple"/> selection is permitted.
+    /// </summary>
+    public static ContentPartFieldDefinitionBuilder ConfigureContentPicker(
+        this ContentPartFieldDefinitionBuilder builder,
+        bool multiple,
+        params string[] contentTypes) =>
+        builder.WithSettings(new ContentPickerFieldSettings
+        {
+            Multiple = multiple,
+            DisplayAllContentTypes = false,
+            DisplayedContentTypes = contentTypes,,
+        });
+}
