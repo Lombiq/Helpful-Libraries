d55a5bba6a46f733b57aa989b088d6c77bbf3507
Sára El-Saig <sara.el-saig@lombiq.com>
2024-04-20 16:53:08 +0200

    More shortcut.

--- Lombiq.HelpfulLibraries.OrchardCore/Data/IndexExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Data/IndexExtensions.cs
@@ -5,6 +5,17 @@ namespace YesSql.Indexes;
 
 public static class IndexExtensions
 {
+    /// <summary>
+    /// Describes a <see cref="ContentItem"/> index to the specified <see cref="ContentPart"/>.
+    /// </summary>
+    public static IGroupFor<TIndex> MapFor<TPart, TIndex>(this DescribeContext<ContentItem> context, Func<TPart, TIndex> map)
+        where TPart : ContentPart
+        where TIndex : IIndex =>
+        context.For<TIndex>().Map(map);
+
+    /// <summary>
+    /// Filters and maps a <see cref="ContentItem"/> index to the specified <see cref="ContentPart"/>.
+    /// </summary>
     public static IGroupFor<TIndex> Map<TPart, TIndex>(this IMapFor<ContentItem, TIndex> mapFor, Func<TPart, TIndex> map)
         where TPart : ContentPart
         where TIndex : IIndex =>
