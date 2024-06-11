d36108c12580ef6ee8135855b483b30aec1c5fdf
Sára El-Saig <sara.el-saig@lombiq.com>
2024-06-11 17:08:09 +0200

    Add AuthorizeForCurrentUserAndExecuteAsync extension method.

--- Lombiq.HelpfulLibraries.OrchardCore/Users/AuthorizationServiceExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Users/AuthorizationServiceExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using OrchardCore.Security.Permissions;
+using OrchardCore.Users.Models;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Authorization;
@@ -13,4 +17,42 @@ public static class AuthorizationServiceExtensions
         context?.User is { Identity.IsAuthenticated: true } user
             ? service.AuthorizeAsync(user, permission)
             : Task.FromResult(false);
+
+    public static async Task<IActionResult> AuthorizeForCurrentUserAndExecuteAsync<TData, TResult>(
+        this IAuthorizationService service,
+        Controller controller,
+        IEnumerable<Permission> permissions,
+        Func<Task<(bool IsSuccess, TData Data)>> validateAsync,
+        Func<TData, Task<TResult>> executeAsync,
+        string authenticationScheme = "Api")
+    {
+        foreach (var permission in permissions)
+        {
+            if (!await service.AuthorizeAsync(controller.User, permission))
+            {
+                return controller.ChallengeOrForbid(authenticationScheme);
+            }
+        }
+
+        var (isSuccess, data) = validateAsync is null ? (true, default) : await validateAsync();
+        if (!isSuccess) return controller.NotFound();
+
+        var result = await executeAsync(data);
+        return result is IActionResult actionResult ? actionResult : controller.Ok(result);
+    }
+
+    public static Task<IActionResult> AuthorizeForCurrentUserValidateNotNullAndExecuteAsync<TData, TResult>(
+        this IAuthorizationService service,
+        Controller controller,
+        IEnumerable<Permission> permissions,
+        Func<Task<TData>> validateAsync,
+        Func<TData, Task<TResult>> executeAsync,
+        string authenticationScheme = "Api")
+        where TData : class =>
+        service.AuthorizeForCurrentUserAndExecuteAsync<TData, TResult>(
+            controller,
+            permissions,
+            async () => await validateAsync() is { } data ? (true, data) : (false, default),
+            executeAsync,
+            authenticationScheme);
 }
