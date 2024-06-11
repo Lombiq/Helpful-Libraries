4ecc45fb17a98e10b6304891cd23f9c4be4816f5
Sára El-Saig <sara.el-saig@lombiq.com>
2024-06-11 17:23:26 +0200

    Add docs.

--- Lombiq.HelpfulLibraries.OrchardCore/Users/AuthorizationServiceExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Users/AuthorizationServiceExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OrchardCore.Security.Permissions;
-using OrchardCore.Users.Models;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -18,6 +17,32 @@ public static class AuthorizationServiceExtensions
             ? service.AuthorizeAsync(user, permission)
             : Task.FromResult(false);
 
+    /// <summary>
+    /// Authorizes the current user against the provided <paramref name="permissions"/>, performs additional optional
+    /// validation and if all passes returns <paramref name="executeAsync"/>.
+    /// </summary>
+    /// <param name="service">The service used for authorization.</param>
+    /// <param name="controller">The controller whose current context is used for authorization and results..</param>
+    /// <param name="permissions">The <see cref="Controller.User"/> must pass each of these permissions.</param>
+    /// <param name="validateAsync">
+    /// An optional fetching or validation step, if it's <see langword="null"/> then it's ignored and <see
+    /// langword="default"/> is passed to the <paramref name="executeAsync"/>. The first value of its result must be <
+    /// see langword="true"/>, otherwise <see cref="NotFoundResult"/> is returned. Use this for example to query a value
+    /// from the database or to simply return an opaque failure state.
+    /// </param>
+    /// <param name="executeAsync">
+    /// The method that returns the final result if everything is ok. If the result is <see cref="IActionResult"/> then
+    /// it's returned as-is, otherwise it's passed to <see cref="ControllerBase.Ok()"/>.
+    /// </param>
+    /// <param name="authenticationScheme">
+    /// The information used by <see cref="ChallengeResult"/> or <see cref="ForbidResult"/> if the authorization fails.
+    /// This is conventionally <c>Api</c> so usually the parameter can be skipped.
+    /// </param>
+    /// <typeparam name="TData">
+    /// The type of the intermediate result received from <paramref name="validateAsync"/> and passed to <paramref
+    /// name="executeAsync"/>.
+    /// </typeparam>
+    /// <typeparam name="TResult">The final outcome.</typeparam>
     public static async Task<IActionResult> AuthorizeForCurrentUserAndExecuteAsync<TData, TResult>(
         this IAuthorizationService service,
         Controller controller,
@@ -41,6 +66,7 @@ public static class AuthorizationServiceExtensions
         return result is IActionResult actionResult ? actionResult : controller.Ok(result);
     }
 
+    /// <inheritdoc cref="AuthorizeForCurrentUserAndExecuteAsync{TData,TResult}"/>
     public static Task<IActionResult> AuthorizeForCurrentUserValidateNotNullAndExecuteAsync<TData, TResult>(
         this IAuthorizationService service,
         Controller controller,
@@ -49,7 +75,7 @@ public static class AuthorizationServiceExtensions
         Func<TData, Task<TResult>> executeAsync,
         string authenticationScheme = "Api")
         where TData : class =>
-        service.AuthorizeForCurrentUserAndExecuteAsync<TData, TResult>(
+        service.AuthorizeForCurrentUserAndExecuteAsync(
             controller,
             permissions,
             async () => await validateAsync() is { } data ? (true, data) : (false, default),
