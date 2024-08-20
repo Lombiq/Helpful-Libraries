bef643bc4902750822c1602a92cb8ad2a128fb11
Sára El-Saig <sara.el-saig@lombiq.com>
2024-08-21 00:54:49 +0200

    More detailed doc.

--- Lombiq.HelpfulLibraries.OrchardCore/Liquid/ILiquidParserTag.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Liquid/ILiquidParserTag.cs
@@ -1,5 +1,6 @@
 using Fluid;
 using Fluid.Ast;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Encodings.Web;
@@ -8,7 +9,8 @@ using System.Threading.Tasks;
 namespace Lombiq.HelpfulLibraries.OrchardCore.Liquid;
 
 /// <summary>
-/// Describes a Liquid parser tag's parser and render methods.
+/// Describes a Liquid parser tag's renderer method. This is used in <see
+/// cref="LiquidServiceCollectionExtensions.AddLiquidParserTag{T}"/> to add a custom <c>{% tag_name %}</c> tag.
 /// </summary>
 public interface ILiquidParserTag
 {
