32ffda3f9cdea2645a459565ca2822be678b00c0
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-03-20 22:37:44 +0100

    Docs and code styling

--- Lombiq.HelpfulLibraries.OrchardCore/Security/SecurityOrchardCoreBuilderExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Security/SecurityOrchardCoreBuilderExtensions.cs
@@ -22,102 +22,7 @@ public static class SecurityOrchardCoreBuilderExtensions
         builder.ConfigureServices((services, _) =>
             services.Configure<AntiforgeryOptions>(options => options.Cookie.SecurePolicy = CookieSecurePolicy.Always));
 
-    /// <summary>
-    /// Provides some default security configuration for Orchard Core.
-    /// </summary>
-    /// <remarks>
-    /// <para>This extension method configures the application as listed below.</para>
-    /// <list type="bullet">
-    ///     <item>
-    ///         <description>
-    ///             Add <see cref="CdnContentSecurityPolicyProvider"/> to permit using script and style resources from
-    ///             some common CDNs.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///             Add <see cref="VueContentSecurityPolicyProvider"/> to permit script evaluation when the <c>vuejs</c>
-    ///             resource is included.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///             Add <see cref="ContentSecurityPolicyAttributeContentSecurityPolicyProvider"/> to amend the content
-    ///             security policy using the <see cref="ContentSecurityPolicyAttribute"/>.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///             Add <see cref="SkipContentSecurityPolicyProvider"/> to skip declaring a content security policy on
-    ///             responses where it makes no sense.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///             Add <see cref="BrowserLinkContentSecurityPolicyProvider"/> to permit accessing other ports on
-    ///             <c>localhost</c> during local development.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///             Make the session token's cookie always secure.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///             Make the anti-forgery token's cookie always secure.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///             Enable the <c>OrchardCore.Diagnostics</c> feature to provide custom error screens in production and
-    ///             don't leak error information.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///            Adds <see cref="EmbeddedMediaContentSecurityPolicyProvider"/> that provides permitted hosts for the
-    ///            <c>frame-src</c> directive of the <c>Content-Security-Policy</c> header, covering usual media
-    ///            embedding sources like YouTube.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///            Adds <see cref="ExternalLoginContentSecurityPolicyProvider"/> that provides permitted hosts for the
-    ///            <c>form-action</c> directive of the <c>Content-Security-Policy</c> header, covering external login
-    ///            providers that require this (like Microsoft and GitHub).
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///            Adds <see cref="ReCaptchaContentSecurityPolicyProvider"/> that provides various directives for the
-    ///            <c>Content-Security-Policy</c> header, allowing using ReCaptcha captchas.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///            Adds <see cref="GoogleAnalyticsContentSecurityPolicyProvider"/> that provides various directives for
-    ///            the <c>Content-Security-Policy</c> header, allowing using Google Analytics tracking.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///            Adds a middleware that supplies the <c>Content-Security-Policy</c> header.
-    ///         </description>
-    ///     </item>
-    ///     <item>
-    ///         <description>
-    ///             Adds a middleware that supplies the <c>X-Content-Type-Options: nosniff</c> header.
-    ///         </description>
-    ///     </item>
-    /// </list>
-    /// <para>
-    /// If you also need static file support, consider using <see
-    /// cref="ConfigureSecurityDefaultsWithStaticFiles(OrchardCoreBuilder, ContentSecurityPolicyHeaderConfiguration)"/>
-    /// instead. Alternatively, make sure to put the <c>app.UseStaticFiles()</c> call at the very end of your app
-    /// configuration chain so it won't short-circuit prematurely and miss adding security headers to your static files.
-    /// </para>
-    /// </remarks>
+    /// <inheritdoc cref="ConfigureSecurityDefaults(OrchardCoreBuilder, ContentSecurityPolicyHeaderConfiguration)"/>
     [Obsolete("Use the overload with the ContentSecurityPolicyHeaderConfiguration parameter instead.")]
     public static OrchardCoreBuilder ConfigureSecurityDefaults(
         this OrchardCoreBuilder builder,
@@ -137,79 +42,79 @@ public static class SecurityOrchardCoreBuilderExtensions
     /// <list type="bullet">
     ///     <item>
     ///         <description>
-    ///             Add <see cref="CdnContentSecurityPolicyProvider"/> to permit using script and style resources from
+    ///             Adds <see cref="CdnContentSecurityPolicyProvider"/> to permit using script and style resources from
     ///             some common CDNs.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///             Add <see cref="VueContentSecurityPolicyProvider"/> to permit script evaluation when the <c>vuejs</c>
-    ///             resource is included.
+    ///             Adds <see cref="VueContentSecurityPolicyProvider"/> to permit script evaluation when the
+    ///             <c>vuejs</c> resource is included.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///             Add <see cref="ContentSecurityPolicyAttributeContentSecurityPolicyProvider"/> to amend the content
+    ///             Adds <see cref="ContentSecurityPolicyAttributeContentSecurityPolicyProvider"/> to amend the content
     ///             security policy using the <see cref="ContentSecurityPolicyAttribute"/>.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///             Add <see cref="SkipContentSecurityPolicyProvider"/> to skip declaring a content security policy on
+    ///             Adds <see cref="SkipContentSecurityPolicyProvider"/> to skip declaring a content security policy on
     ///             responses where it makes no sense.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///             Add <see cref="BrowserLinkContentSecurityPolicyProvider"/> to permit accessing other ports on
+    ///             Adds <see cref="BrowserLinkContentSecurityPolicyProvider"/> to permit accessing other ports on
     ///             <c>localhost</c> during local development.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///             Make the session token's cookie always secure.
+    ///             Makes the session token's cookie always secure.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///             Make the anti-forgery token's cookie always secure.
+    ///             Makes the anti-forgery token's cookie always secure.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///             Enable the <c>OrchardCore.Diagnostics</c> feature to provide custom error screens in production and
+    ///             Enables the <c>OrchardCore.Diagnostics</c> feature to provide custom error screens in production and
     ///             don't leak error information.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///            Adds <see cref="EmbeddedMediaContentSecurityPolicyProvider"/> that provides permitted hosts for the
-    ///            <c>frame-src</c> directive of the <c>Content-Security-Policy</c> header, covering usual media
-    ///            embedding sources like YouTube.
+    ///             Adds <see cref="EmbeddedMediaContentSecurityPolicyProvider"/> that provides permitted hosts for the
+    ///             <c>frame-src</c> directive of the <c>Content-Security-Policy</c> header, covering usual media
+    ///             embedding sources like YouTube.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///            Adds <see cref="ExternalLoginContentSecurityPolicyProvider"/> that provides permitted hosts for the
-    ///            <c>form-action</c> directive of the <c>Content-Security-Policy</c> header, covering external login
-    ///            providers that require this (like Microsoft and GitHub).
+    ///             Adds <see cref="ExternalLoginContentSecurityPolicyProvider"/> that provides permitted hosts for the
+    ///             <c>form-action</c> directive of the <c>Content-Security-Policy</c> header, covering external login
+    ///             providers that require this (like Microsoft and GitHub).
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///            Adds <see cref="ReCaptchaContentSecurityPolicyProvider"/> that provides various directives for the
-    ///            <c>Content-Security-Policy</c> header, allowing using ReCaptcha captchas.
+    ///             Adds <see cref="ReCaptchaContentSecurityPolicyProvider"/> that provides various directives for the
+    ///             <c>Content-Security-Policy</c> header, allowing using ReCaptcha captchas.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///            Adds <see cref="GoogleAnalyticsContentSecurityPolicyProvider"/> that provides various directives for
-    ///            the <c>Content-Security-Policy</c> header, allowing using Google Analytics tracking.
+    ///             Adds <see cref="GoogleAnalyticsContentSecurityPolicyProvider"/> that provides various directives for
+    ///             the <c>Content-Security-Policy</c> header, allowing using Google Analytics tracking.
     ///         </description>
     ///     </item>
     ///     <item>
     ///         <description>
-    ///            Adds a middleware that supplies the <c>Content-Security-Policy</c> header.
+    ///             Adds a middleware that supplies the <c>Content-Security-Policy</c> header.
     ///         </description>
     ///     </item>
     ///     <item>
