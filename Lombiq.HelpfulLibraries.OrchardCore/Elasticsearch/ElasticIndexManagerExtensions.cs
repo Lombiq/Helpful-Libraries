9bfc4bdf3b6a1e84137e28fad5d636124e153567
Sára El-Saig <sara.el-saig@lombiq.com>
2025-04-13 21:42:44 +0200

    Clarify doc.

--- Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ElasticIndexManagerExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ElasticIndexManagerExtensions.cs
@@ -5,8 +5,8 @@ namespace OrchardCore.Search.Elasticsearch.Core.Services;
 public static class ElasticIndexManagerExtensions
 {
     /// <summary>
-    /// Clear all indexes (within the prefix, if there is one) by passing a wildcard character
-    /// (<c>*</c>) as the index name.
+    /// Clear all indexes for the tenant (within the prefix, if there is one) by passing a wildcard
+    /// character (<c>*</c>) as the index name.
     /// </summary>
     public static Task<bool> DeleteAllIndexesAsync(this ElasticIndexManager manager) =>
         manager.DeleteIndex("*");
