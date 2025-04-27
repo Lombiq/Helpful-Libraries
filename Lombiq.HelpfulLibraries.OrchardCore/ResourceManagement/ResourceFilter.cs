082bc5eae518ca86fd67776cd3eb9c092212eab3
Sára El-Saig <sara.el-saig@lombiq.com>
2025-04-27 18:26:13 +0200

    Make ResourceFilter chainable.

--- Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceFilter.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/ResourceManagement/ResourceFilter.cs
@@ -13,38 +13,46 @@ public class ResourceFilter
     public Action<IResourceManager> Execution { get; set; }
     public Func<IResourceManager, Task> ExecutionAsync { get; set; }
 
-    public void Execute(Action<IResourceManager> action) => Execution = action;
+    public ResourceFilter Execute(Action<IResourceManager> action)
+    {
+        Execution = action;
+        return this;
+    }
 
-    public void ExecuteTask(Func<IResourceManager, Task> actionAsync) => ExecutionAsync = actionAsync;
+    public ResourceFilter ExecuteTask(Func<IResourceManager, Task> actionAsync)
+    {
+        ExecutionAsync = actionAsync;
+        return this;
+    }
 
     /// <summary>
     /// Registers the provided <c>stylesheet</c> <paramref name="resources"/>.
     /// </summary>
-    public void RegisterStylesheet(params string[] resources) =>
+    public ResourceFilter RegisterStylesheet(params string[] resources) =>
         Execute(resourceManager => resources.ForEach(resource => resourceManager.RegisterResource("stylesheet", resource)));
 
     /// <summary>
     /// Registers the provided <c>script</c> <paramref name="resources"/> at the foot of the page.
     /// </summary>
-    public void RegisterFootScript(params string[] resources) =>
+    public ResourceFilter RegisterFootScript(params string[] resources) =>
         Execute(resourceManager => resources.ForEach(resource => resourceManager.RegisterResource("script", resource).AtFoot()));
 
     /// <summary>
     /// Registers the provided <c>script</c> <paramref name="resources"/> at the head of the page.
     /// </summary>
-    public void RegisterHeadScript(params string[] resources) =>
+    public ResourceFilter RegisterHeadScript(params string[] resources) =>
         Execute(resourceManager => resources.ForEach(resource => resourceManager.RegisterResource("script", resource).AtHead()));
 
     /// <summary>
     /// Registers the provided <c>link</c> <paramref name="resources"/> at the head of the page.
     /// </summary>
-    public void RegisterLink(params LinkEntry[] resources) =>
+    public ResourceFilter RegisterLink(params LinkEntry[] resources) =>
         Execute(resourceManager => resources.ForEach(resourceManager.RegisterLink));
 
     /// <summary>
     /// Registers an icon <c>link</c> resource with the provided address and attributes.
     /// </summary>
-    public void RegisterFavoriteIcon(string href, string type = "image/x-icon", string rel = "shortcut icon") =>
+    public ResourceFilter RegisterFavoriteIcon(string href, string type = "image/x-icon", string rel = "shortcut icon") =>
         Execute(resourceManager => resourceManager.RegisterLink(new LinkEntry
         {
             Href = href,
