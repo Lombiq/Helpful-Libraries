e872128de8baab4cd520f9a781542f10835eddff
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-21 03:48:30 +0200

    Remove ToSnakeCase because OC already has it in the same namespace.

--- Lombiq.HelpfulLibraries.Common/Extensions/StringExtensions.cs
+++ Lombiq.HelpfulLibraries.Common/Extensions/StringExtensions.cs
@@ -320,20 +320,6 @@ public static class StringExtensions
     }
 
     /// <summary>
-    /// Turns a camelCase or PascalCase token into snake_Case.
-    /// </summary>
-    /// <param name="input">The input in camelCase or PascalCase.</param>
-    /// <returns>
-    /// The input converted to snake_Case. It doesn't alter case so you can call either ToUpper or ToLower without any
-    /// additional penalties.
-    /// </returns>
-    /// <remarks>
-    /// <para><see href="https://stackoverflow.com/a/18781533"/>.</para>
-    /// </remarks>
-    public static string ToSnakeCase(this string input) =>
-        string.Concat(input.Select((character, index) => index > 0 && char.IsUpper(character) ? "_" + character : character.ToString()));
-
-    /// <summary>
     /// Splits the text into three pieces similarly to Python's <c>str.partition</c> method.
     /// </summary>
     /// <param name="text">The text to partition.</param>
