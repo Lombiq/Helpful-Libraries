2c5706a2dc23cdb3a77c40c5dd500d58b36a7dc0
Sára El-Saig <sara.el-saig@lombiq.com>
2025-06-08 21:34:43 +0200

    Add more general ITaxonomyHelper methods.

--- Lombiq.HelpfulLibraries.OrchardCore/Contents/ITaxonomyHelper.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Contents/ITaxonomyHelper.cs
@@ -1,4 +1,5 @@
 using OrchardCore.ContentManagement;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Lombiq.HelpfulLibraries.OrchardCore.Contents;
@@ -12,4 +13,18 @@ public interface ITaxonomyHelper
     /// Returns <see cref="ContentItem"/> by the given <paramref name="alias"/> and <paramref name="termId"/>.
     /// </summary>
     Task<ContentItem> GetTermContentItemByTaxonomyAliasAsync(string alias, string termId);
+
+    /// <summary>
+    /// Gets the taxonomy content item with the ID indicated by <paramref name="taxonomyId"/> and recursively searches
+    /// for each term <see cref="ContentItem"/> with an ID in <paramref name="termIds"/>.
+    /// </summary>
+    /// <param name="termIds">The selected term IDs. If it's <see langword="null"/> or empty, all terms are returned instead.</param>
+    Task<IEnumerable<ContentItem>> GetTermsOfTaxonomyByIdAsync(string taxonomyId, IEnumerable<string> termIds);
+
+    /// <summary>
+    /// Gets the taxonomy content item with the alias indicated by <paramref name="taxonomyAlias"/> and recursively
+    /// searches for each term <see cref="ContentItem"/> with an ID in <paramref name="termIds"/>.
+    /// </summary>
+    /// <param name="termIds">The selected term IDs. If it's <see langword="null"/> or empty, all terms are returned instead.</param>
+    Task<IEnumerable<ContentItem>> GetTermsOfTaxonomyByAliasAsync(string taxonomyAlias, IEnumerable<string> termIds);
 }
