841c641848aeed9c7d8a8c90e8064bdc9a01d477
Sára El-Saig <sara.el-saig@lombiq.com>
2025-06-10 00:08:48 +0200

    Add GetTermsOfTaxonomyFieldAsync shortcut method.

--- Lombiq.HelpfulLibraries.OrchardCore/Contents/ITaxonomyHelper.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/Contents/ITaxonomyHelper.cs
@@ -1,4 +1,5 @@
 using OrchardCore.ContentManagement;
+using OrchardCore.Taxonomies.Fields;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -12,19 +13,25 @@ public interface ITaxonomyHelper
     /// <summary>
     /// Returns <see cref="ContentItem"/> by the given <paramref name="alias"/> and <paramref name="termId"/>.
     /// </summary>
-    Task<ContentItem> GetTermContentItemByTaxonomyAliasAsync(string alias, string termId);
+    public Task<ContentItem> GetTermContentItemByTaxonomyAliasAsync(string alias, string termId);
 
     /// <summary>
     /// Gets the taxonomy content item with the ID indicated by <paramref name="taxonomyId"/> and recursively searches
     /// for each term <see cref="ContentItem"/> with an ID in <paramref name="termIds"/>.
     /// </summary>
     /// <param name="termIds">The selected term IDs. If it's <see langword="null"/>, all terms are returned instead.</param>
-    Task<IEnumerable<ContentItem>> GetTermsOfTaxonomyByIdAsync(string taxonomyId, IEnumerable<string> termIds);
+    public Task<IEnumerable<ContentItem>> GetTermsOfTaxonomyByIdAsync(string taxonomyId, IEnumerable<string> termIds);
 
     /// <summary>
     /// Gets the taxonomy content item with the alias indicated by <paramref name="taxonomyAlias"/> and recursively
     /// searches for each term <see cref="ContentItem"/> with an ID in <paramref name="termIds"/>.
     /// </summary>
     /// <param name="termIds">The selected term IDs. If it's <see langword="null"/>, all terms are returned instead.</param>
-    Task<IEnumerable<ContentItem>> GetTermsOfTaxonomyByAliasAsync(string taxonomyAlias, IEnumerable<string> termIds);
+    public Task<IEnumerable<ContentItem>> GetTermsOfTaxonomyByAliasAsync(string taxonomyAlias, IEnumerable<string> termIds);
+
+    /// <summary>
+    /// Invokes <see cref="GetTermsOfTaxonomyByIdAsync"/> using a provided <see cref="TaxonomyField"/>.
+    /// </summary>
+    public Task<IEnumerable<ContentItem>> GetTermsOfTaxonomyFieldAsync(TaxonomyField field) =>
+        GetTermsOfTaxonomyByIdAsync(field.TaxonomyContentItemId, field.TermContentItemIds);
 }
