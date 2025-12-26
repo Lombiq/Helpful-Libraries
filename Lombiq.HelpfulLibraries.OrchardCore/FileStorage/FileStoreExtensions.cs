d1688c35ac2fd7d471678e5181d248fa6cd23e14
Sára El-Saig <sara.el-saig@lombiq.com>
2025-12-26 22:39:12 +0100

    Update Lombiq.HelpfulLibraries.OrchardCore.FileStorage.

--- Lombiq.HelpfulLibraries.OrchardCore/FileStorage/FileStoreExtensions.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/FileStorage/FileStoreExtensions.cs
@@ -9,12 +9,12 @@ public static class FileStoreExtensions
     /// </summary>
     /// <param name="path">Relative path to the file.</param>
     public static async Task<bool> FileExistsAsync(this IFileStore fileStore, string path) =>
-        (await fileStore.GetFileInfoAsync(path)) != null;
+        await fileStore.GetFileInfoAsync(path) != null;
 
     /// <summary>
     /// Checks the existence of a directory in the file store.
     /// </summary>
     /// <param name="path">Relative path to the directory.</param>
     public static async Task<bool> DirectoryExistsAsync(this IFileStore fileStore, string path) =>
-        (await fileStore.GetDirectoryInfoAsync(path)) != null;
+        await fileStore.GetDirectoryInfoAsync(path) != null;
 }
