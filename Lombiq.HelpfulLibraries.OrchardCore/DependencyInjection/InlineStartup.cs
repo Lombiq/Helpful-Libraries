cf7fc83204dc0c8d910a07247ddb3315c74e2411
Sára El-Saig <sara.el-saig@lombiq.com>
2025-12-26 22:29:47 +0100

    Update Lombiq.HelpfulLibraries.OrchardCore.DependencyInjection.

--- Lombiq.HelpfulLibraries.OrchardCore/DependencyInjection/InlineStartup.cs
+++ Lombiq.HelpfulLibraries.OrchardCore/DependencyInjection/InlineStartup.cs
@@ -1,6 +1,4 @@
-﻿#nullable enable
-
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
