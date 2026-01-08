1995f7a75190328104d4c63ea0cbbf6a644dfe1b
Gábor Domonkos <gabor.domonkos@lombiq.com>
2026-01-08 14:36:52 +0100

    Revert "Upgrading to Orchard 1.10.4"
    This reverts commit 7370e91c9263ef16f8a4788e0f5f629bf2b8bf98.

--- Libraries/Authentication/BasicAuthenticationService.cs
+++ Libraries/Authentication/BasicAuthenticationService.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Text;
-using Orchard;
+﻿using Orchard;
 using Orchard.Environment.Extensions;
 using Orchard.Mvc;
 using Orchard.Security;
+using System;
+using System.Text;
 
 namespace Piedone.HelpfulLibraries.Authentication
 {
@@ -79,7 +79,7 @@ namespace Piedone.HelpfulLibraries.Authentication
             var credentials = GetRequestCredentials();
             if (credentials == null) return null;
 
-            return _membershipService.ValidateUser(credentials.UserName, credentials.Password);
+            return _membershipService.ValidateUser(credentials.UserName, credentials.Password, out _);
         }
 
         public bool SetAuthenticatedUserForRequest()
