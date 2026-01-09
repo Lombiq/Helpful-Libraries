1ae336cbab84010884030f7664c8740eb4e9b291
Sára El-Saig <sara.el-saig@lombiq.com>
2026-01-09 12:19:33 +0100

    Add GetAllElasticsearchIndexesAsync extension.

---
+++ Lombiq.HelpfulLibraries.OrchardCore/Elasticsearch/ElasticsearchIndexProfileStoreExtensions.cs
@@ -0,0 +1,17 @@
+
+using OrchardCore.Indexing.Models;
+using OrchardCore.Search.Elasticsearch;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace OrchardCore.Indexing;
+
+public static class ElasticsearchIndexProfileStoreExtensions
+{
+    /// <summary>
+    /// Returns only the Elasticsearch indexes from the <paramref name="store"/>.
+    /// </summary>
+    public static async Task<IEnumerable<IndexProfile>> GetAllElasticsearchIndexesAsync(this IIndexProfileStore store) =>
+        (await store.GetAllAsync())
+        .Where(profile => profile.ProviderName == ElasticsearchConstants.ProviderName);
+}
